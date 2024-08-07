f57e840a8c4470be3b8c78aeb2c40ed6132cf160
ShikiSuen <shikisuen@outlook.com>
2024-08-07 17:06:05 +0800

    Enka // Add a file access API.

---
+++ Packages/PZKit/Sources/EnkaKit/Utils/EK_FileProvider.swift
@@ -0,0 +1,32 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+
+extension Enka {
+    static func getBundledJSONFileObject<T: Decodable>(
+        fileNameStem: String,
+        type: T.Type,
+        decoderConfigurator: ((JSONDecoder) -> Void)? = nil
+    )
+        -> T? {
+        guard let url = Bundle.module.url(
+            forResource: fileNameStem, withExtension: "json"
+        ) else { return nil }
+        guard let data = try? Data(contentsOf: url) else { return nil }
+        let decoder = JSONDecoder()
+        decoderConfigurator?(decoder)
+        return try? decoder.decode(T.self, from: data)
+    }
+
+    public static func getBundledJSONFileData(
+        fileNameStem: String
+    )
+        -> Data? {
+        guard let url = Bundle.module.url(
+            forResource: fileNameStem, withExtension: "json"
+        ) else { return nil }
+        return try? Data(contentsOf: url)
+    }
+}
