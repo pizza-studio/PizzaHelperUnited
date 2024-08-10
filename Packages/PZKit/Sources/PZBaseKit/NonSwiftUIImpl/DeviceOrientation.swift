e759f9bbf52dd062a570617f6b8d47289b696e5f
ShikiSuen <shikisuen@outlook.com>
2024-08-10 20:06:03 +0800

    PZKit // Refactor the OS dependency structure.

---
+++ Packages/PZKit/Sources/PZBaseKit/NonSwiftUIImpl/DeviceOrientation.swift
@@ -0,0 +1,100 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+
+#if !os(watchOS)
+#if canImport(AppKit)
+import AppKit
+#endif
+import Combine
+#if canImport(UIKit)
+import UIKit
+#endif
+
+// MARK: - DeviceOrientation
+
+@Observable
+public final class DeviceOrientation {
+    // MARK: Lifecycle
+
+    public init() {
+        #if canImport(UIKit)
+        self.orientation = UIDevice.current.orientation
+            .isLandscape ? .landscape : .portrait
+        self.listener = NotificationCenter.default
+            .publisher(for: UIDevice.orientationDidChangeNotification)
+            .compactMap { ($0.object as? UIDevice)?.orientation }
+            .compactMap { deviceOrientation -> Orientation? in
+                if deviceOrientation.isPortrait {
+                    return .portrait
+                } else if deviceOrientation.isLandscape {
+                    return .landscape
+                } else {
+                    return nil
+                }
+            }
+            .assign(to: \.orientation, on: self)
+        #else
+        self.orientation = .landscape
+        #endif
+    }
+
+    deinit {
+        listener?.cancel()
+    }
+
+    // MARK: Public
+
+    public enum Orientation {
+        case portrait
+        case landscape
+    }
+
+    // MARK: Internal
+
+    var orientation: Orientation
+
+    // MARK: Private
+
+    private var listener: AnyCancellable?
+}
+
+extension DeviceOrientation {
+    public static var basicWindowSize: CGSize {
+        .init(
+            width: 622,
+            height: 1107
+        )
+    }
+
+    public static var scaleRatioCompatible: CGFloat {
+        guard let windowSize = getKeyWindowSize() else { return 1 }
+        // 对哀凤优先使用宽度适配，没准哪天哀凤长得跟法棍面包似的也说不定。
+        var result = windowSize.width / basicWindowSize.width
+        let zoomedSize = CGSize(
+            width: basicWindowSize.width * result,
+            height: basicWindowSize.height * result
+        )
+        let compatible = CGRect(origin: .zero, size: windowSize)
+            .contains(CGRect(origin: .zero, size: zoomedSize))
+        if !compatible {
+            result = windowSize.height / basicWindowSize.height
+        }
+        return result
+    }
+
+    public static func getKeyWindowSize() -> CGSize? {
+        #if canImport(UIKit)
+        return UIApplication.shared.connectedScenes
+            .compactMap { scene -> UIWindow? in
+                (scene as? UIWindowScene)?.keyWindow
+            }
+            .first?.frame.size
+        #elseif canImport(AppKit)
+        return NSApplication.shared.keyWindow?.frame.size
+        #endif
+    }
+}
+#endif
---
+++ Packages/PZKit/Sources/PZBaseKit/SwiftUIImpl/SwiftUIImpl-Shared.swift
@@ -0,0 +1,215 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    public static func from(path: String) -> Image? {
+        guard let cgImage = CGImage.instantiate(filePath: path) else { return nil }
+        return Image(decorative: cgImage, scale: 1)
+    }
+}
+
+// MARK: - HelpTextForScrollingOnDesktopComputer
+
+public struct HelpTextForScrollingOnDesktopComputer: View {
+    // MARK: Lifecycle
+
+    public init(_ direction: Direction) {
+        self.direction = direction
+    }
+
+    // MARK: Public
+
+    public enum Direction {
+        case horizontal, vertical
+    }
+
+    public var body: some View {
+        if OS.type == .macOS {
+            let mark: String = (direction == .horizontal) ? "⇆ " : "⇅ "
+            (Text(mark) + Text("operation.scrolling.guide")).font(.footnote).opacity(0.7)
+        } else {
+            EmptyView()
+        }
+    }
+
+    // MARK: Internal
+
+    @State var direction: Direction
+}
+
+// MARK: - Trailing Text Label
+
+extension View {
+    public func corneredTag(
+        _ stringKey: LocalizedStringKey,
+        alignment: Alignment,
+        textSize: CGFloat = 12,
+        opacity: CGFloat = 1,
+        enabled: Bool = true,
+        padding: CGFloat = 0
+    )
+        -> some View {
+        guard stringKey != "", enabled else { return AnyView(self) }
+        return AnyView(
+            ZStack(alignment: alignment) {
+                self
+                Text(stringKey)
+                    .font(.system(size: textSize))
+                    .fontWidth(.condensed)
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 0.3 * textSize)
+                    .adjustedBlurMaterialBackground().clipShape(Capsule())
+                    .opacity(opacity)
+                    .padding(padding)
+                    .fixedSize()
+            }
+        )
+    }
+
+    public func corneredTag(
+        verbatim stringVerbatim: String,
+        alignment: Alignment,
+        textSize: CGFloat = 12,
+        opacity: CGFloat = 1,
+        enabled: Bool = true,
+        padding: CGFloat = 0
+    )
+        -> some View {
+        guard stringVerbatim != "", enabled else { return AnyView(self) }
+        return AnyView(
+            ZStack(alignment: alignment) {
+                self
+                Text(stringVerbatim)
+                    .font(.system(size: textSize))
+                    .fontWidth(.condensed)
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 0.3 * textSize)
+                    .adjustedBlurMaterialBackground().clipShape(Capsule())
+                    .opacity(opacity)
+                    .padding(padding)
+                    .fixedSize()
+            }
+        )
+    }
+}
+
+// MARK: - Blur Background
+
+extension View {
+    public func blurMaterialBackground() -> some View {
+        modifier(BlurMaterialBackground())
+    }
+
+    public func adjustedBlurMaterialBackground() -> some View {
+        modifier(AdjustedBlurMaterialBackground())
+    }
+}
+
+// MARK: - BlurMaterialBackground
+
+struct BlurMaterialBackground: ViewModifier {
+    public func body(content: Content) -> some View {
+        content.background(
+            .regularMaterial,
+            in: RoundedRectangle(cornerRadius: 20, style: .continuous)
+        )
+        .contentShape(RoundedRectangle(
+            cornerRadius: 20,
+            style: .continuous
+        ))
+    }
+}
+
+// MARK: - AdjustedBlurMaterialBackground
+
+struct AdjustedBlurMaterialBackground: ViewModifier {
+    // MARK: Public
+
+    @ViewBuilder
+    public func body(content: Content) -> some View {
+        Group {
+            if colorScheme == .dark {
+                content.background(
+                    .thinMaterial,
+                    in: RoundedRectangle(cornerRadius: 20, style: .continuous)
+                )
+            } else {
+                content.background(
+                    .regularMaterial,
+                    in: RoundedRectangle(cornerRadius: 20, style: .continuous)
+                )
+            }
+        }.contentShape(RoundedRectangle(
+            cornerRadius: 20,
+            style: .continuous
+        ))
+    }
+
+    // MARK: Internal
+
+    @Environment(\.colorScheme) var colorScheme
+}
+
+extension Font {
+    public static let baseFontSize: CGFloat = {
+        #if os(OSX)
+        return NSFont.systemFontSize
+        #elseif targetEnvironment(macCatalyst)
+        return UIFont.systemFontSize / 0.77
+        #elseif os(iOS)
+        return UIFont.systemFontSize
+        #elseif os(watchOS)
+        return 13
+        #else
+        return 13
+        #endif
+    }()
+}
+
+extension CGColor {
+    public var suiColor: Color {
+        .init(cgColor: self)
+    }
+}
+
+// MARK: - Divided
+
+// Ref: https://stackoverflow.com/a/75538094/4162914
+public struct Divided<Content: View>: View {
+    // MARK: Lifecycle
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    // MARK: Public
+
+    public var body: some View {
+        _VariadicView.Tree(DividedLayout()) {
+            content
+        }
+    }
+
+    // MARK: Internal
+
+    struct DividedLayout: _VariadicView_MultiViewRoot {
+        @ViewBuilder
+        public func body(children: _VariadicView.Children) -> some View {
+            let last = children.last?.id
+
+            ForEach(children) { child in
+                child
+
+                if child.id != last {
+                    Divider()
+                }
+            }
+        }
+    }
+
+    var content: Content
+}
