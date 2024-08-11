911ae41528e28064c509eb09e57c6ec17a344d0c
ShikiSuen <shikisuen@outlook.com>
2024-08-11 13:21:20 +0800

    EnkaKit // Fix ProfileSummarized.

--- Packages/PZKit/Sources/EnkaKit/EnkaKitBackend/SummarySupport/ProfileSummarized.swift
+++ Packages/PZKit/Sources/EnkaKit/EnkaKitBackend/SummarySupport/ProfileSummarized.swift
@@ -9,54 +9,46 @@ import Observation
 
 extension Enka {
     @Observable
-    public class ProfileSummarized {
+    public class ProfileSummarized<P: EKQueriedProfileProtocol> {
         // MARK: Lifecycle
 
-        public init(hsrDB: EnkaDB4HSR, rawInfo: QueriedProfileHSR) {
-            self.game = .starRail
-            self.hsrDB = hsrDB
+        public init(db theDB: DBType, rawInfo: P) {
+            self.game = theDB.game
+            self.theDB = theDB
             self.rawInfo = rawInfo
-            self.summarizedAvatars = rawInfo.summarizeAllAvatars(hsrDB: hsrDB)
+            self.summarizedAvatars = rawInfo.summarizeAllAvatars(theDB: theDB) // 肯定是一致的，不用怀疑了。
         }
 
         // MARK: Public
 
+        public typealias DBType = P.DBType
+
         public let game: Enka.GameType
-        public private(set) var hsrDB: EnkaDB4HSR
-        public private(set) var rawInfo: QueriedProfileHSR
+        public private(set) var theDB: DBType
+        public private(set) var rawInfo: P
         public private(set) var summarizedAvatars: [Enka.AvatarSummarized]
     }
 }
 
 extension Enka.ProfileSummarized {
     @MainActor
-    public func update(
-        newRawInfo: Enka.QueriedProfileHSR, dropExistingData: Bool = false
-    ) {
+    public func update(newRawInfo: P, dropExistingData: Bool = false) {
         var newInfoMutable = newRawInfo
         rawInfo = dropExistingData ? newRawInfo : newInfoMutable.merge(old: rawInfo)
-        summarizedAvatars = rawInfo.summarizeAllAvatars(hsrDB: hsrDB)
+        summarizedAvatars = rawInfo.summarizeAllAvatars(theDB: theDB)
     }
 }
 
 // MARK: - Summerizer APIs for Star Rail.
 
-extension Enka.QueriedProfileHSR {
-    public func summarizeAllAvatars(hsrDB: Enka.EnkaDB4HSR) -> [Enka.AvatarSummarized] {
-        avatarDetailList.compactMap { $0.summarize(hsrDB: hsrDB) }
+extension EKQueriedProfileProtocol {
+    public func summarizeAllAvatars(theDB: DBType) -> [Enka.AvatarSummarized] {
+        avatarDetailList.compactMap {
+            $0.summarize(theDB: theDB)
+        }
     }
 
-    public func summarize(hsrDB: Enka.EnkaDB4HSR) -> Enka.ProfileSummarized {
-        .init(hsrDB: hsrDB, rawInfo: self)
-    }
-
-    public func accountPhotoFileNameStem(hsrDB: Enka.EnkaDB4HSR) -> String {
-        let str = hsrDB.profileAvatars[headIcon.description]?
-            .icon.split(separator: "/").last?.description ?? "114514.png"
-        return "avatar_\(str)".replacingOccurrences(of: ".png", with: "")
-    }
-
-    public static var nullPhotoAssetName: String {
-        "avatar_Anonymous"
+    public func summarize(theDB: DBType) -> Enka.ProfileSummarized<Self> {
+        .init(db: theDB, rawInfo: self)
     }
 }
