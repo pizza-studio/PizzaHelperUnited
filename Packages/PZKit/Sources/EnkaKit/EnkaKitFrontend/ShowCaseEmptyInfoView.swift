e6b7401ac69ec49546a36d3a67d24ee11099c454
ShikiSuen <shikisuen@outlook.com>
2024-08-14 15:37:55 +0800

    EnkaKit // Show tutorial information if empty showcase fetched.

---
+++ Packages/PZKit/Sources/EnkaKit/EnkaKitFrontend/ShowCaseEmptyInfoView.swift
@@ -0,0 +1,22 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import SwiftUI
+
+public struct ShowCaseEmptyInfoView: View {
+    public let game: Enka.GameType
+
+    public var body: some View {
+        VStack(alignment: .leading) {
+            Text("enka.showCase.emptyShowCase.description".i18nEnka)
+                .font(.footnote)
+            switch game {
+            case .genshinImpact:
+                Image("showCaseToggle-GI", bundle: .module).resizable().aspectRatio(contentMode: .fit)
+            case .starRail:
+                Image("showCaseToggle-HSR", bundle: .module).resizable().aspectRatio(contentMode: .fit)
+            }
+        }
+    }
+}
