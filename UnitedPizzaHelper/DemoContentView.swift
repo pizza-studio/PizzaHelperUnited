eec5263211ace0a6fba79166f1f0693e3ac9f74b
ShikiSuen <shikisuen@outlook.com>
2024-08-15 16:42:21 +0800

    Xcode // Disable SwiftData demo contents.

--- UnitedPizzaHelper/DemoContentView.swift
+++ UnitedPizzaHelper/DemoContentView.swift
@@ -4,57 +4,57 @@
 
 import SwiftData
 import SwiftUI
-
-struct DemoContentView: View {
-    // MARK: Internal
-
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-            .toolbar {
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    // MARK: Private
-
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
-}
-
-#Preview {
-    DemoContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-}
+//
+// struct DemoContentView: View {
+//    // MARK: Internal
+//
+//    var body: some View {
+//        NavigationSplitView {
+//            List {
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+//                    } label: {
+//                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
+//            .toolbar {
+//                ToolbarItem {
+//                    Button(action: addItem) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//        } detail: {
+//            Text("Select an item")
+//        }
+//    }
+//
+//    // MARK: Private
+//
+//    @Environment(\.modelContext) private var modelContext
+//    @Query private var items: [Item]
+//
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(timestamp: Date())
+//            modelContext.insert(newItem)
+//        }
+//    }
+//
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            for index in offsets {
+//                modelContext.delete(items[index])
+//            }
+//        }
+//    }
+// }
+//
+// #Preview {
+//    DemoContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+// }
