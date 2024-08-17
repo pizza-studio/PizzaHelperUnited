f60db1f9b5bcbac74cde812d707a303b518f4424
ShikiSuen <shikisuen@outlook.com>
2024-08-17 20:50:40 +0800

    ProfileMgr // Add a temporary profile creation feature.

--- Packages/PZHelper/Sources/PZHelper/PZH_Features/ProfileManager/ProfileMgr_SubViews/CreateProfileSheetView.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Features/ProfileManager/ProfileMgr_SubViews/CreateProfileSheetView.swift
@@ -6,37 +6,61 @@ import PZAccountKit
 import PZBaseKit
 import SwiftUI
 
-struct CreateProfileSheetView: View {
-    // MARK: Lifecycle
+extension ProfileManagerPageContent {
+    struct CreateProfileSheetView: View {
+        // MARK: Lifecycle
 
-    init(profile: PZProfileMO, isShown: Binding<Bool>) {
-        self._isShown = isShown
-        self.profile = profile
-    }
+        init(profile: PZProfileMO, isShown: Binding<Bool>) {
+            self._isShown = isShown
+            self.profile = profile
+        }
 
-    // MARK: Internal
+        // MARK: Internal
 
-    var body: some View {
-        NavigationStack {
-            List {
-                Text(verbatim: "# Under Construction")
-            }
-            .navigationTitle("profileMgr.new".i18nPZHelper)
-            .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("sys.cancel".i18nBaseKit) {
-                        modelContext.rollback()
-                        isShown.toggle()
+        var body: some View {
+            // TODO: 现在这个 View 只是临时用的，回头整个 View 都要重写。
+            NavigationStack {
+                Form {
+                    ProfileConfigViewContents(profile: profile)
+                }.formStyle(.grouped)
+                    .navigationTitle("profileMgr.new".i18nPZHelper)
+                    .navigationBarTitleDisplayMode(.large)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button("sys.done".i18nBaseKit) {
+                                do {
+                                    withAnimation {
+                                        modelContext.insert(profile)
+                                    }
+                                    try modelContext.save()
+                                    isShown.toggle()
+                                    // try await HSRNotificationCenter.requestAuthorization() // TODO:
+                                    // WidgetCenter.shared.reloadAllTimelines() // TODO:
+                                    // globalDailyNoteCardRefreshSubject.send(()) // TODO:
+                                    alertToastEventStatus.isDoneButtonTapped.toggle()
+                                } catch {
+                                    saveProfileError = .saveDataError(error)
+                                    isSaveProfileFailAlertShown.toggle()
+                                }
+                            }.disabled(profile.isInvalid)
+                        }
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button("sys.cancel".i18nBaseKit) {
+                                modelContext.rollback()
+                                isShown.toggle()
+                            }
+                        }
                     }
-                }
             }
         }
+
+        // MARK: Private
+
+        @Binding private var isShown: Bool
+        @State private var profile: PZProfileMO
+        @State private var isSaveProfileFailAlertShown: Bool = false
+        @State private var saveProfileError: SaveProfileError?
+        @Environment(\.modelContext) private var modelContext
+        @Environment(AlertToastEventStatus.self) private var alertToastEventStatus
     }
-
-    // MARK: Private
-
-    @Binding private var isShown: Bool
-    @State private var profile: PZProfileMO
-    @Environment(\.modelContext) private var modelContext
 }
