64abe239b3328729ba758d3578141cc2750bafdd
ShikiSuen <shikisuen@outlook.com>
2024-08-17 22:06:17 +0800

    DPV // Initial Implementation with no showcases.

--- Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/2_DetailPortalTabPage.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/2_DetailPortalTabPage.swift
@@ -2,18 +2,42 @@
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
+import Defaults
 import EnkaKit
+import Observation
+import PZAccountKit
+import SFSafeSymbols
+import SwiftData
 import SwiftUI
 
 @MainActor
 struct DetailPortalTabPage: View {
+    // MARK: Lifecycle
+
+    public init() {
+        self.currentPZProfile = profiles.first
+    }
+
     // MARK: Internal
 
     var body: some View {
         NavigationStack {
             Form {
-                CaseQuerySection(theDB: sharedDB.db4GI)
-                CaseQuerySection(theDB: sharedDB.db4HSR)
+                if let currentPZProfile {
+                    switch currentPZProfile.game {
+                    case .genshinImpact:
+                        Text(verbatim: "# GI ShowCase Under Construction.")
+                        CaseQuerySection(theDB: sharedDB.db4GI)
+                    // ShowCaseListView(profile: currentPZProfile, enkaDB: sharedDB.db4GI)
+                    case .starRail:
+                        Text(verbatim: "# HSR ShowCase Under Construction.")
+                        CaseQuerySection(theDB: sharedDB.db4HSR)
+                        // ShowCaseListView(profile: currentPZProfile, enkaDB: sharedDB.db4HSR)
+                    }
+                } else {
+                    CaseQuerySection(theDB: sharedDB.db4GI)
+                    CaseQuerySection(theDB: sharedDB.db4HSR)
+                }
             }
             .formStyle(.grouped)
             .navigationTitle("tab.details.fullTitle".i18nPZHelper)
@@ -29,10 +53,63 @@ struct DetailPortalTabPage: View {
                     enkaDB: sharedDB.db4HSR
                 )
             }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    accountSwitcherMenuContent
+                }
+            }
+        }
+    }
+
+    @ViewBuilder var accountSwitcherMenuContent: some View {
+        Menu {
+            Button {
+                currentPZProfile = nil
+            } label: {
+                LabeledContent {
+                    Text("dpv.query.menuCommandTitle".i18nPZHelper)
+                        .multilineTextAlignment(.leading)
+                        .fontWidth(.condensed)
+                        .frame(maxWidth: .infinity)
+                } label: {
+                    Image(systemSymbol: .magnifyingglassCircleFill).frame(width: 48).padding(.trailing, 4)
+                }
+            }
+            .labelStyle(.titleAndIcon)
+            Divider()
+            ForEach(profiles) { enumeratedProfile in
+                Button {
+                    currentPZProfile = enumeratedProfile
+                } label: {
+                    enumeratedProfile.asAccountMenuLabel4SUI()
+                }
+                .labelStyle(.titleAndIcon)
+            }
+        } label: {
+            let dimension: CGFloat = 35
+            Group {
+                if let profile = currentPZProfile {
+                    Enka.ProfileIconView(uid: profile.uid, game: profile.game).frame(width: dimension)
+                } else {
+                    Image(systemSymbol: .magnifyingglassCircleFill).frame(width: dimension)
+                }
+            }
+            .background {
+                Circle()
+                    .strokeBorder(Color.accentColor, lineWidth: 9)
+                    .frame(width: dimension, height: dimension)
+            }
+            .frame(width: dimension, height: dimension)
+            .compositingGroup()
         }
     }
 
     // MARK: Private
 
-    @State private var sharedDB = Enka.Sputnik.shared
+    @State private var currentPZProfile: PZProfileMO?
+    @State private var sharedDB: Enka.Sputnik = .shared
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \PZProfileMO.priority) private var profiles: [PZProfileMO]
+    @Default(.queriedEnkaProfiles4GI) private var profiles4GI
+    @Default(.queriedEnkaProfiles4HSR) private var profiles4HSR
 }
