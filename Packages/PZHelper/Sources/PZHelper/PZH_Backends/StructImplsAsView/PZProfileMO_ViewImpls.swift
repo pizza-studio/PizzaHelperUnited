b588c1cd2a3057d79637f79ce27e6af1420ff890
ShikiSuen <shikisuen@outlook.com>
2024-08-17 22:00:38 +0800

    PZProfileMO // Add asAccountMenuLabel4SUI() view extension.

--- Packages/PZHelper/Sources/PZHelper/PZH_Backends/StructImplsAsView/PZProfileMO_ViewImpls.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Backends/StructImplsAsView/PZProfileMO_ViewImpls.swift
@@ -12,4 +12,22 @@ extension PZProfileMO {
     func asIcon4SUI() -> some View {
         Enka.ProfileIconView(uid: uid, game: game)
     }
+
+    @ViewBuilder @MainActor
+    func asAccountMenuLabel4SUI() -> some View {
+        LabeledContent {
+            VStack(alignment: .leading, spacing: 3) {
+                Text(name)
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.leading)
+                    .fontWidth(.condensed)
+                Text(uidWithGame).fontDesign(.monospaced)
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+            .frame(maxWidth: .infinity)
+        } label: {
+            asIcon4SUI().frame(width: 48).padding(.trailing, 4)
+        }
+    }
 }
