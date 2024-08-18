be4397ce819e699c70faf07f78d8345b5c4695bf
ShikiSuen <shikisuen@outlook.com>
2024-08-18 18:12:38 +0800

    PZBaseKit // Add InfiniteProgressBar.

---
+++ Packages/PZKit/Sources/PZBaseKit/OSImpl/SwiftUIImpl/InfiniteProgressBar.swift
@@ -0,0 +1,49 @@
+// Ref: https://swiftuirecipes.com/blog/material-indefinite-loading-bar-in-swiftui
+
+import Foundation
+import SwiftUI
+
+// MARK: - InfiniteProgressBar
+
+public struct InfiniteProgressBar: View {
+    // MARK: Lifecycle
+
+    public init() {}
+
+    // MARK: Public
+
+    public var body: some View {
+        Rectangle()
+            .foregroundColor(.gray) // change the color as you see fit
+            .frame(height: Self.height)
+            .overlay(GeometryReader { geo in
+                overlayRect(in: geo.frame(in: .local))
+            })
+            .clipped()
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+
+    // MARK: Private
+
+    // the height of the bar
+    private static let height: CGFloat = 4
+    // how much does the blue part cover the gray part (40%)
+    private static let coverPercentage: CGFloat = 0.4
+    private static let minOffset: CGFloat = -2
+    private static let maxOffset = 1 / coverPercentage * abs(minOffset)
+
+    @State private var offset = Self.minOffset
+
+    private func overlayRect(in rect: CGRect) -> some View {
+        let width = rect.width * Self.coverPercentage
+        return Rectangle()
+            .foregroundColor(.blue)
+            .frame(width: width)
+            .offset(x: width * offset)
+            .onAppear {
+                withAnimation(Animation.linear(duration: 2.5).repeatForever(autoreverses: false)) {
+                    offset = Self.maxOffset
+                }
+            }
+    }
+}
