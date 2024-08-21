fc2e298684b21e0ee8cb8ef9547c0cba9e2431f2
ShikiSuen <shikisuen@outlook.com>
2024-08-21 19:45:12 +0800

    WallpaperKit // Initial implementation (HSR only).

---
+++ Packages/WallpaperKit/Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WallpaperKit",
+    platforms: [.iOS(.v17), .macOS(.v14), .watchOS(.v10), .macCatalyst(.v17), .visionOS(.v1)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "WallpaperKit",
+            targets: ["WallpaperKit"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../PZKit"),
+        .package(
+            url: "https://github.com/sindresorhus/Defaults", .upToNextMajor(from: "8.2.0")
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "WallpaperKit",
+            dependencies: [
+                .product(name: "Defaults", package: "Defaults"),
+                .product(name: "PZBaseKit", package: "PZKit"),
+            ],
+            resources: [
+                .process("Resources/"),
+            ]
+        ),
+        .testTarget(
+            name: "WallpaperKitTests",
+            dependencies: ["WallpaperKit"]
+        ),
+    ]
+)
