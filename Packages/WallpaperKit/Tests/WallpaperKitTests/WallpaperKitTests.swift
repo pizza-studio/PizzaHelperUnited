678f53d4d88ee7aed59e9e11e12961db729e4367
ShikiSuen <shikisuen@outlook.com>
2024-08-22 02:29:15 +0800

    WallpaperKit // Add support for Genshin Impact.

--- Packages/WallpaperKit/Tests/WallpaperKitTests/WallpaperKitTests.swift
+++ Packages/WallpaperKit/Tests/WallpaperKitTests/WallpaperKitTests.swift
@@ -3,6 +3,14 @@ import XCTest
 
 final class WallpaperKitTests: XCTestCase {
     func testAssetMetaAccessibility() throws {
-        XCTAssert(!Wallpaper.allCases4HSR.isEmpty)
+        for theCase in Wallpaper.allCases(for: .genshinImpact) {
+            print(theCase)
+        }
+        for theCase in Wallpaper.allCases(for: .starRail) {
+            print(theCase)
+        }
+        print("------------------")
+        print(Wallpaper.defaultValue(for: .genshinImpact))
+        print(Wallpaper.defaultValue(for: .starRail))
     }
 }
