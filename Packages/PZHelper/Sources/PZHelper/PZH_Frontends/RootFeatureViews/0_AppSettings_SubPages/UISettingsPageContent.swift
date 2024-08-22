51674903f69f58fe3479874f16dbdbbd6a22ea2c
ShikiSuen <shikisuen@outlook.com>
2024-08-22 21:09:57 +0800

    DisplaySettings // Add .restoreTabOnLaunching().

--- Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/0_AppSettings_SubPages/UISettingsPageContent.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/0_AppSettings_SubPages/UISettingsPageContent.swift
@@ -16,6 +16,9 @@ struct UISettingsPageContent: View {
     var body: some View {
         Form {
             Section {
+                Toggle(isOn: $restoreTabOnLaunching) {
+                    Text("setting.display.restoreTabOnLaunching".i18nPZHelper)
+                }
                 AppWallpaperSettingsPicker()
             } header: {
                 Text("settings.display.generalSettings.sectionHeader".i18nPZHelper)
@@ -30,5 +33,6 @@ struct UISettingsPageContent: View {
     // MARK: Private
 
     @State private var sharedDB = Enka.Sputnik.shared
+    @Default(.restoreTabOnLaunching) private var restoreTabOnLaunching: Bool
     @Default(.background4App) private var background4App: Wallpaper
 }
