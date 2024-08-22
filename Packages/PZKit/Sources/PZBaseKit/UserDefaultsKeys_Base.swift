c422d6fb9de6efd5530ca9b4a4b74687773e9a40
ShikiSuen <shikisuen@outlook.com>
2024-08-22 10:55:58 +0800

    Repo // Move certain UserDefaults keys from EnkaKit to PZBaseKit.

--- Packages/PZKit/Sources/PZBaseKit/UserDefaultsKeys_Base.swift
+++ Packages/PZKit/Sources/PZBaseKit/UserDefaultsKeys_Base.swift
@@ -12,9 +12,45 @@ extension UserDefaults {
 
 extension Defaults.Keys {
     /// App UI language. At least, this works with macOS. This must use the standard container.
-    public static let appLanguage = Key<[String]?>(AppLanguage.defaultsKeyName, default: nil, suite: .standard)
+    public static let appLanguage = Key<[String]?>(
+        AppLanguage.defaultsKeyName,
+        default: nil,
+        suite: .standard
+    )
+
     /// Remembering the most-recent tab index.
-    public static let appTabIndex = Key<Int>("appTabIndex", default: 0, suite: .baseSuite)
+    public static let appTabIndex = Key<Int>(
+        "appTabIndex",
+        default: 0,
+        suite: .baseSuite
+    )
+
     /// Remembering the most-recent tab index.
-    public static let restoreTabOnLaunching = Key<Bool>("restoreTabOnLaunching", default: true, suite: .baseSuite)
+    public static let restoreTabOnLaunching = Key<Bool>(
+        "restoreTabOnLaunching",
+        default: true,
+        suite: .baseSuite
+    )
+
+    /// User-Specified Wanderer's name.
+    public static let customizedNameForWanderer = Key<String>(
+        "customizedNameForWanderer",
+        default: .init(),
+        suite: .baseSuite
+    )
+
+    /// 是否强制修复指定语言下的某些角色跟物品的名称用字。
+    /// 该选项仅对中文介面可见。
+    public static let forceCharacterWeaponNameFixed = Key<Bool>(
+        "forceCharacterWeaponNameFixed",
+        default: true,
+        suite: .baseSuite
+    )
+
+    /// Whether displaying real names for certain characters, not affecting SRGF imports & exports.
+    public static let useRealCharacterNames = Key<Bool>(
+        "useRealCharacterNames",
+        default: true,
+        suite: .baseSuite
+    )
 }
