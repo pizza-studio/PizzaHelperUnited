44292bd2dadaf09b9e995acf4ca5b42508e8b7cc
ShikiSuen <shikisuen@outlook.com>
2024-08-26 21:27:08 +0800

    MiHoYoAPIError // Add some new cases.

--- Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/Common/HoYo_Models/APIError.swift
+++ Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/Common/HoYo_Models/APIError.swift
@@ -9,6 +9,8 @@ public enum MiHoYoAPIError: Error, LocalizedError {
     case fingerPrintInvalidOrMissing
     case sTokenV2InvalidOrMissing
     case reloginRequired
+    case serverUnderMaintenanceUpgrade
+    case insufficientDataVisibility
     case other(retcode: Int, message: String)
 
     // MARK: Lifecycle
@@ -16,8 +18,10 @@ public enum MiHoYoAPIError: Error, LocalizedError {
     public init(retcode: Int, message: String) {
         self = switch retcode {
         case 1034, 10035: .verificationNeeded
-        case 5003: .fingerPrintInvalidOrMissing
+        case 5003, 10041: .fingerPrintInvalidOrMissing
+        case 10102: .insufficientDataVisibility
         case 10001: .reloginRequired
+        case 10307: .serverUnderMaintenanceUpgrade
         default: .other(retcode: retcode, message: message)
         }
     }
@@ -32,8 +36,9 @@ public enum MiHoYoAPIError: Error, LocalizedError {
         case .fingerPrintInvalidOrMissing: "MiHoYoAPIError.fingerPrintInvalidOrMissing".i18nAK
         case .sTokenV2InvalidOrMissing: "MiHoYoAPIError.sTokenV2InvalidOrMissing".i18nAK
         case .reloginRequired: "MiHoYoAPIError.reloginRequired".i18nAK
-        case let .other(retcode, message):
-            "[HoYoAPIErr] Ret: \(retcode); Msg: \(message)"
+        case .serverUnderMaintenanceUpgrade: "MiHoYoAPIError.serverUnderMaintenanceUpgrade".i18nAK
+        case .insufficientDataVisibility: "MiHoYoAPIError.insufficientDataVisibility".i18nAK
+        case let .other(retcode, message): "[HoYoAPIErr] Ret: \(retcode); Msg: \(message)"
         }
     }
 
