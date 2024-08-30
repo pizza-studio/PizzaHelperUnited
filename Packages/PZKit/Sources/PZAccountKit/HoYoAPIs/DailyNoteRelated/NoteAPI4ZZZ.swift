002824c7978a5845c6dfe742f577e825cb3342b4
ShikiSuen <shikisuen@outlook.com>
2024-08-30 17:15:32 +0800

    PZAccountKit // Add daily note support for ZZZ.

---
+++ Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/NoteAPI4ZZZ.swift
@@ -0,0 +1,58 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import PZBaseKit
+
+extension HoYo {
+    public static func note4ZZZ(profile: PZProfileMO) async throws -> Note4ZZZ {
+        try await getNote4ZZZ(
+            server: profile.server,
+            uid: profile.uid,
+            cookie: profile.cookie,
+            deviceFingerPrint: profile.deviceFingerPrint,
+            deviceID: profile.deviceID
+        )
+    }
+
+    static func getNote4ZZZ(
+        server: Server,
+        uid: String,
+        cookie: String,
+        deviceFingerPrint: String?,
+        deviceID: String?
+    ) async throws
+        -> Note4ZZZ {
+        let queryItems: [URLQueryItem] = [
+            .init(name: "role_id", value: uid),
+            .init(name: "server", value: server.rawValue),
+        ]
+        var additionalHeaders = [
+            "User-Agent": "WidgetExtension/434 CFNetwork/1492.0.1 Darwin/23.3.0",
+        ]
+
+        if let deviceFingerPrint, !deviceFingerPrint.isEmpty, let deviceID {
+            additionalHeaders.updateValue(deviceFingerPrint, forKey: "x-rpc-device_fp")
+            additionalHeaders.updateValue(deviceID, forKey: "x-rpc-device_id")
+        }
+
+        let host: String = switch server.region {
+        case .miyoushe: "api-takumi-record.mihoyo.com"
+        case .hoyoLab: "sg-act-nap-api.hoyolab.com"
+        }
+
+        let request = try await Self.generateRequest(
+            region: server.region,
+            host: host,
+            path: "/event/game_record_zzz/api/zzz/note",
+            queryItems: queryItems,
+            cookie: cookie,
+            additionalHeaders: additionalHeaders
+        )
+
+        let (data, _) = try await URLSession.shared.data(for: request)
+
+        return try await .decodeFromMiHoYoAPIJSONResult(data: data, with: request)
+    }
+}
