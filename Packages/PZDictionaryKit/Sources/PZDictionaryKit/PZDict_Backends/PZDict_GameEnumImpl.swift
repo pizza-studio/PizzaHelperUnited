ad8e49621b1ea93586a3347e65974b36caf65cfc
ShikiSuen <shikisuen@outlook.com>
2024-08-31 18:25:05 +0800

    PZDictionaryKit // Initial implementation.
    Co-Authored-By: Lava <34743145+CanglongCl@users.noreply.github.com>

---
+++ Packages/PZDictionaryKit/Sources/PZDictionaryKit/PZDict_Backends/PZDict_GameEnumImpl.swift
@@ -0,0 +1,39 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import PZBaseKit
+
+extension Pizza.SupportedGame {
+    fileprivate var dictionaryServerHost: String {
+        switch self {
+        case .genshinImpact: "gidict-api.pizzastudio.org"
+        case .starRail: "hsrdict-api.pizzastudio.org"
+        case .zenlessZone: "zzzdict-api.pizzastudio.org"
+        }
+    }
+
+    var dictURL: URL {
+        switch self {
+        case .genshinImpact: "https://gidict.pizzastudio.org/".asURL
+        case .starRail: "https://hsrdict.pizzastudio.org/".asURL
+        case .zenlessZone: "https://zzzdict.pizzastudio.org/".asURL
+        }
+    }
+}
+
+extension Pizza.SupportedGame {
+    // This function is Package-Domestic.
+    func translate(query: String, page: Int, pageSize: Int) async throws -> TranslationResult {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = dictionaryServerHost
+        components.path = "/v1/translations/\(query)"
+        components.queryItems = [.init(name: "page", value: "\(page)"), .init(name: "page_size", value: "\(pageSize)")]
+        let url = components.url!
+        let request = URLRequest(url: url)
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONDecoder().decode(TranslationResult.self, from: data)
+    }
+}
