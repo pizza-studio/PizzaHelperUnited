35f1dc0c755c01bd5af66a4bd029c823aa2be92f
ShikiSuen <shikisuen@outlook.com>
2024-08-31 22:29:19 +0800

    AppSettings // Revamp the app language chooser.

--- Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/0_AppSettingsTabPage.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/0_AppSettingsTabPage.swift
@@ -31,36 +31,7 @@ struct AppSettingsTabPage: View {
                 }
 
                 Section {
-                    #if !targetEnvironment(macCatalyst) && os(iOS)
-                    Button {
-                        UIApplication.shared.open(UIApplication.openSettingsURLString.asURL)
-                    } label: {
-                        Label {
-                            Text("settings.appLanguage.title".i18nPZHelper)
-                                .foregroundColor(.primary)
-                        } icon: {
-                            Image(systemSymbol: .globe)
-                        }
-                    }
-                    #elseif canImport(UIKit, _version: 18.0)
-                    /// 這個語言選擇器用 Xcode 15 建置的話是無效的。
-                    VStack {
-                        Picker(selection: $appLanguage) {
-                            Text("app.language.followSystemDefault".i18nBaseKit).tag([String]?.none)
-                            ForEach(AppLanguage.allCases) { appLang in
-                                Text(appLang.localizedDescription).tag(appLang.savedValue)
-                            }
-                        } label: {
-                            HStack {
-                                Image(systemSymbol: .globe)
-                                Text("settings.appLanguage.title".i18nPZHelper)
-                            }
-                        }
-                        Text("settings.disclaimer.requiringAppRebootToApplySettings".i18nPZHelper)
-                            .font(.footnote).foregroundStyle(.secondary)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                    #endif
+                    AppLanguageSwitcher()
                     NavigationLink(value: Nav.uiSettings) {
                         Label("settings.uiSettings.title".i18nPZHelper, systemSymbol: .pc)
                     }
