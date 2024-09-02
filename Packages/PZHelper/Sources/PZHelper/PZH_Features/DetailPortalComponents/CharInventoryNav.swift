7fd70f4fbe05bae592db3aef67ff970a91d5d5d4
ShikiSuen <shikisuen@outlook.com>
2024-09-03 03:45:11 +0800

    CharInventoryNav // Fix compilation issue with Xcode 15.

--- Packages/PZHelper/Sources/PZHelper/PZH_Features/DetailPortalComponents/CharInventoryNav.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Features/DetailPortalComponents/CharInventoryNav.swift
@@ -68,8 +68,14 @@ public struct CharInventoryNav: View {
                 if data.avatars.isEmpty {
                     Text("dpv.characterInventory.notice.EmptyInventoryResult".i18nPZHelper).font(.caption)
                 } else {
-                    NavigationLink(destination: AnyView(data.asView(isMiyousheUID: false))) {
-                        thisLabel
+                    if let data = data as? HoYo.CharInventory4GI {
+                        NavigationLink(destination: data.asView(isMiyousheUID: false)) {
+                            thisLabel
+                        }
+                    } else if let data = data as? HoYo.CharInventory4HSR {
+                        NavigationLink(destination: data.asView(isMiyousheUID: false)) {
+                            thisLabel
+                        }
                     }
                 }
             }
