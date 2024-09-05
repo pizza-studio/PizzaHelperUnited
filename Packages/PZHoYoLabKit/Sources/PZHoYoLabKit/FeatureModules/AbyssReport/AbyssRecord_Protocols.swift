1b0f989ffea7868a5e14e762bcf8e509c2c27421
ShikiSuen <shikisuen@outlook.com>
2024-09-06 01:33:24 +0800

    PZHoYoLabKit // Spiral Abyss Records - Phase 1.

---
+++ Packages/PZHoYoLabKit/Sources/PZHoYoLabKit/FeatureModules/AbyssReport/AbyssRecord_Protocols.swift
@@ -0,0 +1,83 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import PZAccountKit
+import SwiftUI
+
+// MARK: - AbyssReportSet
+
+public protocol AbyssReportSet: Codable, Hashable, Sendable {
+    associatedtype Report: AbyssReport
+    var current: Report { get }
+    var previous: Report? { get set }
+    var costumeMap: [String: String] { get set }
+}
+
+// MARK: - AbyssReport
+
+public protocol AbyssReport: Codable, Hashable, Sendable, DecodableFromMiHoYoAPIJSONResult {
+    associatedtype ViewType: AbyssReportView where Self == ViewType.AbyssReportData
+}
+
+extension AbyssReport {
+    @MainActor @ViewBuilder
+    public func asView() -> some View {
+        ViewType(data: self)
+    }
+}
+
+// MARK: - AbyssReportView
+
+@MainActor
+public protocol AbyssReportView: View {
+    associatedtype AbyssReportData: AbyssReport where Self == AbyssReportData.ViewType
+    init(data: AbyssReportData)
+    var data: AbyssReportData { get }
+    static var navTitle: String { get }
+    @ViewBuilder @MainActor var body: Self.Body { get }
+}
+
+// MARK: - Debug
+
+import PZBaseKit
+
+extension AbyssReportView {
+    @MainActor @ViewBuilder public var debugBody: some View {
+        VStack {
+            Text(verbatim: "Abyss Report Loaded Successfully.")
+            Button {
+                copyEncoded()
+            } label: {
+                Text(verbatim: "Dump JSON to clipboard.")
+            }
+            .buttonStyle(.bordered)
+            ScrollView {
+                ZStack(alignment: .topLeading) {
+                    Color.primary.colorInvert()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    Text(textRaw)
+                        .multilineTextAlignment(.leading)
+                        .textSelection(.enabled)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+
+    var textRaw: String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.prettyPrinted, .sortedKeys, .withoutEscapingSlashes]
+        guard let data = try? encoder.encode(data) else { return "" }
+        guard let dataText = String(data: data, encoding: .utf8) else { return "" }
+        return dataText
+    }
+
+    public func copyEncoded() {
+        guard !textRaw.isEmpty else { return }
+        Clipboard.writeString(textRaw)
+    }
+}
---
+++ Packages/PZHoYoLabKit/Sources/PZHoYoLabKit/FeatureModules/AbyssReport/HoYoAPIImpl/AbyssReportAPI.swift
@@ -0,0 +1,167 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import PZAccountKit
+import PZBaseKit
+
+extension HoYo {
+    public static func getAbyssReportSet(for profile: PZProfileMO) async throws -> (any AbyssReportSet)? {
+        switch profile.game {
+        case .genshinImpact:
+            let current = try await abyssReportData4GI(for: profile, isPreviousRound: false)
+            let previous = try? await abyssReportData4GI(for: profile, isPreviousRound: true)
+            return AbyssReportSet4GI(current: current, previous: previous)
+        case .starRail:
+            let current = try await abyssReportData4HSR(for: profile, isPreviousRound: false)
+            let previous = try? await abyssReportData4HSR(for: profile, isPreviousRound: true)
+            return AbyssReportSet4HSR(current: current, previous: previous)
+        default: return nil
+        }
+    }
+}
+
+extension HoYo {
+    fileprivate static func abyssReportData4GI(
+        for profile: PZProfileMO, isPreviousRound: Bool = false
+    ) async throws
+        -> AbyssReport4GI {
+        try await abyssReportData4GI(
+            isPreviousRound: isPreviousRound,
+            server: profile.server,
+            uid: profile.uid,
+            cookie: profile.cookie,
+            deviceFingerPrint: profile.deviceFingerPrint,
+            deviceID: profile.deviceID
+        )
+    }
+
+    fileprivate static func abyssReportData4HSR(
+        for profile: PZProfileMO, isPreviousRound: Bool = false
+    ) async throws
+        -> AbyssReport4HSR {
+        try await abyssReportData4HSR(
+            isPreviousRound: isPreviousRound,
+            server: profile.server,
+            uid: profile.uid,
+            cookie: profile.cookie,
+            deviceFingerPrint: profile.deviceFingerPrint,
+            deviceID: profile.deviceID
+        )
+    }
+}
+
+extension HoYo {
+    fileprivate static func abyssReportData4GI(
+        isPreviousRound: Bool = false,
+        server: Server,
+        uid: String,
+        cookie: String,
+        deviceFingerPrint: String?,
+        deviceID: String?
+    ) async throws
+        -> AbyssReport4GI {
+        #if DEBUG
+        print("||| START REQUESTING SPIRAL ABYSS DATA (GI) |||")
+        #endif
+        let queryItems: [URLQueryItem] = [
+            .init(name: "role_id", value: uid),
+            .init(name: "schedule_type", value: isPreviousRound ? "2" : "1"),
+            .init(name: "server", value: server.rawValue),
+        ]
+
+        let additionalHeaders: [String: String]? = {
+            if let deviceFingerPrint, !deviceFingerPrint.isEmpty, let deviceID {
+                return [
+                    "x-rpc-device_fp": deviceFingerPrint,
+                    "x-rpc-device_id": deviceID,
+                ]
+            } else {
+                return nil
+            }
+        }()
+
+        let request = try await Self.generateRecordAPIRequest(
+            region: server.region,
+            path: server.region.abyssReportRetrievalPath,
+            queryItems: queryItems,
+            cookie: cookie,
+            additionalHeaders: additionalHeaders
+        )
+        request.printDebugIntelIfDebugMode()
+
+        let (data, _) = try await URLSession.shared.data(for: request)
+
+        return try .decodeFromMiHoYoAPIJSONResult(data: data)
+    }
+
+    fileprivate static func abyssReportData4HSR(
+        isPreviousRound: Bool = false,
+        server: Server,
+        uid: String,
+        cookie: String,
+        deviceFingerPrint: String?,
+        deviceID: String?
+    ) async throws
+        -> AbyssReport4HSR {
+        #if DEBUG
+        print("||| START REQUESTING SPIRAL ABYSS DATA (HSR) |||")
+        #endif
+
+        let queryItems: [URLQueryItem] = [
+            .init(name: "isPrev", value: isPreviousRound ? "true" : "false"),
+            .init(name: "need_all", value: "true"),
+            .init(name: "role_id", value: uid),
+            .init(name: "schedule_type", value: isPreviousRound ? "2" : "1"),
+            .init(name: "server", value: server.rawValue),
+        ]
+
+        var additionalHeaders: [String: String]? = {
+            if let deviceFingerPrint, !deviceFingerPrint.isEmpty, let deviceID {
+                return [
+                    "x-rpc-device_fp": deviceFingerPrint,
+                    "x-rpc-device_id": deviceID,
+                ]
+            } else {
+                return [:]
+            }
+        }()
+
+        checkMiyoushe: switch server.region {
+        case .miyoushe: additionalHeaders?["x-rpc-language"] = HoYo.APILang.langCHS.rawValue
+        default: break checkMiyoushe
+        }
+
+        if additionalHeaders?.isEmpty ?? false {
+            additionalHeaders = nil
+        }
+
+        let request = try await Self.generateRecordAPIRequest(
+            region: server.region,
+            path: server.region.abyssReportRetrievalPath,
+            queryItems: queryItems,
+            cookie: cookie,
+            additionalHeaders: additionalHeaders
+        )
+        request.printDebugIntelIfDebugMode()
+
+        let (data, _) = try await URLSession.shared.data(for: request)
+
+        return try .decodeFromMiHoYoAPIJSONResult(data: data)
+    }
+}
+
+extension HoYo.AccountRegion {
+    /// 除绝区零情况未知以外，原神与星穹铁道搭配 `.recordURLAPIHost()` 使用。
+    public var abyssReportRetrievalPath: String {
+        switch (self, game) {
+        case (.hoyoLab, .genshinImpact): "/game_record/app/genshin/api/spiralAbyss"
+        case (.miyoushe, .genshinImpact): "/game_record/app/genshin/api/spiralAbyss"
+        case (.hoyoLab, .starRail): "/game_record/app/hkrpg/api/challenge"
+        case (.miyoushe, .starRail): "/game_record/app/hkrpg/api/challenge"
+        case (.hoyoLab, .zenlessZone): "" // 暂不实作。
+        case (.miyoushe, .zenlessZone): "" // 暂不实作。
+        }
+    }
+}
