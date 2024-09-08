756873fc45e07e433cc42cef4e548223e50d4ff3
ShikiSuen <shikisuen@outlook.com>
2024-09-08 18:18:41 +0800

    Repo // License header fixes.

--- Packages/PZHelper-Watch/Sources/PZHelper-Watch/PZHelper_Watch.swift
+++ Packages/PZHelper-Watch/Sources/PZHelper-Watch/PZHelper_Watch.swift
@@ -1,2 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
