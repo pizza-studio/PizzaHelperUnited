915f1e14d46deb227115bbbc3f6f1be83c42eac5
ShikiSuen <shikisuen@outlook.com>
2024-09-08 18:17:31 +0800

    GITodayMaterialsKit // Initial Preparation.

---
+++ Packages/GITodayMaterialsKit/Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GITodayMaterialsKit",
+    platforms: [.iOS(.v17), .macOS(.v14), .macCatalyst(.v17), .visionOS(.v1)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "GITodayMaterialsKit",
+            targets: ["GITodayMaterialsKit"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../PZKit"),
+        .package(path: "../EnkaKit"),
+        .package(path: "../WallpaperKit"),
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .upToNextMajor(from: "5.3.0")),
+        .package(url: "https://github.com/sindresorhus/Defaults", .upToNextMajor(from: "8.2.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "GITodayMaterialsKit",
+            dependencies: [
+                .product(name: "PZBaseKit", package: "PZKit"),
+                .product(name: "PZAccountKit", package: "PZKit"),
+                .product(name: "EnkaKit", package: "EnkaKit"),
+                .product(name: "WallpaperKit", package: "WallpaperKit"),
+                .product(name: "SFSafeSymbols", package: "SFSafeSymbols"),
+                .product(name: "Defaults", package: "Defaults"),
+            ]
+        ),
+        .testTarget(
+            name: "GITodayMaterialsKitTests",
+            dependencies: ["GITodayMaterialsKit"]
+        ),
+    ]
+)
---
+++ Packages/GITodayMaterialsKit/Sources/GITodayMaterialsKit/GITodayMaterialKit.swift
@@ -0,0 +1,28 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+#if canImport(UIKit)
+import UIKit
+#endif
+#if canImport(AppKit)
+import AppKit
+#endif
+
+extension GITodayMaterial {
+    public static let bundledData: [Self] = {
+        let bundledRawData = NSDataAsset(name: "BundledGIDailyMaterialsData", bundle: .module)!
+        return try! JSONDecoder().decode([Self].self, from: bundledRawData.data)
+    }()
+}
+
+extension String {
+    public var i18nTodayMaterials: String {
+        NSLocalizedString(self, bundle: Bundle.module, comment: "")
+    }
+
+    public var i18nTodayMaterialNames: String {
+        NSLocalizedString(self, tableName: "MaterialNames", bundle: Bundle.module, comment: "")
+    }
+}
