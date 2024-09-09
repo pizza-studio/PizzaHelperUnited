256998dc73f049134b9689c22093209abdcc498a
ShikiSuen <shikisuen@outlook.com>
2024-09-09 21:14:16 +0800

    CIV // Amend the previous edit to fix the incorrect userdefault key.

--- Packages/EnkaKit/Sources/EnkaKit/EnkaKitFrontend/CharacterIconViews/CharacterIconView.swift
+++ Packages/EnkaKit/Sources/EnkaKit/EnkaKitFrontend/CharacterIconViews/CharacterIconView.swift
@@ -186,7 +186,7 @@ public struct CharacterIconView: View {
     // Note: Due to the mission-critical fact in this context, we use @AppStorage in lieu of @Default.
     // @Default(.useGenshinStyleCharacterPhotos) private var useGenshinStyleIcon: Bool
     // @Default(.useNameCardBGWithGICharacters) private var useNameCardBGWithGICharacters: Bool
-    @AppStorage("useGenshinStyleIcon", store: .baseSuite) private var useGenshinStyleIcon: Bool = true
+    @AppStorage("useGenshinStyleCharacterPhotos", store: .baseSuite) private var useGenshinStyleIcon: Bool = true
     @AppStorage(
         "useNameCardBGWithGICharacters",
         store: .baseSuite
