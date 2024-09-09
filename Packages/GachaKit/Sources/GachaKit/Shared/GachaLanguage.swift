986e680ea888a15bf7924d0ba7e7f472f1ae73ce
ShikiSuen <shikisuen@outlook.com>
2024-09-10 02:51:55 +0800

    PZAccountKit // Extend HoYo.APILanguage to GachaLanguage.

---
+++ Packages/GachaKit/Sources/GachaKit/Shared/GachaLanguage.swift
@@ -0,0 +1,13 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import PZAccountKit
+
+public typealias GachaLanguage = HoYo.APILang
+
+extension Locale {
+    /// Get the language code used for gacha API according to current preferred localization.
+    static var gachaLangauge: GachaLanguage { .current }
+}
