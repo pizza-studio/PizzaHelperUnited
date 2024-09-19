6d348043496e42c9ce0f040e82bd7a84ab8c0282
ShikiSuen <shikisuen@outlook.com>
2024-09-19 21:38:37 +0800

    GachaKit // Add GachaProfileDetailedListView() & GachaEntryBar().

---
+++ Packages/GachaKit/Sources/GachaKit/Frontends/CommonComponents/GachaEntryBar.swift
@@ -0,0 +1,86 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Defaults
+import SwiftUI
+
+public struct GachaEntryBar: View {
+    // MARK: Lifecycle
+
+    public init(entry: GachaEntryExpressible) {
+        self.entry = entry
+        self.drawCount = nil
+        self.showDate = nil
+    }
+
+    public init(entry: GachaEntryExpressible, drawCount: Int, showDate: Bool) {
+        self.entry = entry
+        self.drawCount = drawCount
+        self.showDate = showDate
+    }
+
+    // MARK: Public
+
+    @MainActor public var body: some View {
+        HStack {
+            HStack {
+                entry.icon(35)
+                HStack {
+                    Text(entry.nameLocalized(realName: useRealCharacterNames))
+                        .fontWeight(.medium)
+                        .fontWidth(.condensed)
+                    itemIDText
+                        .fontWidth(.condensed)
+                    Spacer()
+
+                    VStack(alignment: .trailing) {
+                        if let drawCount, entry.rarity != .rank3 {
+                            Text(drawCount.description)
+                                .font((showDate ?? false) ? .caption2 : .body)
+                        }
+                        if showDate ?? false {
+                            Text(Self.dateFormatter.string(from: entry.time))
+                                .foregroundColor(.secondary)
+                                .font(.caption2)
+                        }
+                    }
+                }
+            }
+            #if DEBUG
+            .contextMenu {
+                    Text(verbatim: "Debug Info")
+                    Divider()
+                    Text(entry.itemType.getTranslatedRaw(game: entry.game))
+                    Text(entry.pool.localizedTitle)
+                }
+            #endif
+        }
+    }
+
+    // MARK: Internal
+
+    let entry: GachaEntryExpressible
+    let drawCount: Int?
+    let showDate: Bool?
+
+    @MainActor var itemIDText: Text {
+        if showDate ?? false {
+            Text(verbatim: "\(entry.itemID)")
+                .font(.caption).foregroundColor(.secondary)
+        } else {
+            Text(verbatim: "")
+        }
+    }
+
+    // MARK: Private
+
+    private static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        return dateFormatter
+    }()
+
+    @Default(.useRealCharacterNames) private var useRealCharacterNames: Bool
+}
