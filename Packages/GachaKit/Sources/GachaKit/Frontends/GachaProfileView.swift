932890cac98d88fc4e8ee7c35b9fccb49745ab88
ShikiSuen <shikisuen@outlook.com>
2024-09-19 14:48:57 +0800

    GachaKit // Add GachaStatsSection(), etc.
    - Use GachaVM to share common variables.
    - Also cache fetched entries in the VM.

--- Packages/GachaKit/Sources/GachaKit/Frontends/GachaProfileView.swift
+++ Packages/GachaKit/Sources/GachaKit/Frontends/GachaProfileView.swift
@@ -15,42 +15,33 @@ import SwiftUI
 public struct GachaProfileView: View {
     // MARK: Lifecycle
 
-    public init(givenGPID: GachaProfileID) {
-        self.givenGPID = givenGPID
-        switch givenGPID.game {
-        case .genshinImpact: self.poolType = .giCharacterEventWish
-        case .starRail: self.poolType = .srCharacterEventWarp
-        case .zenlessZone: self.poolType = .zzExclusiveChannel
-        }
-        _entries = Query(
-            filter: PZGachaEntryMO.predicate(owner: GachaVM.shared.currentGachaProfile),
-            sort: [SortDescriptor(\PZGachaEntryMO.id, order: .reverse)],
-            animation: .default
-        )
-    }
+    public init() {}
 
     // MARK: Public
 
     @MainActor public var body: some View {
-        GachaStatisticSectionView(gpid: givenGPID, gachaType: poolType)
-//        ForEach(entries) { entry in
-//            let expressible = GachaItemExpressible(rawEntry: entry)
-//            LabeledContent {
-//                Text(expressible.nameLocalized())
-//                    .frame(maxWidth: .infinity, alignment: .leading)
-//            } label: {
-//                expressible.icon(30)
-//            }
-//        }
+        GachaStatsSection(
+            gpid: theVM.currentGachaProfile?.asSendable,
+            gachaType: theVM.currentPoolType
+        )
+        ForEach(theVM.cachedEntries) { entry in
+            Label {
+                HStack {
+                    Text(entry.nameLocalized())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                    Text(String(repeating: "★", count: entry.rarity.rawValue))
+                }
+            } icon: {
+                entry.icon(30)
+            }
+        }
     }
 
     // MARK: Fileprivate
 
-    fileprivate let givenGPID: GachaProfileID
     @State fileprivate var enkaDB = Enka.Sputnik.shared
     @State fileprivate var metaDB = GachaMeta.sharedDB
-    @State fileprivate var poolType: GachaPoolExpressible
-    @Query fileprivate var entries: [PZGachaEntryMO]
     @Environment(\.modelContext) fileprivate var modelContext
     @Environment(GachaVM.self) fileprivate var theVM
 }
