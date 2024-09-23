9b8069f6925e5a36365cb2f1ef15b1987cc83d7c
ShikiSuen <shikisuen@outlook.com>
2024-09-23 21:34:14 +0800

    Tools // Add a disclaimer text for the Gacha Record Manager.

--- Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/3_UtilsTabPage.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootFeatureViews/3_UtilsTabPage.swift
@@ -39,6 +39,9 @@ struct UtilsTabPage: View {
                             GachaRootView.navIcon.resizable().aspectRatio(contentMode: .fit)
                         }
                     }
+                } footer: {
+                    Text(GachaRootView.navDescription)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
 
                 Section {
