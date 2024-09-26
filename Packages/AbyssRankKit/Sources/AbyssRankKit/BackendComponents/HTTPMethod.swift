ae473fe05d7a9fa73fc7f9916b6de58632080dce
ShikiSuen <shikisuen@outlook.com>
2024-09-26 12:55:22 +0800

    AbyssRankKit // Refactor HTTP methods.

--- Packages/AbyssRankKit/Sources/AbyssRankKit/BackendComponents/HTTPMethod.swift
+++ Packages/AbyssRankKit/Sources/AbyssRankKit/BackendComponents/HTTPMethod.swift
@@ -34,10 +34,10 @@ enum RequestError: Error, LocalizedError {
 struct HttpMethod<T: Decodable & Sendable>: Sendable {
     // MARK: - Method
 
-    enum Method {
-        case post
-        case get
-        case put
+    enum Method: String {
+        case post = "POST"
+        case get = "GET"
+        case put = "PUT"
     }
 
     /// 返回OPServer的请求的结果接口
@@ -53,19 +53,17 @@ struct HttpMethod<T: Decodable & Sendable>: Sendable {
         urlStr: String,
         body: Data? = nil,
         headersDict: [String: String] = [:],
-        parasDict: [String: String] = [:],
-        completion: @escaping @Sendable ((Result<T, RequestError>) -> Void)
-    ) async {
+        paramDict: [String: String] = [:]
+    ) async throws
+        -> T {
         // 请求url前缀，后跟request的类型
         let baseStr: String = baseHost
         // 由前缀和后缀共同组成的url
         var url = URLComponents(string: baseStr + urlStr)!
-        var urlQueryItems: [URLQueryItem] = url.queryItems ?? []
-        for para in parasDict {
-            urlQueryItems
-                .append(URLQueryItem(name: para.key, value: para.value))
-        }
-        url.queryItems = urlQueryItems
+        url.queryItems = url.queryItems ?? []
+        url.queryItems?.append(
+            contentsOf: paramDict.map { URLQueryItem(name: $0.key, value: $0.value) }
+        )
 
         // 初始化请求
         var request = URLRequest(url: url.url!)
@@ -78,86 +76,52 @@ struct HttpMethod<T: Decodable & Sendable>: Sendable {
             "Content-Type": "application/json",
         ]
 
-        request.setValue(
-            "Genshin-Pizza-Helper/3.0",
-            forHTTPHeaderField: "User-Agent"
-        )
+        request.setValue("Genshin-Pizza-Helper/3.0", forHTTPHeaderField: "User-Agent")
         for header in headersDict {
-            request.setValue(
-                header.value,
-                forHTTPHeaderField: header.key
-            )
+            request.setValue(header.value, forHTTPHeaderField: header.key)
         }
         // http方法
-        switch method {
-        case .post:
-            request.httpMethod = "POST"
-        case .get:
-            request.httpMethod = "GET"
-        case .put:
-            request.httpMethod = "PUT"
-        }
+        request.httpMethod = method.rawValue
         // request body
         if let body = body {
             request.httpBody = body
-            request.setValue(
-                "\(body.count)",
-                forHTTPHeaderField: "Content-Length"
-            )
+            request.setValue("\(body.count)", forHTTPHeaderField: "Content-Length")
         }
-        //                print(request)
-        //                print(request.allHTTPHeaderFields!)
-        //                print(String(data: request.httpBody!, encoding: .utf8)!)
+        // print(request)
+        // print(request.allHTTPHeaderFields!)
+        // print(String(data: request.httpBody!, encoding: .utf8)!)
         // 开始请求
+
         do {
             var (data, response) = try await URLSession.shared.data(for: request)
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(.failure(.responseError))
                 print("response error")
-                return
+                throw RequestError.responseError
             }
             let statusCode = response.statusCode.description
             guard let stringData = String(
                 data: data,
                 encoding: .utf8
             ) else {
-                completion(
-                    .failure(
-                        .decodeError(
-                            "fail convert data to .utf8 string"
-                        )
-                    )
-                ); return
+                throw RequestError.decodeError("fail convert data to .utf8 string")
             }
             print(stringData)
             data = stringData.replacingOccurrences(of: "\"NaN\"", with: "0").data(using: .utf8)!
             let decoder = JSONDecoder()
             print(statusCode)
             print(stringData)
-
             do {
-                let requestResult = try decoder.decode(
-                    T.self,
-                    from: data
-                )
-                completion(.success(requestResult))
+                return try decoder.decode(T.self, from: data)
             } catch {
                 print(error)
-                completion(.failure(.decodeError(
-                    error
-                        .localizedDescription
-                )))
+                throw RequestError.decodeError("\(error)")
             }
-
         } catch {
             print(error)
-            completion(.failure(.dataTaskError(
-                error
-                    .localizedDescription
-            )))
             print(
                 "DataTask error in General HttpMethod: \(error)\n"
             )
+            throw RequestError.dataTaskError("\(error)")
         }
     }
 }
