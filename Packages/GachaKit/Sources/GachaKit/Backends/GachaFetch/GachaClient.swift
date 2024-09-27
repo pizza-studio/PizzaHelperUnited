a652c3ad29f2cab8c5e5412b48df5cbbe807560a
ShikiSuen <shikisuen@outlook.com>
2024-09-27 21:02:12 +0800

    GachaKit // Let GachaClient sleeps per each tick during iteration.

--- Packages/GachaKit/Sources/GachaKit/Backends/GachaFetch/GachaClient.swift
+++ Packages/GachaKit/Sources/GachaKit/Backends/GachaFetch/GachaClient.swift
@@ -64,6 +64,8 @@ public struct GachaClient<GachaType: GachaTypeProtocol>: AsyncSequence, AsyncIte
                 endID: pagination.endID
             )
 
+            let sleepSeconds = Double.random(in: GachaClient.getGachaDelayRangeRandom)
+            try? await Task.sleep(nanoseconds: UInt64(sleepSeconds * 1_000_000_000))
             let (data, _) = try await URLSession.shared.data(for: request)
 
             var result = try GachaResult.decodeFromMiHoYoAPIJSONResult(data: data)
