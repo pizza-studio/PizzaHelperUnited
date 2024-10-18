a8412f457ea5ef0de738be595565fc7a52bf8902
ShikiSuen <shikisuen@outlook.com>
2024-10-18 22:28:43 +0800

    Repo // Clean-up SPM dependencies & + PZInGameEventKit (scratch).

--- Packages/PZHelper/Sources/PZHelper/PZHelper.swift
+++ Packages/PZHelper/Sources/PZHelper/PZHelper.swift
@@ -2,8 +2,25 @@
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
-import GachaKit
 import PZAccountKit
 import SwiftData
+import SwiftUI
+
+// MARK: - PZHelper
 
 public enum PZHelper {}
+
+extension PZHelper {
+    @MainActor @SceneBuilder
+    public static func makeMainScene() -> some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.horizontalSizeClass, .compact)
+            #if targetEnvironment(macCatalyst)
+                .frame(minWidth: 600, minHeight: 800)
+            #endif
+        }
+        .windowResizability(.contentMinSize)
+        .modelContainer(PZProfileActor.shared.modelContainer)
+    }
+}
