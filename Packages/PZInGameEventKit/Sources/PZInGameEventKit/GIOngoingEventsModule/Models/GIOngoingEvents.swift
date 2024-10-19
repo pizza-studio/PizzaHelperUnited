ce8a143175dfd6601d3594f17a992d540b23acd6
ShikiSuen <shikisuen@outlook.com>
2024-10-19 20:08:30 +0800

    PZInGameEventKit // Initial implementation for GI Events.

---
+++ Packages/PZInGameEventKit/Sources/PZInGameEventKit/GIOngoingEventsModule/Models/GIOngoingEvents.swift
@@ -0,0 +1,57 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Defaults
+import Foundation
+import PZAccountKit
+import PZBaseKit
+
+// MARK: - GIOngoingEvents.Exception
+
+extension GIOngoingEvents {
+    public enum Exception: Error, LocalizedError {
+        case dataRetrievalException(Error)
+
+        // MARK: Public
+
+        public var localizedDescription: String {
+            switch self {
+            case let .dataRetrievalException(error):
+                "GIOngoingEvents.dataRetrievalException: \(error)"
+            }
+        }
+
+        public var errorDescription: String? { localizedDescription }
+
+        public var description: String { localizedDescription }
+    }
+}
+
+extension GIOngoingEvents {
+    public static func fetch() async -> Result<Self.EventList, Self.Exception> {
+        let urlStr = "https://gi.yatta.moe/assets/data/event.json"
+        do {
+            let (data, _) = try await URLSession.shared.data(from: urlStr.asURL)
+            let decoded = try JSONDecoder().decode(Self.EventList.self, from: data)
+            return .success(decoded)
+        } catch {
+            return .failure(Self.Exception.dataRetrievalException(error))
+        }
+    }
+
+    public static func getRemainDays(_ endAt: String) -> Date.IntervalDate? {
+        let dateFormatter = DateFormatter.Gregorian()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let cachedServerRawValue = Defaults[.defaultServer]
+        let cachedServerTyped = HoYo.Server(rawValue: cachedServerRawValue) ?? .asia(.genshinImpact)
+        dateFormatter.timeZone = cachedServerTyped.timeZone
+        let endDate = dateFormatter.date(from: endAt)
+        guard let endDate = endDate else {
+            return nil
+        }
+        let interval = endDate - Date()
+        return interval
+    }
+}
---
+++ Packages/PZInGameEventKit/Sources/PZInGameEventKit/SharedComponents/EventDetailWebView.swift
@@ -0,0 +1,201 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+#if !os(watchOS)
+
+import PZBaseKit
+import SafariServices
+import SwiftUI
+import WebKit
+
+@MainActor
+struct EventDetailWebView {
+    // MARK: Lifecycle
+
+    init(banner: String, nameFull: String, content: String) {
+        self.banner = banner
+        self.nameFull = nameFull
+        self.content = content
+    }
+
+    // MARK: Internal
+
+    @Environment(\.colorScheme) var colorScheme
+    let webView = OPWebView()
+    let banner: String
+    let nameFull: String
+    let content: String
+    var articleDic = [
+        // 主题颜色：亮色：""，暗色："bg-amberDark-500 text-amberHalfWhite"
+        "themeClass": "",
+        "banner": "",
+        "nameFull": "",
+        "description": "",
+    ]
+
+    func getArticleDic() -> [String: String] {
+        if colorScheme == .dark {
+            let articleDic = [
+                "themeClass": "bg-amberDark-800 text-amberHalfWhite", // 主题颜色
+                "banner": banner,
+                "nameFull": nameFull,
+                "description": content,
+            ]
+            return articleDic
+        } else {
+            let articleDic = [
+                "themeClass": "", // 主题颜色
+                "banner": banner,
+                "nameFull": nameFull,
+                "description": content,
+            ]
+            return articleDic
+        }
+    }
+}
+
+#if os(macOS)
+
+extension EventDetailWebView: NSViewRepresentable {
+    class Coordinator: NSObject, WKScriptMessageHandler, WKUIDelegate {
+        // MARK: Lifecycle
+
+        init(_ parent: EventDetailWebView) {
+            self.parent = parent
+        }
+
+        // MARK: Internal
+
+        var parent: EventDetailWebView
+
+        func userContentController(
+            _ userContentController: WKUserContentController,
+            didReceive message: WKScriptMessage
+        ) {
+            print("message: \(message.name)")
+            switch message.name {
+            case "getArticleInfoBeforeLoaded":
+                if let articleData = try? JSONSerialization.data(
+                    withJSONObject: parent.getArticleDic(),
+                    options: JSONSerialization.WritingOptions.prettyPrinted
+                ) {
+                    let articleInfo = String(
+                        data: articleData,
+                        encoding: String.Encoding.utf8
+                    )
+
+                    let inputJS = "updateArticleInfo(\(articleInfo ?? ""))"
+                    print(inputJS)
+                    parent.webView.evaluateJavaScript(inputJS)
+                }
+            default:
+                break
+            }
+        }
+    }
+
+    static func dismantleNSView(_ nsView: OPWebView, coordinator: Coordinator) {
+        nsView.configuration.userContentController
+            .removeScriptMessageHandler(forName: "getArticleInfoBeforeLoaded")
+    }
+
+    func makeNSView(context: Context) -> OPWebView {
+        webView.configuration.userContentController.add(
+            makeCoordinator(),
+            name: "getArticleInfoBeforeLoaded"
+        )
+        return webView
+    }
+
+    func updateNSView(_ nsView: OPWebView, context: Context) {
+        nsView.uiDelegate = context.coordinator
+        if let startPageURL = Bundle.module.url(
+            forResource: "article",
+            withExtension: "html"
+        ) {
+            nsView.loadFileURL(
+                startPageURL,
+                allowingReadAccessTo: Bundle.module.bundleURL
+            )
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
+
+#elseif os(iOS) || targetEnvironment(macCatalyst)
+
+extension EventDetailWebView: UIViewRepresentable {
+    class Coordinator: NSObject, WKScriptMessageHandler, WKUIDelegate {
+        // MARK: Lifecycle
+
+        init(_ parent: EventDetailWebView) {
+            self.parent = parent
+        }
+
+        // MARK: Internal
+
+        var parent: EventDetailWebView
+
+        func userContentController(
+            _ userContentController: WKUserContentController,
+            didReceive message: WKScriptMessage
+        ) {
+            print("message: \(message.name)")
+            switch message.name {
+            case "getArticleInfoBeforeLoaded":
+                if let articleData = try? JSONSerialization.data(
+                    withJSONObject: parent.getArticleDic(),
+                    options: JSONSerialization.WritingOptions.prettyPrinted
+                ) {
+                    let articleInfo = String(
+                        data: articleData,
+                        encoding: String.Encoding.utf8
+                    )
+
+                    let inputJS = "updateArticleInfo(\(articleInfo ?? ""))"
+                    print(inputJS)
+                    parent.webView.evaluateJavaScript(inputJS)
+                }
+            default:
+                break
+            }
+        }
+    }
+
+    static func dismantleUIView(_ uiView: OPWebView, coordinator: Coordinator) {
+        uiView.configuration.userContentController
+            .removeScriptMessageHandler(forName: "getArticleInfoBeforeLoaded")
+    }
+
+    func makeUIView(context: Context) -> OPWebView {
+        webView.configuration.userContentController.add(
+            makeCoordinator(),
+            name: "getArticleInfoBeforeLoaded"
+        )
+        return webView
+    }
+
+    func updateUIView(_ uiView: OPWebView, context: Context) {
+        uiView.uiDelegate = context.coordinator
+        if let startPageURL = Bundle.module.url(
+            forResource: "article",
+            withExtension: "html"
+        ) {
+            uiView.loadFileURL(
+                startPageURL,
+                allowingReadAccessTo: Bundle.module.bundleURL
+            )
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
+
+#endif
+#endif
