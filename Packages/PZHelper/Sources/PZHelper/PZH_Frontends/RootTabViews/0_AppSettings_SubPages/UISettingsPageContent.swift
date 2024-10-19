1bcf898b95a3a9d0c0aac10d937b9f1803abca81
ShikiSuen <shikisuen@outlook.com>
2024-10-19 23:42:00 +0800

    PZHelper // Add settings for GI Information TimeZone.

--- Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootTabViews/0_AppSettings_SubPages/UISettingsPageContent.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootTabViews/0_AppSettings_SubPages/UISettingsPageContent.swift
@@ -4,6 +4,7 @@
 
 @preconcurrency import Defaults
 import EnkaKit
+import PZAccountKit
 import SwiftUI
 import WallpaperKit
 
@@ -15,10 +16,14 @@ struct UISettingsPageContent: View {
     @MainActor var body: some View {
         Form {
             Section {
+                AppWallpaperSettingsPicker()
+                    .alignmentGuide(.listRowSeparatorLeading) { d in
+                        d[.leading]
+                    }
                 Toggle(isOn: $restoreTabOnLaunching) {
                     Text("setting.display.restoreTabOnLaunching".i18nPZHelper)
                 }
-                AppWallpaperSettingsPicker()
+                defaultServerSelector4GI
             } header: {
                 Text("settings.display.generalSettings.sectionHeader".i18nPZHelper)
             }
@@ -29,9 +34,27 @@ struct UISettingsPageContent: View {
         .navigationTitle("settings.uiSettings.title".i18nPZHelper)
     }
 
+    @MainActor @ViewBuilder var defaultServerSelector4GI: some View {
+        VStack {
+            Picker(selection: $defaultServer4GI) {
+                ForEach(HoYo.Server.allCases4GI) { server in
+                    Text(
+                        server.localizedDescriptionByGame + " (\(server.timeZone.identifier))"
+                    ).tag(server.rawValue)
+                }
+            } label: {
+                Text("settings.display.timeZone4GI.title".i18nPZHelper)
+            }
+            Text("settings.display.timeZone4GI.description".i18nPZHelper)
+                .font(.footnote).foregroundStyle(.secondary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+
     // MARK: Private
 
     @State private var sharedDB = Enka.Sputnik.shared
     @Default(.restoreTabOnLaunching) private var restoreTabOnLaunching: Bool
     @Default(.background4App) private var background4App: Wallpaper
+    @Default(.defaultServer) private var defaultServer4GI: String
 }
