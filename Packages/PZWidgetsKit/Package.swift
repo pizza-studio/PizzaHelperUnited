a1b450d8af27be6004c9178678e25f898aab7970
ShikiSuen <shikisuen@outlook.com>
2024-10-21 23:56:13 +0800

    PZWidgetsKit // Starting from scratch.

---
+++ Packages/PZWidgetsKit/Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PZWidgetsKit",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v17), .macOS(.v14), .macCatalyst(.v17), .visionOS(.v1)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PZWidgetsKit",
+            targets: ["PZWidgetsKit"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../PZKit"),
+        .package(path: "../PZInGameEventKit"),
+        .package(path: "../GITodayMaterialsKit"),
+        .package(path: "../WallpaperKit"),
+        .package(
+            url: "https://github.com/sindresorhus/Defaults", .upToNextMajor(from: "8.2.0")
+        ),
+        .package(
+            url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .upToNextMajor(from: "5.3.0")
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PZWidgetsKit",
+            dependencies: [
+                .product(name: "PZAccountKit", package: "PZKit"),
+                .product(name: "PZBaseKit", package: "PZKit"),
+                .product(name: "PZInGameEventKit", package: "PZInGameEventKit"),
+                .product(name: "GITodayMaterialsKit", package: "GITodayMaterialsKit"),
+                .product(name: "WallpaperKit", package: "WallpaperKit"),
+                .product(name: "PZIntentKit", package: "PZKit"),
+                .product(name: "SFSafeSymbols", package: "SFSafeSymbols"),
+                .product(name: "Defaults", package: "Defaults"),
+            ],
+            resources: [
+                .process("Resources/"),
+            ]
+        ),
+        .testTarget(
+            name: "PZWidgetsKitTests",
+            dependencies: ["PZWidgetsKit"]
+        ),
+    ]
+)
