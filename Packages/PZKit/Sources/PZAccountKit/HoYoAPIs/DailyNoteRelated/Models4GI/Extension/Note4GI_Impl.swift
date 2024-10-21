d5320ad18b7ad2d6c822756ab119dd043bbdda9b
ShikiSuen <shikisuen@outlook.com>
2024-10-22 01:13:11 +0800

    PZAccountKit // Extend Note4GI for future purposes.

---
+++ Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/Models4GI/Extension/Note4GI_Impl.swift
@@ -0,0 +1,23 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Defaults
+import Foundation
+import PZBaseKit
+
+extension HomeCoinInfo4GI {
+    public func calculatedCurrentHomeCoin(referTo date: Date) -> Int {
+        let secondToFull = fullTime.timeIntervalSinceReferenceDate - date.timeIntervalSinceReferenceDate
+        guard secondToFull > 0 else { return maxHomeCoin }
+        return maxHomeCoin - Int(secondToFull * Defaults[.homeCoinRefreshFrequencyInHour] / 3600)
+    }
+}
+
+extension ResinInfo4GI {
+    public func calculatedCurrentResin(referTo date: Date) -> Int {
+        let secondToFull = resinRecoveryTime.timeIntervalSinceReferenceDate - date.timeIntervalSinceReferenceDate
+        guard secondToFull > 0 else { return maxResin }
+        return maxResin - Int(ceil(secondToFull / 8 / 60))
+    }
+}
