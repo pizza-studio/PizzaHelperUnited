6d1051e738672f0244995dc585c5982e04ea38d7
ShikiSuen <shikisuen@outlook.com>
2024-10-21 20:18:05 +0800

    PZAboutKit // Initial implementation.

---
+++ Packages/PZAboutKit/Sources/PZAboutKit/OSImpl.swift
@@ -0,0 +1,18 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import SwiftUI
+
+extension String {
+    public var i18nAboutKit: String {
+        String(localized: .init(stringLiteral: self), bundle: .module)
+    }
+}
+
+extension String.LocalizationValue {
+    public var i18nAboutKit: String {
+        String(localized: self, bundle: .module)
+    }
+}
