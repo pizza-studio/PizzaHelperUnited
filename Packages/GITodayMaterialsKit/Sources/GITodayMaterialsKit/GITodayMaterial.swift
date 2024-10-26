a95269ccb69bac4d2885edfcd227e421b9ff00e9
ShikiSuen <shikisuen@outlook.com>
2024-10-26 11:23:08 +0800

    GITodayMaterialsKit // API changes for future purposes.

--- Packages/GITodayMaterialsKit/Sources/GITodayMaterialsKit/GITodayMaterial.swift
+++ Packages/GITodayMaterialsKit/Sources/GITodayMaterialsKit/GITodayMaterial.swift
@@ -2,11 +2,14 @@
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
+import Defaults
 import Foundation
+import PZAccountKit
+import SwiftUI
 
 // MARK: - GITodayMaterial
 
-public struct GITodayMaterial: Codable, Identifiable, Equatable, Hashable {
+public struct GITodayMaterial: Codable, Identifiable, Equatable, Hashable, Sendable {
     public let id: Int
     public let isWeapon: Bool
     public let nameTag: String
@@ -14,6 +17,8 @@ public struct GITodayMaterial: Codable, Identifiable, Equatable, Hashable {
 }
 
 extension GITodayMaterial {
+    public var iconObj: Image { Image(nameTag, bundle: .module) }
+
     public var localized: String {
         "asset.dailyMaterial:\(nameTag)".i18nTodayMaterialNames
     }
@@ -30,5 +35,58 @@ extension GITodayMaterial {
         // MARK: Public
 
         public var id: Int { rawValue }
+
+        public static func today() -> Self? {
+            var calendar = Calendar.current
+            let maybeServer = HoYo.Server(rawValue: Defaults[.defaultServer])
+            calendar.timeZone = (maybeServer ?? .asia(.genshinImpact)).timeZone
+            let isTimePast4am: Bool = Date() > calendar
+                .date(bySettingHour: 4, minute: 0, second: 0, of: Date())!
+            let todayWeekDayNum = calendar.dateComponents([.weekday], from: Date())
+                .weekday!
+            let weekdayNum = isTimePast4am ? todayWeekDayNum : (todayWeekDayNum - 1)
+            return switch weekdayNum {
+            case 1: nil
+            case 2, 5: .MonThu
+            case 3, 6: .TueFri
+            case 0, 4, 7: .WedSat
+            default: nil
+            }
+        }
+
+        public func tomorrow() -> Self? {
+            switch self {
+            case .MonThu:
+                return .TueFri
+            case .TueFri:
+                return .WedSat
+            case .WedSat:
+                return .none
+            }
+        }
+    }
+}
+
+extension GITodayMaterial.AvailableWeekDay? {
+    public func tomorrow() -> Self {
+        switch self {
+        case .MonThu:
+            return .TueFri
+        case .TueFri:
+            return .WedSat
+        case .WedSat:
+            return .none
+        case .none:
+            return .MonThu
+        }
+    }
+
+    public var localizedName: String {
+        switch self {
+        case .MonThu: "todayMaterialsKit.week.MR".i18nTodayMaterials
+        case .TueFri: "todayMaterialsKit.week.TF".i18nTodayMaterials
+        case .WedSat: "todayMaterialsKit.week.WS".i18nTodayMaterials
+        case .none: "todayMaterialsKit.week.SU".i18nTodayMaterials
+        }
     }
 }
