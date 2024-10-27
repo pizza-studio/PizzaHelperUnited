7e0a9eee78e254e47bb7d2468a0f5048d8720a0d
ShikiSuen <shikisuen@outlook.com>
2024-10-27 22:22:45 +0800

    WidgetExtension // Disable Live Activity codes on macOS.

--- WidgetExtension/Modules/LiveActivityWidget/ResinRecoveryActivityController.swift
+++ WidgetExtension/Modules/LiveActivityWidget/ResinRecoveryActivityController.swift
@@ -2,7 +2,7 @@
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
-#if canImport(ActivityKit)
+#if canImport(ActivityKit) && !targetEnvironment(macCatalyst)
 import ActivityKit
 @preconcurrency import Defaults
 import Foundation
