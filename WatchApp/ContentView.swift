dcc699ff299e393611b4f607c212837542842c54
ShikiSuen <shikisuen@outlook.com>
2024-10-27 18:39:10 +0800

    Xcode // Add WatchOS target with its widget extensions.

---
+++ WatchApp/ContentView.swift
@@ -0,0 +1,21 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
