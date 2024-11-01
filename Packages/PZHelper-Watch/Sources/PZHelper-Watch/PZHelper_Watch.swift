39d281b66d902c7aca7ca6b77f1f3a4f3cecc33e
ShikiSuen <shikisuen@outlook.com>
2024-11-02 02:31:37 +0800

    WatchApp // Hook SwiftData container to ContentView().

--- Packages/PZHelper-Watch/Sources/PZHelper-Watch/PZHelper_Watch.swift
+++ Packages/PZHelper-Watch/Sources/PZHelper-Watch/PZHelper_Watch.swift
@@ -1,3 +1,38 @@
 // (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import PZAccountKit
+import SwiftData
+import SwiftUI
+
+// MARK: - PZHelperWatch
+
+public enum PZHelperWatch {}
+
+extension PZHelperWatch {
+    @MainActor @SceneBuilder
+    public static func makeMainScene() -> some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.horizontalSizeClass, .compact)
+                .defaultAppStorage(.baseSuite)
+                .onAppear {
+                    if !isApplicationBooted {
+                        startupTasks()
+                    }
+                    isApplicationBooted = true
+                }
+        }
+        .modelContainer(PZProfileActor.shared.modelContainer)
+    }
+
+    @MainActor public private(set) static var isApplicationBooted = false
+}
+
+extension PZHelperWatch {
+    @MainActor
+    private static func startupTasks() {
+        PZProfileActor.attemptToAutoInheritOldAccountsIntoProfiles(resetNotifications: true)
+    }
+}
