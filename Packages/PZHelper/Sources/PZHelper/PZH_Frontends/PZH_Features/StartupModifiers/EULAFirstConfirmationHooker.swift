d5586dec81c273fc57721c51f7c80562e0b446ef
ShikiSuen <shikisuen@outlook.com>
2024-11-05 19:16:19 +0800

    PZHelper // Confirm EULA on OOBE.

---
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/PZH_Features/StartupModifiers/EULAFirstConfirmationHooker.swift
@@ -0,0 +1,38 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+@preconcurrency import Defaults
+import Foundation
+import PZAboutKit
+import PZBaseKit
+import SwiftUI
+
+extension View {
+    func hookEULACheckerOnOOBE() -> some View {
+        modifier(EULAFirstConfirmationHooker())
+    }
+}
+
+// MARK: - EULAFirstConfirmationHooker
+
+private struct EULAFirstConfirmationHooker: ViewModifier {
+    // MARK: Internal
+
+    func body(content: Content) -> some View {
+        if isEULAConfirmed {
+            content
+        } else {
+            content
+                .sheet(isPresented: $isSheetShown) {
+                    EULAView(isOOBE: true)
+                        .interactiveDismissDisabled()
+                }
+        }
+    }
+
+    // MARK: Private
+
+    @State private var isSheetShown = true
+    @Default(.isEULAConfirmed) private var isEULAConfirmed: Bool
+}
