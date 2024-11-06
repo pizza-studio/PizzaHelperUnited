b38491d4186f693a3128a19ca40f64935fbc98d3
ShikiSuen <shikisuen@outlook.com>
2024-11-06 12:45:50 +0800

    Widgets // Add timestampOnCreation to TimelineEntries.

--- WidgetExtension/Modules/TimelineProviders/LockScreenLoopWidgetProvider.swift
+++ WidgetExtension/Modules/TimelineProviders/LockScreenLoopWidgetProvider.swift
@@ -14,6 +14,7 @@ import WidgetKit
 
 struct AccountAndShowWhichInfoIntentEntry: TimelineEntry {
     let date: Date
+    let timestampOnCreation: Date = .now
     let result: Result<any DailyNoteProtocol, any Error>
     var accountName: String?
 
