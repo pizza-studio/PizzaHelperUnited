d0560c4451e2a606de09e6e3a5ea3dad8cbbddbd
ShikiSuen <shikisuen@outlook.com>
2024-11-06 21:54:31 +0800

    Widgets // Tuned the layout for Live Activity.

--- Packages/PZWidgetsKit/Sources/PZWidgetsKit/LiveActivityRelated/LiveActivityBackend.swift
+++ Packages/PZWidgetsKit/Sources/PZWidgetsKit/LiveActivityRelated/LiveActivityBackend.swift
@@ -88,25 +88,21 @@ public struct ResinRecoveryAttributes: Sendable {
             self.maxResin = staminaIntel.max
             self.resinCountWhenUpdated = staminaIntel.existing
             self.resinRecoveryTime = dailyNote.staminaFullTimeOnFinish
+            self.game = dailyNote.game
             switch dailyNote {
             case _ as WidgetNote4GI:
-                self.game = .genshinImpact
                 self.expeditionAllCompleteTime = nil
                 self.showExpedition = showExpedition
             case let data as GeneralNote4GI:
-                self.game = .genshinImpact
                 self.expeditionAllCompleteTime = data.expeditions.expeditions.map(\.finishTime).max() ?? .now
                 self.showExpedition = showExpedition
             case let data as Note4HSR:
-                self.game = .starRail
                 self.expeditionAllCompleteTime = data.assignmentInfo.assignments.map(\.finishedTime).max() ?? .now
                 self.showExpedition = showExpedition
             case _ as Note4ZZZ:
-                self.game = .zenlessZone
                 self.showExpedition = false
                 self.expeditionAllCompleteTime = nil
             default:
-                self.game = .genshinImpact // 乱填。
                 self.showExpedition = false
                 self.expeditionAllCompleteTime = nil
             }
