e8cdfb2774ca4f343d2c7f4f5efaa782b79a7828
ShikiSuen <shikisuen@outlook.com>
2024-11-13 10:58:52 +0800

    PZAboutKit // Add FAQ.

---
+++ Packages/PZAboutKit/Sources/PZAboutKit/AboutViewRelated/FAQView.swift
@@ -0,0 +1,39 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+@preconcurrency import Defaults
+import Foundation
+import PZBaseKit
+import SwiftUI
+
+// MARK: - FAQView
+
+public struct FAQView: View {
+    // MARK: Lifecycle
+
+    public init() {}
+
+    // MARK: Public
+
+    public static let navTitle: String = {
+        let key: String.LocalizationValue = "aboutKit.FAQ.title"
+        return .init(localized: key, bundle: .module)
+    }()
+
+    public var body: some View {
+        NavigationStack {
+            WebBrowserView(url: Self.urlString)
+                .navigationTitle(Self.navTitle)
+                .navBarTitleDisplayMode(.inline)
+        }
+    }
+
+    // MARK: Private
+
+    private static let urlString: String = {
+        Bundle.module.url(forResource: "FAQ", withExtension: "html")!.absoluteString
+    }()
+
+    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
+}
