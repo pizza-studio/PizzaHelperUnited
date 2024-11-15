1c6dee3b671a57c628dcd77461278f7c0aa5944c
ShikiSuen <shikisuen@outlook.com>
2024-11-15 23:49:01 +0800

    WallpaperKit // Fix the Live Activity background setter in the Gallery.

--- Packages/WallpaperKit/Package.swift
+++ Packages/WallpaperKit/Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         .package(
             url: "https://github.com/sindresorhus/Defaults", .upToNextMajor(from: "8.2.0")
         ),
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .upToNextMajor(from: "5.3.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -27,6 +28,7 @@ let package = Package(
             name: "WallpaperKit",
             dependencies: [
                 .product(name: "Defaults", package: "Defaults"),
+                .product(name: "SFSafeSymbols", package: "SFSafeSymbols"),
                 .product(name: "PZBaseKit", package: "PZKit"),
             ],
             resources: [
