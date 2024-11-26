d03cd0990f335b2b51ff103e6c24ada0e9e62a6d
ShikiSuen <shikisuen@outlook.com>
2024-11-26 21:14:01 +0800

    Widgets // Add Echo-of-War to HSR desktop widgets.

---
+++ WidgetExtension/Modules/MainWidget/Views/InfoBars/TrounceBlossomInfoBar.swift
@@ -0,0 +1,57 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import PZAccountKit
+import PZBaseKit
+import SFSafeSymbols
+import SwiftUI
+
+@available(watchOS, unavailable)
+struct TrounceBlossomInfoBar: View {
+    let weeklyBossesInfo: GeneralNote4GI.WeeklyBossesInfo4GI
+
+    var isWeeklyBossesFinishedImage: some View {
+        (weeklyBossesInfo.allDiscountsAreUsedUp)
+            ? Image(systemSymbol: .checkmark)
+            .overlayImageWithRingProgressBar(1.0, scaler: 0.70)
+            : Image(systemSymbol: .questionmark)
+            .overlayImageWithRingProgressBar(1.0)
+    }
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 8) {
+            AccountKit.imageAsset("gi_note_weeklyBosses")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 25)
+                .shadow(color: .white, radius: 1)
+                .legibilityShadow(isText: false)
+            isWeeklyBossesFinishedImage
+                .frame(width: 13, height: 13)
+                .foregroundColor(Color("textColor3", bundle: .main))
+                .legibilityShadow()
+            HStack(alignment: .lastTextBaseline, spacing: 1) {
+                if !weeklyBossesInfo.allDiscountsAreUsedUp {
+                    Text(verbatim: "\(weeklyBossesInfo.remainResinDiscount)")
+                        .lineLimit(1)
+                        .foregroundColor(Color("textColor3", bundle: .main))
+                        .font(.system(.caption, design: .rounded))
+                        .minimumScaleFactor(0.2)
+                    Text(verbatim: " / \(weeklyBossesInfo.totalResinDiscount)")
+                        .lineLimit(1)
+                        .foregroundColor(Color("textColor3", bundle: .main))
+                        .font(.system(.footnote, design: .rounded))
+                        .minimumScaleFactor(0.2)
+                } else {
+                    Text(verbatim: weeklyBossesInfo.textDescription)
+                        .lineLimit(1)
+                        .foregroundColor(Color("textColor3", bundle: .main))
+                        .font(.system(.caption, design: .rounded))
+                        .minimumScaleFactor(0.2)
+                }
+            }
+            .legibilityShadow(isText: false)
+        }
+    }
+}
