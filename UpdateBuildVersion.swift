6162cf944c127ad99bb9b0216ff2e7af441c03b9
ShikiSuen <shikisuen@outlook.com>
2024-12-07 15:49:53 +0800

    Repo // Fix version boost scripts.

--- UpdateBuildVersion.swift
+++ UpdateBuildVersion.swift
@@ -19,33 +19,39 @@ extension String {
     }
 }
 
-var verMarket: String = "1.0.0"
-var verBuild: String = "1000"
-var strXcodeProjContent: String = ""
-var dirXcodeProjectFile = "./UnitedPizzaHelper.xcodeproj/project.pbxproj"
-var theDictionary: NSDictionary?
+func process(_ dirXcodeProjectFile: String) {
+    var verMarket = "1.0.0"
+    var verBuild = "1000"
+    var strXcodeProjContent = ""
 
-if CommandLine.arguments.count == 3 {
-    verMarket = CommandLine.arguments[1]
-    verBuild = CommandLine.arguments[2]
+    if CommandLine.arguments.count == 3 {
+        verMarket = CommandLine.arguments[1]
+        verBuild = CommandLine.arguments[2]
 
-    // Xcode project file version update.
-    do {
-        strXcodeProjContent += try String(contentsOfFile: dirXcodeProjectFile, encoding: .utf8)
-    } catch {
-        NSLog(" - Exception happened when reading raw phrases data.")
+        // Xcode project file version update.
+        do {
+            strXcodeProjContent += try String(contentsOfFile: dirXcodeProjectFile, encoding: .utf8)
+        } catch {
+            NSLog(" - Exception happened when reading raw phrases data.")
+        }
+
+        strXcodeProjContent.regReplace(
+            pattern: #"CURRENT_PROJECT_VERSION = .*$"#, replaceWith: "CURRENT_PROJECT_VERSION = " + verBuild + ";"
+        )
+        strXcodeProjContent.regReplace(
+            pattern: #"MARKETING_VERSION = .*$"#, replaceWith: "MARKETING_VERSION = " + verMarket + ";"
+        )
+        do {
+            try strXcodeProjContent.write(
+                to: URL(fileURLWithPath: dirXcodeProjectFile),
+                atomically: false,
+                encoding: .utf8
+            )
+        } catch {
+            NSLog(" -: Error on writing strings to file: \(error)")
+        }
+        NSLog(" - Xcode 專案版本資訊更新完成：\(verMarket) \(verBuild)。")
     }
-
-    strXcodeProjContent.regReplace(
-        pattern: #"CURRENT_PROJECT_VERSION = .*$"#, replaceWith: "CURRENT_PROJECT_VERSION = " + verBuild + ";"
-    )
-    strXcodeProjContent.regReplace(
-        pattern: #"MARKETING_VERSION = .*$"#, replaceWith: "MARKETING_VERSION = " + verMarket + ";"
-    )
-    do {
-        try strXcodeProjContent.write(to: URL(fileURLWithPath: dirXcodeProjectFile), atomically: false, encoding: .utf8)
-    } catch {
-        NSLog(" -: Error on writing strings to file: \(error)")
-    }
-    NSLog(" - Xcode 專案版本資訊更新完成：\(verMarket) \(verBuild)。")
 }
+
+process("./UnitedPizzaHelper.xcodeproj/project.pbxproj")
