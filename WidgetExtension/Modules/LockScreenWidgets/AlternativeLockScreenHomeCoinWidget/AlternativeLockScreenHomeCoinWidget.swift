c8142a915456d8371c4c58e82fc7e539107aa4df
ShikiSuen <shikisuen@outlook.com>
2024-12-13 00:13:27 +0800

    Widgets // Filter nonsense widget recommendations.

--- WidgetExtension/Modules/LockScreenWidgets/AlternativeLockScreenHomeCoinWidget/AlternativeLockScreenHomeCoinWidget.swift
+++ WidgetExtension/Modules/LockScreenWidgets/AlternativeLockScreenHomeCoinWidget/AlternativeLockScreenHomeCoinWidget.swift
@@ -17,7 +17,10 @@ struct AlternativeLockScreenHomeCoinWidget: Widget {
         AppIntentConfiguration(
             kind: kind,
             intent: SelectOnlyAccountIntent.self,
-            provider: LockScreenWidgetProvider(recommendationsTag: "watch.info.RealmCurrency")
+            provider: LockScreenWidgetProvider(
+                games: [.genshinImpact],
+                recommendationsTag: "watch.info.RealmCurrency"
+            )
         ) { entry in
             AlternativeLockScreenHomeCoinWidgetView(entry: entry)
                 .lockscreenContainerBackground { EmptyView() }
