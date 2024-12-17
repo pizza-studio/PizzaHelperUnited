f018955250c2afdebaa67dc5c17d4dc3f6f193f8
ShikiSuen <shikisuen@outlook.com>
2024-12-17 13:43:46 +0800

    Widgets // Migrating timeline implementations from PZHelper4HSR.

--- WidgetExtension/Modules/TimelineProviders/LockScreenLoopWidgetProvider.swift
+++ WidgetExtension/Modules/TimelineProviders/LockScreenLoopWidgetProvider.swift
@@ -125,91 +125,109 @@ struct LockScreenLoopWidgetProvider: AppIntentTimelineProvider {
         in context: Context
     ) async
         -> Timeline<Entry> {
-        let configs = PZWidgets.getAllProfiles()
-        let style = configuration.usingResinStyle
+        var refreshTime = PZWidgets.getRefreshDate()
+        let entries: [Entry] = await getEntries(configuration: configuration, refreshTime: &refreshTime)
+        return Timeline(
+            entries: entries,
+            policy: .after(refreshTime)
+        )
+    }
 
-        func makeFallbackResult(error: WidgetError) -> Timeline<Entry> {
-            let entry = Entry(
-                date: PZWidgets.getRefreshDate(),
-                result: .failure(error),
-                accountUUIDString: nil,
-                usingResinStyle: style
-            )
-            return Timeline<Entry>(
-                entries: [entry],
-                policy: .after(
-                    PZWidgets.getRefreshDate(isError: true)
-                )
-            )
-        }
+    // MARK: Private
 
-        func getTimelineEntries(
-            config: PZProfileSendable
-        ) async
-            -> Timeline<Entry> {
-            do {
-                let data = try await config.getDailyNote()
-                let entries = (0 ... 40).map { index in
-                    let timeInterval = TimeInterval(Double(index) * data.eachStaminaRecoveryTime)
-                    let entryDate = Date(timeIntervalSinceNow: timeInterval)
-                    return Entry(
-                        date: entryDate,
-                        result: .success(data),
-                        accountName: config.name,
+    private func getEntries(configuration: Intent, refreshTime: inout Date) async -> [Entry] {
+        let findProfileResult = findProfile(for: configuration)
+        switch findProfileResult {
+        case let .success(profile):
+            let dailyNoteResult = await fetchDailyNote(for: profile)
+            switch dailyNoteResult {
+            case .success:
+                refreshTime = PZWidgets.getRefreshDate() // fetchDailyNote 的过程本身就会消耗时间，需要统计。
+                var tlEntryDate = Date.now
+
+                func boostDate() {
+                    tlEntryDate = tlEntryDate.addingTimeInterval(profile.game.eachStaminaRecoveryTime)
+                }
+
+                var entries: [Entry] = []
+                while refreshTime > tlEntryDate {
+                    entries.append(
+                        Entry(
+                            date: tlEntryDate,
+                            result: dailyNoteResult,
+                            accountName: profile.name,
+                            showEchoOfWar: configuration.showEchoOfWar,
+                            showTrounceBlossom: configuration.showTrounceBlossom,
+                            showTransformer: configuration.showTransformer,
+                            accountUUIDString: profile.uuid.uuidString,
+                            usingResinStyle: configuration.usingResinStyle
+                        )
+                    )
+                    boostDate()
+                }
+                return entries
+            case let .failure(error):
+                refreshTime = PZWidgets.getRefreshDate(isError: true) // fetchDailyNote 的过程本身就会消耗时间，需要统计。
+                return [
+                    Entry(
+                        date: Date(),
+                        result: .failure(error),
+                        accountName: profile.name,
                         showEchoOfWar: configuration.showEchoOfWar,
                         showTrounceBlossom: configuration.showTrounceBlossom,
                         showTransformer: configuration.showTransformer,
-                        accountUUIDString: config.uuid.uuidString,
-                        usingResinStyle: style
-                    )
-                }
-                return .init(entries: entries, policy: .after(PZWidgets.getRefreshDate()))
-            } catch {
-                let entry = Entry(
+                        accountUUIDString: profile.uuid.uuidString,
+                        usingResinStyle: configuration.usingResinStyle
+                    ),
+                ]
+            }
+        case let .failure(exception):
+            return [
+                Entry(
                     date: Date(),
-                    result: .failure(error),
+                    result: .failure(exception),
+                    accountName: nil,
                     showEchoOfWar: configuration.showEchoOfWar,
                     showTrounceBlossom: configuration.showTrounceBlossom,
                     showTransformer: configuration.showTransformer,
-                    accountUUIDString: config.uuid.uuidString,
-                    usingResinStyle: style
-                )
-                return .init(
-                    entries: [entry], policy: .after(
-                        PZWidgets.getRefreshDate(isError: true)
-                    )
-                )
-            }
+                    accountUUIDString: nil,
+                    usingResinStyle: configuration.usingResinStyle
+                ),
+            ]
         }
+    }
 
-        guard let firstProfile = configs.first else {
+    private func fetchDailyNote(for profile: PZProfileSendable) async -> Result<DailyNoteProtocol, Error> {
+        await Task(priority: .background) {
+            try await profile.getDailyNote()
+        }.result
+    }
+
+    private func findProfile(for configuration: Intent) -> Result<PZProfileSendable, WidgetError> {
+        let allProfiles = PZWidgets.getAllProfiles()
+        guard let firstProfile = allProfiles.first else {
             print("Config is empty")
-            return makeFallbackResult(error: .noProfileFound)
+            return .failure(.noProfileFound)
         }
-
         guard let intent = configuration.account else {
             print("no account intent got")
-            guard configs.count == 1 else {
+            guard allProfiles.count == 1 else {
                 print("Need to choose account")
-                return makeFallbackResult(error: .profileSelectionNeeded)
+                return .failure(.profileSelectionNeeded)
             }
-            // 如果还未选择账号且只有一个账号，默认获取第一个
-            return await getTimelineEntries(config: firstProfile)
+            return .success(firstProfile)
         }
-
         let selectedAccountUUID = intent.id
         print("// [SELECTED WIDGET PROFILE] ", selectedAccountUUID, configuration)
-
-        let firstMatchedProfile = configs.first {
+        let firstMatchedProfile = allProfiles.first {
             $0.uuid.uuidString == selectedAccountUUID
         }
 
         guard let firstMatchedProfile else {
             // 有时候删除账号，Intent没更新就会出现这样的情况
             print("Need to choose account")
-            return makeFallbackResult(error: .profileSelectionNeeded)
+            return .failure(.profileSelectionNeeded)
         }
-
-        return await getTimelineEntries(config: firstMatchedProfile)
+        return .success(firstMatchedProfile)
     }
 }
--- WidgetExtension/Modules/TimelineProviders/LockScreenWidgetProvider.swift
+++ WidgetExtension/Modules/TimelineProviders/LockScreenWidgetProvider.swift
@@ -115,81 +115,97 @@ struct LockScreenWidgetProvider: AppIntentTimelineProvider {
         in context: Context
     ) async
         -> Timeline<Entry> {
-        let configs = PZWidgets.getAllProfiles()
+        var refreshTime = PZWidgets.getRefreshDate()
+        let entries: [Entry] = await getEntries(configuration: configuration, refreshTime: &refreshTime)
+        return Timeline(
+            entries: entries,
+            policy: .after(refreshTime)
+        )
+    }
 
-        func makeFallbackResult(error: WidgetError) -> Timeline<Entry> {
-            let entry = Entry(
-                date: PZWidgets.getRefreshDate(),
-                result: .failure(error),
-                accountUUIDString: nil
-            )
-            return Timeline<Entry>(
-                entries: [entry],
-                policy: .after(
-                    PZWidgets.getRefreshDate(isError: true)
-                )
-            )
-        }
+    // MARK: Private
 
-        func getTimelineEntries(
-            config: PZProfileSendable
-        ) async
-            -> Timeline<Entry> {
-            do {
-                let data = try await config.getDailyNote()
-                let entries = (0 ... 40).map { index in
-                    let timeInterval = TimeInterval(Double(index) * data.eachStaminaRecoveryTime)
-                    let entryDate = Date(timeIntervalSinceNow: timeInterval)
-                    return Entry(
-                        date: entryDate,
-                        result: .success(data),
-                        accountName: config.name,
-                        accountUUIDString: config.uuid.uuidString
-                    )
+    private func getEntries(configuration: Intent, refreshTime: inout Date) async -> [Entry] {
+        let findProfileResult = findProfile(for: configuration)
+        switch findProfileResult {
+        case let .success(profile):
+            let dailyNoteResult = await fetchDailyNote(for: profile)
+            switch dailyNoteResult {
+            case .success:
+                refreshTime = PZWidgets.getRefreshDate() // fetchDailyNote 的过程本身就会消耗时间，需要统计。
+                var tlEntryDate = Date.now
+
+                func boostDate() {
+                    tlEntryDate = tlEntryDate.addingTimeInterval(profile.game.eachStaminaRecoveryTime)
                 }
-                return .init(entries: entries, policy: .after(PZWidgets.getRefreshDate()))
-            } catch {
-                let entry = Entry(
-                    date: Date(),
-                    result: .failure(error),
-                    accountUUIDString: config.uuid.uuidString
-                )
-                return .init(
-                    entries: [entry], policy: .after(
-                        PZWidgets.getRefreshDate(isError: true)
+
+                var entries: [Entry] = []
+                while refreshTime > tlEntryDate {
+                    entries.append(
+                        Entry(
+                            date: tlEntryDate,
+                            result: dailyNoteResult,
+                            accountName: profile.name,
+                            accountUUIDString: profile.uuid.uuidString
+                        )
                     )
-                )
+                    boostDate()
+                }
+                return entries
+            case let .failure(error):
+                refreshTime = PZWidgets.getRefreshDate(isError: true) // fetchDailyNote 的过程本身就会消耗时间，需要统计。
+                return [
+                    Entry(
+                        date: Date(),
+                        result: .failure(error),
+                        accountName: profile.name,
+                        accountUUIDString: profile.uuid.uuidString
+                    ),
+                ]
             }
+        case let .failure(exception):
+            return [
+                Entry(
+                    date: Date(),
+                    result: .failure(exception),
+                    accountName: nil,
+                    accountUUIDString: nil
+                ),
+            ]
         }
+    }
 
-        guard let firstProfile = configs.first else {
+    private func fetchDailyNote(for profile: PZProfileSendable) async -> Result<DailyNoteProtocol, Error> {
+        await Task(priority: .background) {
+            try await profile.getDailyNote()
+        }.result
+    }
+
+    private func findProfile(for configuration: Intent) -> Result<PZProfileSendable, WidgetError> {
+        let allProfiles = PZWidgets.getAllProfiles()
+        guard let firstProfile = allProfiles.first else {
             print("Config is empty")
-            return makeFallbackResult(error: .noProfileFound)
+            return .failure(.noProfileFound)
         }
-
         guard let intent = configuration.account else {
             print("no account intent got")
-            guard configs.count == 1 else {
+            guard allProfiles.count == 1 else {
                 print("Need to choose account")
-                return makeFallbackResult(error: .profileSelectionNeeded)
+                return .failure(.profileSelectionNeeded)
             }
-            // 如果还未选择账号且只有一个账号，默认获取第一个
-            return await getTimelineEntries(config: firstProfile)
+            return .success(firstProfile)
         }
-
         let selectedAccountUUID = intent.id
         print("// [SELECTED WIDGET PROFILE] ", selectedAccountUUID, configuration)
-
-        let firstMatchedProfile = configs.first {
+        let firstMatchedProfile = allProfiles.first {
             $0.uuid.uuidString == selectedAccountUUID
         }
 
         guard let firstMatchedProfile else {
             // 有时候删除账号，Intent没更新就会出现这样的情况
             print("Need to choose account")
-            return makeFallbackResult(error: .profileSelectionNeeded)
+            return .failure(.profileSelectionNeeded)
         }
-
-        return await getTimelineEntries(config: firstMatchedProfile)
+        return .success(firstMatchedProfile)
     }
 }
