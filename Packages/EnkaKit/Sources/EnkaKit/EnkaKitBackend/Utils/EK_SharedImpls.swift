e2b761133d34c21c39f0088e761b9bf7d96b7a86
ShikiSuen <shikisuen@outlook.com>
2024-12-19 13:58:50 +0800

    EnkaKit & PZBaseKit // Migrating some Data extensions.

--- Packages/EnkaKit/Sources/EnkaKit/EnkaKitBackend/Utils/EK_SharedImpls.swift
+++ Packages/EnkaKit/Sources/EnkaKit/EnkaKitBackend/Utils/EK_SharedImpls.swift
@@ -124,25 +124,6 @@ extension Locale {
     }
 }
 
-// MARK: - Data Implementation
-
-extension Data {
-    public func parseAs<T: Decodable>(_ type: T.Type) throws -> T {
-        try JSONDecoder().decode(T.self, from: self)
-    }
-}
-
-extension Data? {
-    public func parseAs<T: Decodable>(_ type: T.Type) throws -> T? {
-        guard let this = self else { return nil }
-        return try JSONDecoder().decode(T.self, from: this)
-    }
-
-    public func assertedParseAs<T: Decodable>(_ type: T.Type) throws -> T {
-        try JSONDecoder().decode(T.self, from: self ?? .init([]))
-    }
-}
-
 extension Bundle {
     public static let enka = Bundle.module
 }
