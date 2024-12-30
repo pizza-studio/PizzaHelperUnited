c4a5acfe45bbb3f74867a1d82423cdb299015f13
ShikiSuen <shikisuen@outlook.com>
2024-12-30 19:04:04 +0800

    EnkaKit & PZHoYoLabKit // Change the cache method for HoYoLAB avatars.

--- Packages/EnkaKit/Sources/EnkaKit/EnkaKitBackend/QueriedModels_HoYoLab/HYQueriedModels.swift
+++ Packages/EnkaKit/Sources/EnkaKit/EnkaKitBackend/QueriedModels_HoYoLab/HYQueriedModels.swift
@@ -2,6 +2,7 @@
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
+import Foundation
 import PZBaseKit
 
 // MARK: - HYQueriedModels
@@ -19,4 +20,5 @@ public protocol HYQueriedAvatarProtocol: Identifiable, Equatable, AbleToCodeSend
     func summarize(theDB: DBType) -> Enka.AvatarSummarized?
     @MainActor
     static func getLocalHoYoAvatars(theDB: DBType, uid: String) -> [Enka.AvatarSummarized]
+    static func cacheLocalHoYoAvatars(uid: String, data: Data)
 }
