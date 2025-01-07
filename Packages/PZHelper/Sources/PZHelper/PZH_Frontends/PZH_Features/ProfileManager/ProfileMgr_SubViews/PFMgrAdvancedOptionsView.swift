d6728a568da8dae0e640a7e10a9e9d091bf18d46
ShikiSuen <shikisuen@outlook.com>
2025-01-07 23:05:10 +0800

    PZHelper // Allow switching the placement of PZProfileMO database.

---
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/PZH_Features/ProfileManager/ProfileMgr_SubViews/PFMgrAdvancedOptionsView.swift
@@ -0,0 +1,64 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+@preconcurrency import Defaults
+import PZAccountKit
+import PZBaseKit
+import SwiftUI
+
+struct PFMgrAdvancedOptionsView: View {
+    // MARK: Public
+
+    public static let navTitle = "settings.profile.advanced.navTitle".i18nPZHelper
+
+    public var body: some View {
+        Form {
+            Section {
+                Picker(selection: $situatePZProfileDBIntoGroupContainer) {
+                    Text(
+                        "settings.profile.advanced.dbSaveLocation.toGroupContainer",
+                        bundle: .module
+                    ).tag(true)
+                    Text(
+                        "settings.profile.advanced.dbSaveLocation.toAppContainer",
+                        bundle: .module
+                    ).tag(false)
+                } label: {
+                    Text(
+                        "settings.profile.advanced.dbSaveLocation.title",
+                        bundle: .module
+                    )
+                }
+                .onChange(of: situatePZProfileDBIntoGroupContainer) { _, _ in
+                    alertPresented.toggle()
+                }
+            } header: {
+                Text(verbatim: "SwiftData™").textCase(.none)
+            } footer: {
+                Text(
+                    "settings.profile.advanced.dbSaveLocation.footer",
+                    bundle: .module
+                )
+            }
+        }
+        .formStyle(.grouped)
+        .navigationTitle(Self.navTitle)
+        .navBarTitleDisplayMode(.large)
+        .alert(
+            "settings.disclaimer.requiringAppRebootToApplySettings".i18nPZHelper,
+            isPresented: $alertPresented
+        ) {
+            Button("sys.ok".i18nBaseKit) {
+                exit(0)
+            }
+        } message: {
+            Text("settings.profile.advanced.dbSaveLocation.restartRequired.description", bundle: .module)
+        }
+    }
+
+    // MARK: Private
+
+    @Default(.situatePZProfileDBIntoGroupContainer) private var situatePZProfileDBIntoGroupContainer: Bool
+    @State private var alertPresented: Bool = false
+}
