67b64c53665300a496383d588e262c5212f650b8
ShikiSuen <shikisuen@outlook.com>
2025-01-16 20:03:30 +0800

    Repo // Remove redundant preconcurrency tags.

--- Packages/PZKit/Sources/PZBaseKit/UserDefaultsKeys_Base.swift
+++ Packages/PZKit/Sources/PZBaseKit/UserDefaultsKeys_Base.swift
@@ -3,7 +3,7 @@
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
 import Defaults
-@preconcurrency import Foundation
+import Foundation
 
 extension UserDefaults {
     // 此处的 suiteName 得与 container ID 一致。
