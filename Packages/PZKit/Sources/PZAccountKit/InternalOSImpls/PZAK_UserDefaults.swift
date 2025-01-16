c42a465fdaa78dc4558196ee90175fa657393d7b
ShikiSuen <shikisuen@outlook.com>
2025-01-16 20:03:30 +0800

    Repo // Remove redundant preconcurrency tags.

--- Packages/PZKit/Sources/PZAccountKit/InternalOSImpls/PZAK_UserDefaults.swift
+++ Packages/PZKit/Sources/PZAccountKit/InternalOSImpls/PZAK_UserDefaults.swift
@@ -3,7 +3,7 @@
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
 import Defaults
-@preconcurrency import Foundation
+import Foundation
 import PZBaseKit
 
 extension UserDefaults {
