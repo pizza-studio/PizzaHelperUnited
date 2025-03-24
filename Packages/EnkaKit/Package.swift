fcf58fc487707905935b2f15a244eaa7b4f3fe9d
ShikiSuen <shikisuen@outlook.com>
2025-03-24 22:12:38 +0800

    SPM // Update ARDB for Genshin 5.5 update.

--- Packages/EnkaKit/Package.swift
+++ Packages/EnkaKit/Package.swift
@@ -27,7 +27,7 @@ let package = Package(
             url: "https://github.com/pizza-studio/EnkaDBGenerator", .upToNextMajor(from: "1.7.2")
         ),
         .package(
-            url: "https://github.com/pizza-studio/ArtifactRatingDB.git", .upToNextMajor(from: "1.0.9")
+            url: "https://github.com/pizza-studio/ArtifactRatingDB.git", .upToNextMajor(from: "1.1.0")
         ),
     ],
     targets: [
