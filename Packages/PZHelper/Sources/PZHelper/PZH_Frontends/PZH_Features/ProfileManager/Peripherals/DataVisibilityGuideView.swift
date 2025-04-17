1b9667f730be42efae5b82aaf9f658a5e5f4399e
ShikiSuen <shikisuen@outlook.com>
2025-04-17 15:01:41 +0800

    PZHelper // Add guides regarding HoYoLAB privacy settings.

---
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/PZH_Features/ProfileManager/Peripherals/DataVisibilityGuideView.swift
@@ -0,0 +1,24 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import PZAccountKit
+import PZBaseKit
+import SwiftUI
+
+public struct DataVisibilityGuideView: View {
+    public let region: HoYo.AccountRegion
+
+    public var body: some View {
+        VStack(alignment: .leading) {
+            Text("profile.accountConnectivity.insufficientDataVisibility".i18nPZHelper)
+                .font(.footnote)
+            switch region {
+            case .hoyoLab:
+                Image("DataVisibilityGuide_HoYoLAB", bundle: .module).resizable().aspectRatio(contentMode: .fit)
+            case .miyoushe:
+                Image("DataVisibilityGuide_Miyoushe", bundle: .module).resizable().aspectRatio(contentMode: .fit)
+            }
+        }
+    }
+}
