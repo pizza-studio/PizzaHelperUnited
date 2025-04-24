78919e2f3134839e83056654016af0b54c216ca8
ShikiSuen <shikisuen@outlook.com>
2025-04-24 21:11:19 +0800

    Widgets // Add SelectDualProfileIntent.

--- WidgetExtension/Modules/Intents/AppIntents.swift
+++ WidgetExtension/Modules/Intents/AppIntents.swift
@@ -98,6 +98,70 @@ public struct SelectAccountIntent: AppIntent, WidgetConfigurationIntent, CustomI
     }
 }
 
+// MARK: - SelectDualProfileIntent
+
+// Only for iOS Springboard Widgets and macOS Desktop Widgets.
+// 该 Intent 允许指定两个本地帐号。
+public struct SelectDualProfileIntent: AppIntent, WidgetConfigurationIntent {
+    // MARK: Lifecycle
+
+    public init() {}
+
+    // MARK: Public
+
+    public static let title: LocalizedStringResource = "intent.name.selectLocalProfile"
+    public static let description = IntentDescription("intent.description.pickTheLocalProfileForThisWidget")
+
+    public static var isDiscoverable: Bool { false }
+
+    public static var parameterSummary: some ParameterSummary {
+        When(\.$randomBackground, .equalTo, true) {
+            // Omitting `chosenBackgrounds`.
+            Summary {
+                \.$profileSlot1
+                \.$profileSlot2
+                \.$isDarkModeRespected
+                \.$echoOfWarDisplayMethod
+                \.$trounceBlossomDisplayMethod
+                \.$showTransformer
+                \.$showMaterialsInLargeSizeWidget
+            }
+        } otherwise: {
+            Summary()
+        }
+    }
+
+    @Parameter(title: "intent.field.localeProfile.slot1") public var profileSlot1: AccountIntentAppEntity?
+    @Parameter(title: "intent.field.localeProfile.slot2") public var profileSlot2: AccountIntentAppEntity?
+
+    @Parameter(title: "intent.field.useRandomWallpaper", default: false) public var randomBackground: Bool
+
+    /// This property, as an array with typed contents, is not inheritable from SiriKit Intents.
+    /// If not changing the field name to a new one, the previous data will hinder this property
+    /// from being configured by the user. Hence the change from `background` to `chosenBackgrounds`.
+    @Parameter(title: "intent.field.wallpaper", default: []) public var chosenBackgrounds: [WidgetBackgroundAppEntity]
+
+    @Parameter(title: "intent.field.followSystemDarkMode", default: true) public var isDarkModeRespected: Bool
+
+    @Parameter(title: "intent.field.echoOfWarDisplayMethod", default: .alwaysShow)
+    public var echoOfWarDisplayMethod: WeeklyBossesDisplayMethodAppEnum
+
+    @Parameter(title: "intent.field.trounceBlossomDisplayMethod", default: .alwaysShow)
+    public var trounceBlossomDisplayMethod: WeeklyBossesDisplayMethodAppEnum
+
+    @Parameter(title: "intent.field.showTransformer", default: true) public var showTransformer: Bool
+
+    @Parameter(
+        title: "intent.field.showMaterialsInLargeSizeWidget",
+        default: true
+    ) public var showMaterialsInLargeSizeWidget: Bool
+
+    public func perform() async throws -> some IntentResult {
+        // TODO: Place your refactored intent handler code here.
+        .result()
+    }
+}
+
 // MARK: - SelectAccountAndShowWhichInfoIntent
 
 // Only for watchOS and iOS_Lock_Screen.
