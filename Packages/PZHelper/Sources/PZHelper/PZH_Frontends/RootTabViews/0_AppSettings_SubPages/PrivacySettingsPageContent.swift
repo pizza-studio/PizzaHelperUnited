424d28fed6a350b48f93ccd70eca45baf456800c
ShikiSuen <shikisuen@outlook.com>
2025-05-24 18:38:53 +0800

    Repo // Deprecating abyss record commission & collection feature.

--- Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootTabViews/0_AppSettings_SubPages/PrivacySettingsPageContent.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/RootTabViews/0_AppSettings_SubPages/PrivacySettingsPageContent.swift
@@ -8,69 +8,12 @@ import PZHoYoLabKit
 import SwiftUI
 
 struct PrivacySettingsPageContent: View {
-    // MARK: Internal
-
     var body: some View {
         List {
-            Section {
-                Toggle(isOn: $allowAbyssDataCollection) {
-                    Text("settings.privacy.abyssDataCollect".i18nPZHelper)
-                }
-            } footer: {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("settings.privacy.abyssDataCollect.detail".i18nPZHelper)
-                        .font(.footnote)
-                        .multilineTextAlignment(.leading)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    NavigationLink {
-                        WebBrowserView(url: Self.privacyFAQURL.absoluteString)
-                            .navigationTitle("FAQ")
-                            .navBarTitleDisplayMode(.inline)
-                    } label: {
-                        Text("settings.privacy.abyssDataCollect.faqLink".i18nPZHelper)
-                            .font(.footnote)
-                            .multilineTextAlignment(.leading)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-
-            Section {
-                Toggle(isOn: $enforceReservedUserName.animation()) {
-                    Text("settings.privacy.SnapHutao.enforceUsername.title", bundle: .module)
-                }
-                if $enforceReservedUserName.animation().wrappedValue {
-                    let theLabel = Text("settings.privacy.SnapHutao.username.title", bundle: .module)
-                    TextField(
-                        "settings.privacy.SnapHutao.username.title".i18nPZHelper,
-                        text: $reservedUserNameForSnapHutao,
-                        prompt: theLabel
-                    )
-                    .fontDesign(.monospaced)
-                }
-            } header: {
-                Text(verbatim: "Snap Hutao").textCase(.none)
-            } footer: {
-                Text("settings.privacy.SnapHutao.footer", bundle: .module)
-            }
+            Text("settings.privacy.noOptionsAvailable", bundle: .module)
+                .asInlineTextDescription()
         }
         .navigationTitle("settings.privacy.title".i18nPZHelper)
         .navBarTitleDisplayMode(.large)
     }
-
-    // MARK: Private
-
-    private static let privacyFAQURL: URL = {
-        switch Bundle.main.preferredLocalizations.first?.prefix(2) {
-        case "zh": "https://gi.pizzastudio.org/static/faq_abyss.html".asURL
-        case "en": "https://gi.pizzastudio.org/static/faq_abyss_en.html".asURL
-        case "ja": "https://gi.pizzastudio.org/static/faq_abyss_ja.html".asURL
-        default: "https://gi.pizzastudio.org/static/faq_abyss_en.html".asURL
-        }
-    }()
-
-    @Default(.allowAbyssDataCollection) private var allowAbyssDataCollection: Bool
-    @Default(.reservedUserNameForSnapHutao) private var reservedUserNameForSnapHutao: String
-    @Default(.enforceReservedUserNameForSnapHutaoSubmission) private var enforceReservedUserName: Bool
 }
