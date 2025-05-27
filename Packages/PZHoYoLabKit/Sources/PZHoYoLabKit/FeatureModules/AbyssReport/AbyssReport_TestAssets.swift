05bb0ca6283394e4c0a7eec3c818e7aff5efc79a
ShikiSuen <shikisuen@outlook.com>
2025-05-27 17:19:57 +0800

    PZHoYoLABKit // Add new HSR TL data types (and their APIs).

--- Packages/PZHoYoLabKit/Sources/PZHoYoLabKit/FeatureModules/AbyssReport/AbyssReport_TestAssets.swift
+++ Packages/PZHoYoLabKit/Sources/PZHoYoLabKit/FeatureModules/AbyssReport/AbyssReport_TestAssets.swift
@@ -10,8 +10,12 @@ import PZBaseKit
 enum AbyssReportTestAssets: String {
     case giCurr = "abyssReport_sample_gi_curr"
     case giPrev = "abyssReport_sample_gi_prev"
-    case hsrCurr = "abyssReport_sample_hsr_fh_curr"
-    case hsrPrev = "abyssReport_sample_hsr_fh_prev"
+    case hsrFHCurr = "abyssReport_sample_hsr_fh_curr"
+    case hsrFHPrev = "abyssReport_sample_hsr_fh_prev"
+    case hsrASCurr = "abyssReport_sample_hsr_as_curr"
+    case hsrASPrev = "abyssReport_sample_hsr_as_prev"
+    case hsrPFCurr = "abyssReport_sample_hsr_pf_curr"
+    case hsrPFPrev = "abyssReport_sample_hsr_pf_prev"
 
     // MARK: Internal
 
@@ -27,16 +31,28 @@ enum AbyssReportTestAssets: String {
         return try! Data(contentsOf: exampleURL)
     }
 
-    func getReport4GI(isPrev: Bool = false) throws -> HoYo.AbyssReport4GI {
-        try rawData.parseAs(HoYo.AbyssReport4GI.self)
+    static func getReport4HSR(isPrev: Bool = false) throws -> HoYo.AbyssReport4HSR {
+        var step = 0
+        do {
+            let dataForgottenHall = try hsrFHCurr.rawData.parseAs(HoYo.AbyssReport4HSR.ForgottenHallData.self)
+            step += 1
+            let dataPureFiction = try hsrPFCurr.rawData.parseAs(HoYo.AbyssReport4HSR.PureFictionData.self)
+            step += 1
+            let dataApocalypticShadow = try hsrASCurr.rawData.parseAs(HoYo.AbyssReport4HSR.ApocalypticShadowData.self)
+            step += 1
+            let result = HoYo.AbyssReport4HSR(
+                forgottenHall: dataForgottenHall,
+                pureFiction: dataPureFiction,
+                apocalypticShadow: dataApocalypticShadow
+            )
+            return result
+        } catch {
+            throw error
+        }
     }
 
-    func getReport4HSR(isPrev: Bool = false) throws -> HoYo.AbyssReport4HSR {
-        let fhData = try rawData.parseAs(HoYo.AbyssReport4HSR.ForgottenHallData.self)
-        let result = HoYo.AbyssReport4HSR(
-            forgottenHall: fhData
-        )
-        return result
+    func getReport4GI(isPrev: Bool = false) throws -> HoYo.AbyssReport4GI {
+        try rawData.parseAs(HoYo.AbyssReport4GI.self)
     }
 }
 
