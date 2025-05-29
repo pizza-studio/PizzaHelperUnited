c5eefe7669f0a29e4cc254f1a03234bd619e0f2d
ShikiSuen <shikisuen@outlook.com>
2025-05-29 21:29:29 +0800

    PZAccountKit // Add the API for alternative ZZZ realtime note.

--- Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/NoteAPI4ZZZ.swift
+++ Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/NoteAPI4ZZZ.swift
@@ -8,7 +8,7 @@ import PZBaseKit
 
 extension HoYo {
     public static func note4ZZZ(profile: PZProfileSendable) async throws -> Note4ZZZ {
-        try await getNote4ZZZ(
+        try await note4ZZZ(
             uidWithGame: profile.uidWithGame,
             server: profile.server,
             uid: profile.uid,
@@ -18,7 +18,65 @@ extension HoYo {
         )
     }
 
-    static func getNote4ZZZ(
+    /// Fetches the daily note of the specified user.
+    ///
+    /// - Parameter server: The server where the user's account exists.
+    /// - Parameter uid: The uid of the user whose daily note to fetch.
+    /// - Parameter cookie: The cookie of the user. This is used for authentication purposes.
+    ///
+    /// - Throws: An error of type `MiHoYoAPIError` if an error occurs while making the API request.
+    ///
+    /// - Returns: An instance of `Note4ZZZ` that represents the user's daily note.
+    static func note4ZZZ(
+        uidWithGame: String,
+        server: Server,
+        uid: String,
+        cookie: String,
+        deviceFingerPrint: String?,
+        deviceID: String?
+    ) async throws
+        -> Note4ZZZ {
+        switch server.region {
+        case .miyoushe:
+            let firstAttempt = try? await fullNote4ZZZ(
+                uidWithGame: uidWithGame,
+                server: server,
+                uid: uid,
+                cookie: cookie,
+                deviceFingerPrint: deviceFingerPrint,
+                deviceID: deviceID
+            )
+            if let firstAttempt { return firstAttempt }
+            if cookie.contains("stoken=v2_") {
+                return try await widgetNote4ZZZ(
+                    uidWithGame: uidWithGame,
+                    cookie: cookie,
+                    deviceFingerPrint: deviceFingerPrint,
+                    deviceID: deviceID
+                )
+            } else {
+                throw MiHoYoAPIError.sTokenV2InvalidOrMissing
+            }
+        case .hoyoLab:
+            return try await fullNote4ZZZ(
+                uidWithGame: uidWithGame,
+                server: server,
+                uid: uid,
+                cookie: cookie,
+                deviceFingerPrint: deviceFingerPrint,
+                deviceID: deviceID
+            )
+        }
+    }
+
+    /// Fetches the daily note of the specified user.
+    ///
+    /// - Parameter server: The server where the user's account exists.
+    /// - Parameter uid: The uid of the user whose daily note to fetch.
+    /// - Parameter cookie: The cookie of the user. This is used for authentication purposes.
+    ///
+    /// - Throws: An error of type `MiHoYoAPI.Error` if an error occurs while making the API request.
+    static func fullNote4ZZZ(
         uidWithGame: String,
         server: Server,
         uid: String,
@@ -56,6 +114,40 @@ extension HoYo {
 
         let data = try await request.serializingData().value
         return try .decodeFromMiHoYoAPIJSONResult(data: data, debugTag: "HoYo.getNote4ZZZ") {
+            Note4ZZZ.CacheSputnik.cache(data, uidWithGame: uidWithGame)
+        }
+    }
+
+    /// Fetches the daily note of the specified user. Using widget api.
+    /// - Parameters:
+    ///   - cookie: The cookie of the user.
+    ///   - deviceFingerPrint: The device finger print of the user.
+    static func widgetNote4ZZZ(
+        uidWithGame: String,
+        cookie: String,
+        deviceFingerPrint: String?,
+        deviceID: String?
+    ) async throws
+        -> Note4ZZZ {
+        var additionalHeaders = [
+            "User-Agent": "WidgetExtension/434 CFNetwork/1492.0.1 Darwin/23.3.0",
+        ]
+
+        if let deviceFingerPrint, !deviceFingerPrint.isEmpty, let deviceID {
+            additionalHeaders.updateValue(deviceFingerPrint, forKey: "x-rpc-device_fp")
+            additionalHeaders.updateValue(deviceID, forKey: "x-rpc-device_id")
+        }
+
+        let request = try await Self.generateRecordAPIRequest(
+            region: .miyoushe(.starRail),
+            path: "/event/game_record_zzz/api/zzz/widget",
+            queryItems: [],
+            cookie: cookie,
+            additionalHeaders: additionalHeaders
+        )
+
+        let data = try await request.serializingData().value
+        return try .decodeFromMiHoYoAPIJSONResult(data: data, debugTag: "HoYo.widgetNote4ZZZ()") {
             Note4ZZZ.CacheSputnik.cache(data, uidWithGame: uidWithGame)
         }
     }
