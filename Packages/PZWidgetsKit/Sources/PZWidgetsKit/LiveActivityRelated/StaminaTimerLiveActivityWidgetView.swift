5868ecf4d0386abe5f43269d8f6eb66677d36d6e
ShikiSuen <shikisuen@outlook.com>
2025-06-03 19:28:07 +0800

    Widgets // Refactor StaminaTimerLiveActivityWidgetView.

--- Packages/PZWidgetsKit/Sources/PZWidgetsKit/LiveActivityRelated/StaminaTimerLiveActivityWidgetView.swift
+++ Packages/PZWidgetsKit/Sources/PZWidgetsKit/LiveActivityRelated/StaminaTimerLiveActivityWidgetView.swift
@@ -24,42 +24,8 @@ public struct StaminaTimerLiveActivityWidgetView<RendererIntent: AppIntent, Refr
         let mainContent = contentView
         #if !os(watchOS)
             .background {
-                if let userWallpaperOverride, context.state.background != .noBackground {
-                    userWallpaperOverride
-                        .resizable()
-                        .scaledToFill()
-                    Color.black
-                        .opacity(0.3)
-                } else {
-                    Group {
-                        let randomCardBg: Image = (
-                            BundledWallpaper.allCases(
-                                for: context.state.game
-                            ).randomElement() ?? .defaultValue()
-                        )
-                        .image4LiveActivity
-                        switch context.state.background {
-                        case .random:
-                            randomCardBg
-                                .resizable()
-                                .scaledToFill()
-                            Color.black
-                                .opacity(0.3)
-                        case .customize:
-                            let chosenCardBackgrounds = BundledWallpaper.allCases.filter { wallpaper in
-                                backgroundIDs.contains(wallpaper.assetName4LiveActivity)
-                            }
-                            (chosenCardBackgrounds.randomElement()?.image4LiveActivity ?? randomCardBg)
-                                .resizable()
-                                .scaledToFill()
-                            Color.black
-                                .opacity(0.3)
-                        case .noBackground:
-                            EmptyView()
-                        }
-                    }
-                    .scaleEffect(1.01) // HSR 的名片有光边。
-                }
+                LiveActivityWallpaperView(game: context.state.game)
+                    .scaleEffect(context.state.game == .starRail ? 1.01 : 1) // HSR 的名片有光边。
             }
         #endif
             .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -72,10 +38,6 @@ public struct StaminaTimerLiveActivityWidgetView<RendererIntent: AppIntent, Refr
     }
 
     // MARK: Internal
-
-    var backgroundIDs: [String] {
-        backgrounds4LiveActivity.map(\.assetName4LiveActivity)
-    }
 
     var useNoBackground: Bool { context.state.background == .noBackground }
 
@@ -166,19 +128,6 @@ public struct StaminaTimerLiveActivityWidgetView<RendererIntent: AppIntent, Refr
     // MARK: Private
 
     @State private var context: ActivityViewContext<LiveActivityAttributes>
-
-    @Default(.backgrounds4LiveActivity) private var backgrounds4LiveActivity: Set<BundledWallpaper>
-    @Default(.userWallpapers4LiveActivity) private var userWallpaperIDs4LiveActivity: Set<String>
-
-    private var userWallpapers4LiveActivity: Set<UserWallpaper> {
-        .init(defaultsValueIDs: userWallpaperIDs4LiveActivity)
-    }
-
-    private var userWallpaperOverride: Image? {
-        let cgImage = userWallpapers4LiveActivity.randomElement()?.imageHorizontal
-        guard let cgImage else { return nil }
-        return Image(decorative: cgImage, scale: 1, orientation: .up)
-    }
 }
 
 #endif
