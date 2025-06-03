261d950b4149a56e9aa48cc5e4dfb9b89b46e523
ShikiSuen <shikisuen@outlook.com>
2025-06-03 19:25:31 +0800

    WPKit // +LiveActivityWallpaperView.

--- Packages/WallpaperKit/Sources/WallpaperKit/PublicViews/LiveActivityWallpaperView.swift
+++ Packages/WallpaperKit/Sources/WallpaperKit/PublicViews/LiveActivityWallpaperView.swift
@@ -12,3 +12,86 @@ extension BundledWallpaper {
         Image(assetName4LiveActivity, bundle: .module)
     }
 }
+
+// MARK: - LiveActivityWallpaperView
+
+public struct LiveActivityWallpaperView: View {
+    // MARK: Lifecycle
+
+    public init(game: Pizza.SupportedGame? = nil) {
+        self.game = game
+    }
+
+    // MARK: Public
+
+    public enum BackgroundSettings: AbleToCodeSendHash, Equatable {
+        case noBackground
+        case multiple(Set<Wallpaper>)
+    }
+
+    public var body: some View {
+        switch currentSettings {
+        case .noBackground: EmptyView()
+        case let .multiple(wallpapers):
+            let finalWallpaper = getRandomWallpaper(wallpapers)
+            let image = getRawImage(finalWallpaper)
+            image
+                .resizable()
+                .scaledToFill()
+            Color.black
+                .opacity(0.3)
+        }
+    }
+
+    // MARK: Private
+
+    @Environment(\.colorScheme) private var colorScheme
+
+    @Default(.liveActivityWallpaperIDs) private var liveActivityWallpaperIDs: Set<String>
+
+    private let game: Pizza.SupportedGame?
+
+    private var currentSettings: BackgroundSettings {
+        let ids = liveActivityWallpaperIDs
+        if ids.contains(Wallpaper.nullLiveActivityWallpaperIdentifier) { return .noBackground }
+        var idsToRemove: Set<String> = []
+        let mapped: [Wallpaper] = ids.compactMap { idStr in
+            Wallpaper(id: idStr) {
+                idsToRemove.insert(idStr)
+            }
+        }
+        defer {
+            liveActivityWallpaperIDs = ids.subtracting(idsToRemove)
+        }
+        return .multiple(.init(mapped))
+    }
+
+    private func getRandomWallpaper(_ rawSet: Set<Wallpaper>) -> Wallpaper {
+        var wallpapersReturnable = rawSet
+        if rawSet.isEmpty {
+            wallpapersReturnable = .init(Wallpaper.allCases)
+        }
+        if rawSet.isEmpty, let game {
+            wallpapersReturnable = wallpapersReturnable.filter { currentWallpaper in
+                switch currentWallpaper {
+                case .user: return true
+                case let .bundled(bundledOne): return bundledOne.game == game
+                }
+            }
+        }
+        return wallpapersReturnable.randomElement() ?? .bundled(BundledWallpaper.defaultValue(for: game))
+    }
+
+    private func getRawImage(_ targetWallpaper: Wallpaper) -> Image {
+        switch targetWallpaper {
+        case let .bundled(bundledWallpaper):
+            bundledWallpaper.image4LiveActivity
+        case let .user(userWallpaper):
+            if let cgImage = userWallpaper.imageHorizontal {
+                Image(decorative: cgImage, scale: 1, orientation: .up)
+            } else {
+                Image(systemSymbol: .trashSlashSquare)
+            }
+        }
+    }
+}
