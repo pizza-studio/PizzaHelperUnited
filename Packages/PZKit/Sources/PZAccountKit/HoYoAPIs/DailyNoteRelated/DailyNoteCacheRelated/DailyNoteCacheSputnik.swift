2ca13b5381768b8792a81c3963f7f3477f6585f4
ShikiSuen <shikisuen@outlook.com>
2025-06-04 18:08:25 +0800

    PZAccountKit // Extend DailyNoteProtocol with image cache APIs.

--- Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/DailyNoteCacheRelated/DailyNoteCacheSputnik.swift
+++ Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/DailyNoteCacheRelated/DailyNoteCacheSputnik.swift
@@ -2,6 +2,7 @@
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
+import CoreGraphics
 import Defaults
 import Foundation
 import PZBaseKit
@@ -34,5 +35,131 @@ public struct DailyNoteCacheSputnik<T: DailyNoteProtocol> {
             data: data, debugTag: "DailyNoteCacheSputnik.getCache()"
         )
         return decoded
+    }
+}
+
+extension DailyNoteProtocol {
+    public func getExpeditionAssetMap() async -> [URL: SendableImagePtr]? {
+        let dailyNote = self
+        guard dailyNote.hasExpeditions else { return nil }
+        let expeditions = dailyNote.expeditionTasks
+        guard !expeditions.isEmpty else { return nil }
+        var assetMap = [URL: SendableImagePtr]()
+        if dailyNote.hasExpeditions {
+            for task in dailyNote.expeditionTasks {
+                let urls = [task.iconURL, task.iconURL4Copilot].compactMap { $0 }
+                for url in urls {
+                    if let image = await ImageMap.shared.assetMap[url] {
+                        assetMap[url] = image
+                    } else if var cgImage = CGImage.instantiate(url: url) {
+                        genshinSpecificHandling: if dailyNote.game == .genshinImpact {
+                            let croppedCGImage = cgImage.croppedPilotPhoto4Genshin()
+                            guard let croppedCGImage else {
+                                continue
+                            }
+                            cgImage = croppedCGImage
+                        }
+                        let image = SendableImagePtr(img: .init(decorative: cgImage, scale: 1.0))
+                        await ImageMap.shared.insertValue(url: url, image: image)
+                        assetMap[url] = image
+                    }
+                }
+            }
+        }
+        return assetMap
+    }
+
+    @MainActor
+    public func getExpeditionAssetMapFromMainActor() -> [URL: SendableImagePtr]? {
+        let dailyNote = self
+        guard dailyNote.hasExpeditions else { return nil }
+        let expeditions = dailyNote.expeditionTasks
+        guard !expeditions.isEmpty else { return nil }
+        var assetMap = [URL: SendableImagePtr]()
+        if dailyNote.hasExpeditions {
+            for task in dailyNote.expeditionTasks {
+                let urls = [task.iconURL, task.iconURL4Copilot].compactMap { $0 }
+                for url in urls {
+                    if let image = ImageMap.shared.assetMap[url] {
+                        assetMap[url] = image
+                    } else if var cgImage = CGImage.instantiate(url: url) {
+                        genshinSpecificHandling: if dailyNote.game == .genshinImpact {
+                            let croppedCGImage = cgImage.croppedPilotPhoto4Genshin()
+                            guard let croppedCGImage else {
+                                continue
+                            }
+                            cgImage = croppedCGImage
+                        }
+                        let image = SendableImagePtr(img: .init(decorative: cgImage, scale: 1.0))
+                        ImageMap.shared.insertValue(url: url, image: image)
+                        assetMap[url] = image
+                    }
+                }
+            }
+        }
+        return assetMap
+    }
+
+    /// 这是给 .placeholder() 专用的函式，不会调用 ImageMap 的缓存。
+    public func getExpeditionAssetMapImmediately()
+        -> [URL: SendableImagePtr]? {
+        let dailyNote = self
+        guard dailyNote.hasExpeditions else { return nil }
+        let expeditions = dailyNote.expeditionTasks
+        guard !expeditions.isEmpty else { return nil }
+        var assetMap = [URL: SendableImagePtr]()
+        if dailyNote.hasExpeditions {
+            for task in dailyNote.expeditionTasks {
+                let urls = [task.iconURL, task.iconURL4Copilot].compactMap { $0 }
+                for url in urls {
+                    if var cgImage = CGImage.instantiate(url: url) {
+                        genshinSpecificHandling: if dailyNote.game == .genshinImpact {
+                            let croppedCGImage = cgImage.croppedPilotPhoto4Genshin()
+                            guard let croppedCGImage else {
+                                continue
+                            }
+                            cgImage = croppedCGImage
+                        }
+                        let image = SendableImagePtr(img: .init(decorative: cgImage, scale: 1.0))
+                        assetMap[url] = image
+                    }
+                }
+            }
+        }
+        return assetMap
+    }
+}
+
+extension Array where Element == (any DailyNoteProtocol) {
+    public func prepareAssetMap() async -> [URL: SendableImagePtr]? {
+        var assetMap: [URL: SendableImagePtr]? = [:]
+        for currentDailyNote in self {
+            let fetchedMap = await Task(priority: .userInitiated) {
+                await currentDailyNote.getExpeditionAssetMap()
+            }.value
+            guard let fetchedMap else { continue }
+            fetchedMap.forEach { theKey, theValue in
+                assetMap?[theKey] = theValue
+            }
+        }
+        if assetMap?.isEmpty ?? false {
+            assetMap = nil
+        }
+        return assetMap
+    }
+
+    /// 这是给 .placeholder() 专用的函式，不会调用 ImageMap 的缓存。
+    public func prepareAssetMapImmediately() -> [URL: SendableImagePtr]? {
+        var assetMap: [URL: SendableImagePtr]? = [:]
+        for currentDailyNote in self {
+            let fetchedMap = currentDailyNote.getExpeditionAssetMapImmediately()
+            fetchedMap?.forEach { theKey, theValue in
+                assetMap?[theKey] = theValue
+            }
+        }
+        if assetMap?.isEmpty ?? false {
+            assetMap = nil
+        }
+        return assetMap
     }
 }
