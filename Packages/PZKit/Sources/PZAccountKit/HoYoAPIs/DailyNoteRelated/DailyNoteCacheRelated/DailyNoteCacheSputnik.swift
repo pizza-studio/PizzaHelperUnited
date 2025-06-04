78b151daeac26024ed83922e982a431fdedbc234
ShikiSuen <shikisuen@outlook.com>
2025-06-04 17:59:28 +0800

    PZBaseKit // Inherit CachedJSON from PZAccountKit.

--- Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/DailyNoteCacheRelated/DailyNoteCacheSputnik.swift
+++ Packages/PZKit/Sources/PZAccountKit/HoYoAPIs/DailyNoteRelated/DailyNoteCacheRelated/DailyNoteCacheSputnik.swift
@@ -36,21 +36,3 @@ public struct DailyNoteCacheSputnik<T: DailyNoteProtocol> {
         return decoded
     }
 }
-
-// MARK: - CachedJSON
-
-public struct CachedJSON: AbleToCodeSendHash, Defaults.Serializable {
-    // MARK: Lifecycle
-
-    public init(rawJSONString: String, timestamp: TimeInterval? = nil) {
-        self.rawJSONString = rawJSONString
-        self.timestamp = timestamp ?? Date.now.timeIntervalSince1970
-    }
-
-    // MARK: Public
-
-    public let rawJSONString: String
-    public let timestamp: TimeInterval
-
-    public var cachedTime: Date { .init(timeIntervalSince1970: timestamp) }
-}
---
+++ Packages/PZKit/Sources/PZBaseKit/OSImpl/CachedJSON.swift
@@ -0,0 +1,24 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Defaults
+import Foundation
+
+// MARK: - CachedJSON
+
+public struct CachedJSON: AbleToCodeSendHash, Defaults.Serializable {
+    // MARK: Lifecycle
+
+    public init(rawJSONString: String, timestamp: TimeInterval? = nil) {
+        self.rawJSONString = rawJSONString
+        self.timestamp = timestamp ?? Date.now.timeIntervalSince1970
+    }
+
+    // MARK: Public
+
+    public let rawJSONString: String
+    public let timestamp: TimeInterval
+
+    public var cachedTime: Date { .init(timeIntervalSince1970: timestamp) }
+}
