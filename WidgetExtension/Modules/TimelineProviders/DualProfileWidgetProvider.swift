e9a9b8cca90ec8d8a29bd7e12527134d20e2c0f5
ShikiSuen <shikisuen@outlook.com>
2025-06-04 17:59:47 +0800

    Widgets // Remove duplicated copyright header.

--- WidgetExtension/Modules/TimelineProviders/DualProfileWidgetProvider.swift
+++ WidgetExtension/Modules/TimelineProviders/DualProfileWidgetProvider.swift
@@ -1,9 +1,6 @@
 // (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
-// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
-// ====================
-// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
 import CoreGraphics
 import Defaults
