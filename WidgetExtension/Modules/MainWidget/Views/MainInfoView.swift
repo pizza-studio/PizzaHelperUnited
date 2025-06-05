cbc88636cd5eb884ca083efc51fc3e5e1db9d27f
ShikiSuen <shikisuen@outlook.com>
2025-06-05 16:37:31 +0800

    Widgets // Migrate ProfileAndMainStaminaView to PZWidgetsKit.

--- WidgetExtension/Modules/MainWidget/Views/MainInfoView.swift
+++ WidgetExtension/Modules/MainWidget/Views/MainInfoView.swift
@@ -19,55 +19,12 @@ struct MainInfo: View {
     let dailyNote: any DailyNoteProtocol
     let viewConfig: WidgetViewConfiguration
     let accountName: String?
-    let accountNameTest = "account.manage.title"
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            if let accountName = accountName {
-                HStack(alignment: .lastTextBaseline, spacing: 2) {
-                    Image(systemSymbol: .personFill)
-                        .legibilityShadow(isText: false)
-                    Text(accountName)
-                        .allowsTightening(true)
-                        .lineLimit(1)
-                        .fixedSize()
-                        .minimumScaleFactor(0.5)
-                        .legibilityShadow()
-                }
-                .font(.footnote)
-                .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
-                Spacer()
-            }
-
-            HStack(alignment: .firstTextBaseline, spacing: 2) {
-                let staminaIntel = dailyNote.staminaIntel
-                Text(staminaIntel.finished.description)
-                    .font(.system(size: 50, design: .rounded))
-                    .fontWeight(.medium)
-                    .minimumScaleFactor(0.1)
-                    .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
-                    .legibilityShadow()
-                dailyNote.game.primaryStaminaAssetIcon
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 30)
-                    .alignmentGuide(.firstTextBaseline) { context in
-                        context[.bottom] - 0.17 * context.height
-                    }
-                    .legibilityShadow(isText: false)
-            }
-            Spacer()
-            HStack {
-                Button(intent: WidgetRefreshIntent(dailyNoteUIDWithGame: entry.profile?.uidWithGame)) {
-                    Image(systemSymbol: .arrowClockwiseCircle)
-                        .font(.title3)
-                        .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
-                        .clipShape(.circle)
-                }
-                .buttonStyle(.plain)
-                .legibilityShadow()
-                RecoveryTimeText(entry: entry, data: dailyNote)
-            }
-        }
+        ProfileAndMainStaminaView(
+            profile: entry.profile,
+            dailyNote: dailyNote,
+            refreshIntent: WidgetRefreshIntent(dailyNoteUIDWithGame: entry.profile?.uidWithGame)
+        )
     }
 }
