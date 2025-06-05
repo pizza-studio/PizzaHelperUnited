62d7322df301da238d55440611db2dca45088207
ShikiSuen <shikisuen@outlook.com>
2025-06-05 23:20:31 +0800

    Widgets // Refactoring LargeWidgetView.

--- WidgetExtension/Modules/MainWidget/Views/LargeWidgetView.swift
+++ WidgetExtension/Modules/MainWidget/Views/LargeWidgetView.swift
@@ -13,23 +13,63 @@ import WidgetKit
 
 @available(watchOS, unavailable)
 struct LargeWidgetView: View {
-    let entry: MainWidgetProvider.Entry
-    @Environment(\.widgetFamily) var family: WidgetFamily
-    var dailyNote: any DailyNoteProtocol
-    let viewConfig: WidgetViewConfiguration
-    let accountName: String?
-    let events: [EventModel]
+    // MARK: Lifecycle
 
-    var body: some View {
+    public init(
+        entry: MainWidgetProvider.Entry,
+        dailyNote: any DailyNoteProtocol,
+        viewConfig: WidgetViewConfiguration,
+        accountName: String?,
+        events: [EventModel]
+    ) {
+        self.entry = entry
+        self.dailyNote = dailyNote
+        self.viewConfig = viewConfig
+        self.accountName = accountName
+        self.events = events
+    }
+
+    // MARK: Public
+
+    public var body: some View {
+        switch viewConfig.useTinyGlassDisplayStyle {
+        case false: viewWhenTinyGlassModeIsOff
+        case true: viewWhenTinyGlassModeIsON
+        }
+    }
+
+    // MARK: Private
+
+    @Environment(\.widgetFamily) private var family: WidgetFamily
+
+    private let entry: MainWidgetProvider.Entry
+    private let dailyNote: any DailyNoteProtocol
+    private let viewConfig: WidgetViewConfiguration
+    private let accountName: String?
+    private let events: [EventModel]
+}
+
+// MARK: - View used when Tiny Glass Display Style is OFF.
+
+@available(watchOS, unavailable)
+extension LargeWidgetView {
+    @ViewBuilder private var viewWhenTinyGlassModeIsOff: some View {
         HStack {
             Spacer()
             VStack(alignment: .leading) {
-                mainInfo()
+                ProfileAndMainStaminaView(
+                    profile: entry.profile,
+                    dailyNote: dailyNote,
+                    tinyGlassDisplayStyle: viewConfig.useTinyGlassDisplayStyle,
+                    verticalSpacing4NonTinyGlassMode: 5,
+                    useSpacer: false,
+                    refreshIntent: WidgetRefreshIntent(
+                        dailyNoteUIDWithGame: entry.profile?.uidWithGame
+                    )
+                )
                 Spacer(minLength: 18)
                 switch viewConfig.prioritizeExpeditionDisplay {
-                case false:
-                    DetailInfo(entry: entry, dailyNote: dailyNote, viewConfig: viewConfig, spacing: 17)
-                case true:
+                case true where hasExpeditionInfoForDisplay:
                     ExpeditionsView(
                         layout: .tinyWithShrinkedIconSpaces,
                         max4AllowedToDisplay: false,
@@ -39,11 +79,12 @@ struct LargeWidgetView: View {
                     .frame(width: 160)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .offset(x: dailyNote.game == .starRail ? -4 : -5)
+                default:
+                    DetailInfo(entry: entry, dailyNote: dailyNote, viewConfig: viewConfig, spacing: 17)
                 }
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            /// 绝区零没有探索派遣。
-            if dailyNote.game != .zenlessZone, !viewConfig.prioritizeExpeditionDisplay {
+            if hasExpeditionInfoForDisplay {
                 Spacer()
                 VStack(alignment: .leading) {
                     ExpeditionsView(
@@ -51,22 +92,148 @@ struct LargeWidgetView: View {
                         pilotAssetMap: entry.pilotAssetMap
                     )
                     Spacer(minLength: 15)
-                    if dailyNote.game == .genshinImpact, viewConfig.showMaterialsInLargeSizeWidget {
+                    if hasGIMaterialForDisplay {
                         MaterialView()
                     }
                 }
                 .frame(maxWidth: .infinity, alignment: .trailing)
             }
             if family == .systemExtraLarge {
-                officialFeedBlock()
+                trailingPaneForNonGlassViewMode()
                     .frame(width: 300)
             }
             Spacer()
         }
     }
 
-    // MARK: Private
+    @ViewBuilder
+    private func trailingPaneForNonGlassViewMode() -> some View {
+        VStack(alignment: .trailing) {
+            OfficialFeedList4WidgetsView(
+                events: entry.events,
+                showLeadingBorder: false
+            )
+            .padding(.leading, 20)
+            .widgetAccessibilityBackground(enabled: viewConfig.useTinyGlassDisplayStyle)
+            Spacer()
+            WeekdayDisplayView()
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
 
+// MARK: - View used when Tiny Glass Display Style is ON.
+
+@available(watchOS, unavailable)
+extension LargeWidgetView {
+    @ViewBuilder private var viewWhenTinyGlassModeIsON: some View {
+        let mainInfoView = ProfileAndMainStaminaView(
+            profile: entry.profile,
+            dailyNote: dailyNote,
+            tinyGlassDisplayStyle: viewConfig.useTinyGlassDisplayStyle,
+            verticalSpacing4NonTinyGlassMode: 5,
+            useSpacer: false,
+            refreshIntent: WidgetRefreshIntent(
+                dailyNoteUIDWithGame: entry.profile?.uidWithGame
+            )
+        )
+
+        Grid {
+            GridRow {
+                HStack {
+                    VStack(alignment: .leading) {
+                        mainInfoView.profileNameLabel
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                        Spacer()
+                        Group {
+                            switch viewConfig.prioritizeExpeditionDisplay {
+                            case true where hasExpeditionInfoForDisplay:
+                                ExpeditionsView(
+                                    layout: .tinyWithShrinkedIconSpaces,
+                                    max4AllowedToDisplay: false,
+                                    expeditions: dailyNote.expeditionTasks,
+                                    pilotAssetMap: entry.pilotAssetMap
+                                )
+                                .padding(.vertical, 8)
+                                .padding(.leading, 4)
+                                .padding(.trailing, 12)
+                                .frame(width: dailyNote.game == .starRail ? 155 : 145)
+                            default:
+                                DetailInfo(
+                                    entry: entry,
+                                    dailyNote: dailyNote,
+                                    viewConfig: viewConfig,
+                                    spacing: 6
+                                )
+                                .fixedSize(horizontal: true, vertical: false)
+                                .padding(.vertical, 8)
+                                .padding(.leading, 8)
+                            }
+                        }
+                        .widgetAccessibilityBackground(enabled: true)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                    VStack(alignment: .trailing) {
+                        if hasExpeditionInfoForDisplay {
+                            ExpeditionsView(
+                                layout: .tinyWithShrinkedIconSpaces,
+                                max4AllowedToDisplay: false,
+                                expeditions: dailyNote.expeditionTasks,
+                                pilotAssetMap: entry.pilotAssetMap
+                            )
+                            .padding(.vertical, 8)
+                            .padding(.leading, 4)
+                            .padding(.trailing, 12)
+                            .frame(width: dailyNote.game == .starRail ? 155 : 145)
+                            .widgetAccessibilityBackground(enabled: true)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .gridCellAnchor(.topLeading)
+
+                if family == .systemExtraLarge {
+                    OfficialFeedList4WidgetsView(
+                        events: entry.events,
+                        showLeadingBorder: false
+                    )
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .widgetAccessibilityBackground(enabled: viewConfig.useTinyGlassDisplayStyle)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .gridCellAnchor(.topTrailing)
+                }
+            }
+            GridRow {
+                mainInfoView.staminaLabelCompact
+                    .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                    .overlay(alignment: .bottomTrailing) {
+                        if hasGIMaterialForDisplay {
+                            MaterialView()
+                                .frame(maxWidth: 100)
+                        }
+                    }
+                    .gridCellAnchor(.bottomLeading)
+                if family == .systemExtraLarge {
+                    WeekdayDisplayView()
+                        .padding(.horizontal, 10)
+                        .widgetAccessibilityBackground(enabled: viewConfig.useTinyGlassDisplayStyle)
+                        .gridCellAnchor(.bottomTrailing)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+// MARK: - Peripherals.
+
+@available(watchOS, unavailable)
+extension LargeWidgetView {
     private var weekday: String {
         let formatter = DateFormatter.CurrentLocale()
         formatter.dateFormat = "E" // Shortened weekday format
@@ -80,65 +247,12 @@ struct LargeWidgetView: View {
         return formatter.string(from: Date())
     }
 
-    @ViewBuilder
-    private func officialFeedBlock() -> some View {
-        VStack(alignment: .trailing) {
-            OfficialFeedList4WidgetsView(
-                events: entry.events,
-                showLeadingBorder: false
-            )
-            .padding(.leading, 20)
-            Spacer()
-            WeekdayDisplayView()
-                .frame(maxWidth: .infinity, alignment: .trailing)
-        }
-        .frame(maxWidth: .infinity)
+    private var hasExpeditionInfoForDisplay: Bool {
+        /// 绝区零没有探索派遣。
+        dailyNote.game != .zenlessZone && !viewConfig.prioritizeExpeditionDisplay
     }
 
-    @ViewBuilder
-    private func mainInfo() -> some View {
-        VStack(alignment: .leading, spacing: 5) {
-            if let accountName = accountName {
-                HStack(alignment: .lastTextBaseline, spacing: 2) {
-                    Image(systemSymbol: .personFill)
-                        .legibilityShadow(isText: false)
-                    Text(accountName)
-                        .allowsTightening(true)
-                        .lineLimit(1)
-                        .fixedSize()
-                        .minimumScaleFactor(0.5)
-                        .legibilityShadow()
-                }
-                .font(.footnote)
-                .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
-            }
-            HStack(alignment: .firstTextBaseline, spacing: 2) {
-                Text(verbatim: "\(dailyNote.staminaIntel.finished)")
-                    .font(.system(size: 50, design: .rounded))
-                    .fontWeight(.medium)
-                    .minimumScaleFactor(0.1)
-                    .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
-                    .legibilityShadow()
-                dailyNote.game.primaryStaminaAssetIcon
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 30)
-                    .alignmentGuide(.firstTextBaseline) { context in
-                        context[.bottom] - 0.17 * context.height
-                    }
-                    .legibilityShadow(isText: false)
-            }
-            HStack {
-                Button(intent: WidgetRefreshIntent(dailyNoteUIDWithGame: entry.profile?.uidWithGame)) {
-                    Image(systemSymbol: .arrowClockwiseCircle)
-                        .font(.title3)
-                        .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
-                        .clipShape(.circle)
-                        .legibilityShadow()
-                }
-                .buttonStyle(.plain)
-                StaminaRecoveryTimeText(data: dailyNote)
-            }
-        }
+    private var hasGIMaterialForDisplay: Bool {
+        viewConfig.showMaterialsInLargeSizeWidget && dailyNote.game == .genshinImpact
     }
 }
