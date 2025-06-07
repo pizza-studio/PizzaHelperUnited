0fc29aa5f47906acc7fc157ce29728b9593e3c69
ShikiSuen <shikisuen@outlook.com>
2025-06-07 22:00:30 +0800

    PZWidgetsKit // +WidgetRefreshIntentProtocol.

---
+++ Packages/PZWidgetsKit/Sources/PZWidgetsKit/BasicTypesAndConfigs/RefreshIntentProtocol.swift
@@ -0,0 +1,9 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import AppIntents
+
+public protocol WidgetRefreshIntentProtocol: AppIntent {
+    init(dailyNoteUIDWithGame: String?)
+}
