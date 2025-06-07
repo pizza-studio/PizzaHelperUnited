eb333f8db692ea78077d4f0628dcff877b382415
ShikiSuen <shikisuen@outlook.com>
2025-06-07 18:45:21 +0800

    PZWidgetsKit // +MetaBlockView4Desktop.

---
+++ Packages/PZWidgetsKit/Sources/PZWidgetsKit/ViewsForWidgets/ViewComponents/MetaBlockRelated/MetaBlockView4Desktop.swift
@@ -0,0 +1,73 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import PZAccountKit
+import PZBaseKit
+import SwiftUI
+import WidgetKit
+
+// MARK: - MetaBlockView4Desktop
+
+public struct MetaBlockView4Desktop: View {
+    // MARK: Lifecycle
+
+    public init(
+        dailyNote: any DailyNoteProtocol,
+        viewConfig: WidgetViewConfig,
+        spacing: CGFloat = 13
+    ) {
+        self.dailyNote = dailyNote
+        self.viewConfig = viewConfig
+        self.spacing = spacing
+    }
+
+    // MARK: Public
+
+    public var lineHeightMax: CGFloat { 17 }
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: spacing) {
+            ForEach(Array(allMetaBars.enumerated()), id: \.offset) { offset, currentMetaBar in
+                AnyView(currentMetaBar.body).tag(offset)
+            }
+        }
+    }
+
+    // MARK: Private
+
+    private let dailyNote: any DailyNoteProtocol
+    private let viewConfig: WidgetViewConfig
+    private let spacing: CGFloat
+
+    private var allMetaBars: [any MetaBar] {
+        dailyNote.getMetaBlockContents(config: viewConfig)
+    }
+}
+
+#if DEBUG
+
+#Preview {
+    let viewConfig = WidgetViewConfig()
+    NavigationStack {
+        Form {
+            MetaBlockView4Desktop(
+                dailyNote: Pizza.SupportedGame.genshinImpact.exampleDailyNoteData,
+                viewConfig: viewConfig,
+                spacing: 0
+            )
+            MetaBlockView4Desktop(
+                dailyNote: Pizza.SupportedGame.starRail.exampleDailyNoteData,
+                viewConfig: viewConfig,
+                spacing: 0
+            )
+            MetaBlockView4Desktop(
+                dailyNote: Pizza.SupportedGame.zenlessZone.exampleDailyNoteData,
+                viewConfig: viewConfig,
+                spacing: 0
+            )
+        }.formStyle(.grouped)
+    }
+}
+
+#endif
