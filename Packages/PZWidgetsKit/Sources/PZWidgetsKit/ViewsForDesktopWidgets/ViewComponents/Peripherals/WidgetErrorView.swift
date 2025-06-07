ec7e8c67dc8f68d8e94d95e86c023b5260135977
ShikiSuen <shikisuen@outlook.com>
2025-06-07 22:40:55 +0800

    PZWidgetsKit // Add a namespace for refreshable desktop widgets.

---
+++ Packages/PZWidgetsKit/Sources/PZWidgetsKit/ViewsForDesktopWidgets/ViewComponents/Peripherals/WidgetErrorView.swift
@@ -0,0 +1,55 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import AppIntents
+import PZAccountKit
+import PZBaseKit
+import SwiftUI
+
+@available(watchOS, unavailable)
+extension DesktopWidgets {
+    public struct WidgetErrorView: View {
+        // MARK: Lifecycle
+
+        public init(error: any Error, message: String, refreshIntent: RefreshIntent? = nil) {
+            self.error = error
+            self.message = message
+            self.refreshIntent = refreshIntent
+        }
+
+        // MARK: Public
+
+        public var body: some View {
+            HStack(alignment: .top) {
+                let imageLabel = Image(systemSymbol: .arrowClockwiseCircle)
+                    .font(.title3)
+                    .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
+                    .clipShape(.circle)
+                    .legibilityShadow()
+                Group {
+                    if let refreshIntent {
+                        Button(intent: refreshIntent) {
+                            imageLabel
+                        }
+                        .buttonStyle(.plain)
+                    } else {
+                        imageLabel
+                    }
+                }
+                .padding()
+                Text(error.localizedDescription)
+                    .bold()
+                    .foregroundColor(PZWidgetsSPM.Colors.TextColor.primaryWhite.suiColor)
+                    .legibilityShadow()
+            }
+            .padding(20)
+        }
+
+        // MARK: Private
+
+        private let error: any Error
+        private let message: String
+        private let refreshIntent: RefreshIntent?
+    }
+}
---
+++ Packages/PZWidgetsKit/Sources/PZWidgetsKit/ViewsForDesktopWidgets/ViewComponents/ProfileWidgetComponents/MainInfo.swift
@@ -0,0 +1,40 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import PZAccountKit
+import PZBaseKit
+import SwiftUI
+
+@available(watchOS, unavailable)
+extension DesktopWidgets {
+    public struct MainInfo: View {
+        // MARK: Lifecycle
+
+        public init(
+            entry: ProfileWidgetEntry,
+            dailyNote: any DailyNoteProtocol,
+            viewConfig: WidgetViewConfig
+        ) {
+            self.entry = entry
+            self.dailyNote = dailyNote
+            self.viewConfig = viewConfig
+        }
+
+        // MARK: Public
+
+        public var body: some View {
+            ProfileAndMainStaminaView(
+                profile: entry.profile,
+                dailyNote: dailyNote,
+                tinyGlassDisplayStyle: viewConfig.useTinyGlassDisplayStyle
+            )
+        }
+
+        // MARK: Private
+
+        private let entry: ProfileWidgetEntry
+        private let dailyNote: any DailyNoteProtocol
+        private let viewConfig: WidgetViewConfig
+    }
+}
