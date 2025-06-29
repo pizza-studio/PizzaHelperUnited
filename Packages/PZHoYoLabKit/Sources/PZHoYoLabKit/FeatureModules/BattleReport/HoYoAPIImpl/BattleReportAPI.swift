c8e42a229bfba6d2087922ea51b6794c88d967ab
ShikiSuen <shikisuen@outlook.com>
2025-06-29 15:59:18 +0800

    PZHoYoLABKit // BattleReport: + Stygian Onslaught.

--- Packages/PZHoYoLabKit/Sources/PZHoYoLabKit/FeatureModules/BattleReport/HoYoAPIImpl/BattleReportAPI.swift
+++ Packages/PZHoYoLabKit/Sources/PZHoYoLabKit/FeatureModules/BattleReport/HoYoAPIImpl/BattleReportAPI.swift
@@ -10,8 +10,26 @@ extension HoYo {
     public static func getBattleReportSet(for profile: PZProfileSendable) async throws -> (any BattleReportSet)? {
         switch profile.game {
         case .genshinImpact:
-            let current = try await battleReportData4GI(for: profile, isPreviousRound: false)
-            let previous = try await battleReportData4GI(for: profile, isPreviousRound: true)
+            var current = try await battleReportData4GI(for: profile, isPreviousRound: false)
+            var previous = try await battleReportData4GI(for: profile, isPreviousRound: true)
+            let stygianOnslaught = try await battleReportData4GIStygianOnslaught(
+                server: profile.server,
+                uid: profile.uid,
+                cookie: profile.cookie,
+                deviceFingerPrint: profile.deviceFingerPrint,
+                deviceID: profile.deviceID
+            )
+            if stygianOnslaught.isUnlock {
+                if let currentSeasonSO = stygianOnslaught.data.first, currentSeasonSO.single.hasData {
+                    current.stygianOnslaught = currentSeasonSO
+                }
+                if stygianOnslaught.data.count > 1 {
+                    let previousSeasonSO = stygianOnslaught.data.dropFirst().first
+                    if let previousSeasonSO, previousSeasonSO.single.hasData {
+                        previous.stygianOnslaught = previousSeasonSO
+                    }
+                }
+            }
             return BattleReportSet4GI(current: current, previous: previous, profile: profile)
         case .starRail:
             let current = try await battleReportData4HSR(for: profile, isPreviousRound: false)
@@ -35,6 +53,7 @@ extension HoYo {
             deviceFingerPrint: profile.deviceFingerPrint,
             deviceID: profile.deviceID
         )
+        // 幽境危战的报告不在这里插入。
         return .init(spiralAbyss: spiralAbyss)
     }
 
@@ -121,6 +140,51 @@ extension HoYo {
         let data = try await request.serializingData().value
 
         return try .decodeFromMiHoYoAPIJSONResult(data: data, debugTag: "HoYo.battleReportData4GISpiralAbyss()")
+    }
+
+    private static func battleReportData4GIStygianOnslaught(
+        server: Server,
+        uid: String,
+        cookie: String,
+        deviceFingerPrint: String?,
+        deviceID: String?
+    ) async throws
+        -> BattleReport4GI.StygianOnslaughtQueryResult {
+        let type = HoYo.BattleReport4GI.TreasuresStarwardType.stygianOnslaught
+        await HoYo.waitFor300ms()
+        #if DEBUG
+        print("||| START REQUESTING SPIRAL ABYSS DATA (GI - StygianOnslaught) |||")
+        #endif
+        let queryItems: [URLQueryItem] = [
+            .init(name: "role_id", value: uid),
+            .init(name: "schedule_type", value: "1"), // 幽境危战会「同时」打包返回当前与上一个赛季的报告。
+            .init(name: "need_detail", value: "true"),
+            .init(name: "server", value: server.rawValue),
+        ]
+
+        let additionalHeaders: [String: String]? = {
+            if let deviceFingerPrint, !deviceFingerPrint.isEmpty, let deviceID {
+                [
+                    "x-rpc-device_fp": deviceFingerPrint,
+                    "x-rpc-device_id": deviceID,
+                ]
+            } else {
+                nil
+            }
+        }()
+
+        let request = try await Self.generateRecordAPIRequest(
+            region: server.region,
+            path: type.getAPIPath(region: server.region),
+            queryItems: queryItems,
+            cookie: cookie,
+            additionalHeaders: additionalHeaders
+        )
+        request.printDebugIntelIfDebugMode()
+
+        let data = try await request.serializingData().value
+
+        return try .decodeFromMiHoYoAPIJSONResult(data: data, debugTag: "HoYo.battleReportData4GIStygianOnslaught()")
     }
 }
 
@@ -305,6 +369,8 @@ extension HoYo.BattleReport4GI.TreasuresStarwardType {
         switch (self, region) {
         case (.spiralAbyss, .hoyoLab): "/game_record/app/genshin/api/spiralAbyss"
         case (.spiralAbyss, .miyoushe): "/game_record/app/genshin/api/spiralAbyss"
+        case (.stygianOnslaught, .hoyoLab): "/game_record/app/genshin/api/hard_challenge"
+        case (.stygianOnslaught, .miyoushe): "/game_record/app/genshin/api/hard_challenge"
         }
     }
 }
