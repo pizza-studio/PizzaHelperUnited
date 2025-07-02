0333b861b53dd0923c2bce8c35a3e42cd735a4b7
ShikiSuen <shikisuen@outlook.com>
2025-07-02 11:21:31 +0800

    Repo // Update SPM dependencies for Star Rail 3.4 release.

--- Packages/EnkaKit/Package.swift
+++ Packages/EnkaKit/Package.swift
@@ -24,10 +24,10 @@ let package = Package(
             url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .upToNextMajor(from: "6.2.0")
         ),
         .package(
-            url: "https://github.com/pizza-studio/EnkaDBGenerator", .upToNextMajor(from: "1.8.1")
+            url: "https://github.com/pizza-studio/EnkaDBGenerator", .upToNextMajor(from: "1.8.3")
         ),
         .package(
-            url: "https://github.com/pizza-studio/ArtifactRatingDB", .upToNextMajor(from: "1.1.4")
+            url: "https://github.com/pizza-studio/ArtifactRatingDB", .upToNextMajor(from: "1.1.6")
         ),
         .package(
             url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")
