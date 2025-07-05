a6b0ee15339de06e3f4c8ff1c02603fe061f5ac1
ShikiSuen <shikisuen@outlook.com>
2025-07-05 23:18:09 +0800

    PZHelper // Add RefugeeVM and ContentView4iOS14.

--- Packages/PZHelper/Sources/PZHelper/PZH_iOS14/ContentView4iOS14.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_iOS14/ContentView4iOS14.swift
@@ -3,12 +3,140 @@
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
 import Foundation
+import PZBaseKit
+import SFSafeSymbols
 import SwiftUI
 
 public struct ContentView4iOS14: View {
+    // MARK: Lifecycle
+
+    public init() {}
+
+    // MARK: Public
+
     public var body: some View {
+        let msgPack = fileSaveActionResultMessagePack
         NavigationView {
-            List {}
+            List {
+                Section {
+                    if theVM.localProfileEntriesCount > 0 {
+                        HStack {
+                            Text("refugee.exportableCount.profiles", bundle: .module)
+                                .multilineTextAlignment(.leading)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text(verbatim: theVM.localProfileEntriesCount.description)
+                                .foregroundColor(.secondary)
+                                .padding(.leading)
+                        }
+                    }
+                    if theVM.gachaEntriesCount > 0 {
+                        HStack {
+                            Text("refugee.exportableCount.gachaEntries", bundle: .module)
+                                .multilineTextAlignment(.leading)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text(verbatim: theVM.gachaEntriesCount.description)
+                                .foregroundColor(.secondary)
+                                .padding(.leading)
+                        }
+                    }
+                    if !hasData {
+                        Text("refugee.noDataExportable", bundle: .module)
+                            .foregroundColor(.secondary)
+                    }
+                } header: {
+                    HStack {
+                        Text("refugee.exportableCount.header", bundle: .module)
+                            .multilineTextAlignment(.leading)
+                            .textCase(.none)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+
+                        Button {
+                            theVM.startCountingDataEntriesTask(forced: true)
+                        } label: {
+                            Image(systemSymbol: .arrowClockwiseCircle)
+                        }
+                        .disabled(theVM.taskState == .busy)
+                    }
+                    .frame(maxWidth: .infinity)
+                } footer: {
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("refugee.footer.exportInstructions", bundle: .module)
+                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundColor(.accentColor)
+                        Text("refugee.footer.whyServiceTerminatedForOS21", bundle: .module)
+                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .navigationTitle(Text("app.appName.full", bundle: .module))
+            .navBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    if theVM.taskState == .busy {
+                        ProgressView()
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        theVM.startDocumentationPreparationTask(forced: false)
+                    } label: {
+                        Image(systemSymbol: .squareAndArrowUp)
+                    }
+                    .disabled(theVM.taskState == .busy)
+                }
+            }
+            .fileExporter(
+                isPresented: theVM.isExportDialogVisible,
+                document: theVM.currentExportableDocument,
+                contentType: .json,
+                defaultFilename: "PizzaHelper4Genshin_RefugeeMigrationData"
+            ) { result in
+                fileSaveActionResult = result
+                theVM.currentExportableDocument = nil
+            }
+            .alert(isPresented: isExportResultAvailable) {
+                Alert(
+                    title: Text(msgPack.title),
+                    message: Text(msgPack.message),
+                    dismissButton: Alert.Button.default(Text(verbatim: "✔︎")) {
+                        fileSaveActionResult = nil
+                        theVM.forceStopTheTask()
+                    }
+                )
+            }
+            .onChange(of: isExportResultAvailable.wrappedValue) { _ in
+                theVM.forceStopTheTask()
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+
+    // MARK: Private
+
+    @StateObject private var theVM = RefugeeVM4iOS14.shared
+    @State private var fileSaveActionResult: Result<URL, any Error>?
+
+    private var isExportResultAvailable: Binding<Bool> {
+        .init(get: { fileSaveActionResult != nil }, set: { _ in })
+    }
+
+    private var hasData: Bool {
+        theVM.localProfileEntriesCount + theVM.gachaEntriesCount > 0
+    }
+
+    private var fileSaveActionResultMessagePack: (title: String, message: String) {
+        switch fileSaveActionResult {
+        case let .success(url):
+            (
+                "refugee.export.succeededInSavingToFile".i18nRefugee,
+                "refugee.export.fileSavedTo:".i18nRefugee + "\n\n\(url)"
+            )
+        case let .failure(message):
+            ("refugee.export.failedInSavingToFile".i18nRefugee, "⚠︎ \(message)")
+        case nil: ("", "")
         }
     }
 }
---
+++ Packages/PZHelper/Sources/PZHelper/PZH_iOS14/RefugeeVM4iOS14.swift
@@ -0,0 +1,119 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Combine
+import GachaKit
+import PZCoreDataKit4GachaEntries
+import PZCoreDataKit4LocalAccounts
+import PZCoreDataKitShared
+import SwiftUI
+
+// MARK: - RefugeeVM4iOS14
+
+@MainActor
+public final class RefugeeVM4iOS14: TaskManagedVM4OS21 {
+    // MARK: Lifecycle
+
+    override private init() {
+        super.init()
+        configurePublisherObservations()
+        startCountingDataEntriesTask(forced: true)
+    }
+
+    // MARK: Public
+
+    public static let shared = RefugeeVM4iOS14()
+
+    @Published public var currentExportableDocument: RefugeeDocument?
+    @Published public var gachaEntriesCount: Int = 0
+    @Published public var localProfileEntriesCount: Int = 0
+
+    // MARK: Internal
+
+    var isExportDialogVisible: Binding<Bool> {
+        .init(get: {
+            self.currentExportableDocument != nil
+        }, set: { newValue in
+            if !newValue {
+                self.currentExportableDocument = nil
+            }
+        })
+    }
+
+    // MARK: Private
+
+    nonisolated(unsafe) private var cancellables: [AnyCancellable] = []
+
+    private func configurePublisherObservations() {
+        if #unavailable(iOS 17) {
+            NotificationCenter.default.publisher(for: .NSManagedObjectContextDidSave)
+                .sink(receiveValue: { _ in
+                    self.startCountingDataEntriesTask(forced: false)
+                })
+                .store(in: &cancellables)
+        }
+    }
+}
+
+extension RefugeeVM4iOS14 {
+    public func startDocumentationPreparationTask(forced: Bool) {
+        fireTask(
+            cancelPreviousTask: forced, givenTask: { [weak self] this in
+                self?.prepareDocument(completion: this)
+            }, completionHandler: { [weak self] newResult in
+                guard let this = self, let newResult else { return }
+                withAnimation {
+                    this.currentExportableDocument = .init(file: newResult)
+                }
+            }
+        )
+    }
+
+    public func startCountingDataEntriesTask(forced: Bool) {
+        fireTask(
+            cancelPreviousTask: forced, givenTask: { [weak self] this in
+                self?.countAllDataEntries(completion: this)
+            }, completionHandler: { [weak self] newResult in
+                guard let this = self, let newResult else { return }
+                let (intGacha, intProfile) = newResult
+                withAnimation {
+                    this.gachaEntriesCount = intGacha
+                    this.localProfileEntriesCount = intProfile
+                }
+            }
+        )
+    }
+}
+
+extension RefugeeVM4iOS14 {
+    private func prepareDocument(completion: @escaping (Result<RefugeeFile?, Error>) -> Void) {
+        DispatchQueue.main.async { [weak self] in
+            var result = RefugeeFile()
+            do {
+                result.oldGachaEntries4GI = try CDGachaMOSputnik.shared.getAllGenshinDataEntriesVanilla()
+                result.oldProfiles4GI = try AccountMOSputnik.shared.allAccountDataForGenshin()
+                completion(.success(result))
+                self?.taskState = .standBy
+            } catch {
+                completion(.failure(error))
+                self?.taskState = .standBy
+            }
+        }
+    }
+
+    private func countAllDataEntries(completion: @escaping (Result<(Int, Int)?, Error>) -> Void) {
+        DispatchQueue.main.async { [weak self] in
+            var (intGacha, intProfile) = (0, 0)
+            do {
+                intGacha = try CDGachaMOSputnik.shared.countAllDataEntries(for: .genshinImpact)
+                intProfile = try AccountMOSputnik.shared.countAllAccountData(for: .genshinImpact)
+                completion(.success((intGacha, intProfile)))
+                self?.taskState = .standBy
+            } catch {
+                completion(.failure(error))
+                self?.taskState = .standBy
+            }
+        }
+    }
+}
