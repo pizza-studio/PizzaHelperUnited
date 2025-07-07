90e66310c0b4de0583cccadb1938588e31d5a64a
ShikiSuen <shikisuen@outlook.com>
2025-07-07 21:44:23 +0800

    SPM // Trim unused Sworm imports.

--- Packages/PZKit/Package.swift
+++ Packages/PZKit/Package.swift
@@ -35,9 +35,6 @@ let package = Package(
             url: "https://github.com/sindresorhus/Defaults", .upToNextMajor(from: "9.0.2")
         )
         Package.Dependency.package(
-            url: "https://github.com/prisma-ai/Sworm.git", .upToNextMajor(from: "1.1.0")
-        )
-        Package.Dependency.package(
             url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")
         )
         Package.Dependency.package(path: "../PZCoreDataKit")
@@ -70,10 +67,6 @@ let package = Package(
                 Target.Dependency.product(
                     name: "PZCoreDataKit4LocalAccounts",
                     package: "PZCoreDataKit"
-                )
-                Target.Dependency.product(
-                    name: "Sworm",
-                    package: "Sworm"
                 )
                 Target.Dependency.product(
                     name: "Alamofire",
