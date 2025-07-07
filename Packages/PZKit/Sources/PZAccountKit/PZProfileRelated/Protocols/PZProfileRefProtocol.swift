1c76e44faa3b93e34fde9c4fb84404ddc8abfe34
ShikiSuen <shikisuen@outlook.com>
2025-07-07 18:14:57 +0800

    PZAccountKit // Organize PZProfileRefProtocol into a dedicated file.

---
+++ Packages/PZKit/Sources/PZAccountKit/PZProfileRelated/Protocols/PZProfileRefProtocol.swift
@@ -0,0 +1,151 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import PZBaseKit
+import PZCoreDataKit4LocalAccounts
+
+// MARK: - PZProfileRefProtocol
+
+public protocol PZProfileRefProtocol: AnyObject, ProfileMOProtocol, Codable {
+    init(
+        game: Pizza.SupportedGame,
+        server: HoYo.Server,
+        uid: String,
+        uuid: UUID,
+        allowNotification: Bool,
+        cookie: String,
+        deviceFingerPrint: String,
+        name: String,
+        priority: Int,
+        serverRawValue: String,
+        sTokenV2: String?,
+        deviceID: String
+    )
+
+    var asSendable: PZProfileSendable { get }
+}
+
+extension PZProfileRefProtocol {
+    @MainActor
+    public static func new(server: HoYo.Server, uid: String) -> Self {
+        // .description 很重要，防止 EXC_BAD_ACCESS。
+        Self.makeDefaultInstance(server: server, uid: uid)
+    }
+
+    @MainActor
+    public static func makeNewInstance(
+        server: HoYo.Server,
+        uid: String
+    )
+        -> Self {
+        makeDefaultInstance(server: server, uid: uid)
+    }
+
+    @MainActor
+    public static func makeDefaultInstance(
+        game: Pizza.SupportedGame = Pizza.SupportedGame.genshinImpact,
+        server: HoYo.Server = HoYo.Server.celestia(.genshinImpact),
+        uid: String = "114514810",
+        uuid: UUID = UUID(),
+        allowNotification: Bool = true,
+        cookie: String = "",
+        deviceFingerPrint: String = "",
+        name: String = "",
+        priority: Int = 0,
+        serverRawValue: String = HoYo.Server.celestia(.genshinImpact).rawValue,
+        sTokenV2: String? = "",
+        deviceID: String? = nil,
+    )
+        -> Self {
+        .init(
+            game: game,
+            server: server,
+            uid: uid,
+            uuid: uuid,
+            allowNotification: allowNotification,
+            cookie: cookie,
+            deviceFingerPrint: deviceFingerPrint,
+            name: name,
+            priority: priority,
+            serverRawValue: serverRawValue,
+            sTokenV2: sTokenV2,
+            deviceID: deviceID ?? ThisDevice.identifier4Vendor.description
+        )
+    }
+
+    // MARK: Lifecycle
+
+    /// 专门用来从旧版 AccountMO 迁移到全新的 PZProfileMO 账号体系的建构子。
+    /// - Parameters:
+    ///   - game: 游戏。
+    ///   - uid: UID。
+    ///   - configuration: 旧版 AccountMO。
+    @MainActor
+    public static func makeInheritedInstance(
+        game: Pizza.SupportedGame, uid: String,
+        configuration: AccountMOProtocol? = nil
+    )
+        -> Self? {
+        guard let server = HoYo.Server(uid: uid, game: game) else { return nil }
+        // .description 很重要，防止 EXC_BAD_ACCESS。
+        var result = makeDefaultInstance()
+        result.game = game
+        result.uid = uid
+        result.serverRawValue = server.rawValue
+        result.server = server.withGame(game)
+        if let configuration {
+            result.allowNotification = configuration.allowNotification
+            result.cookie = configuration.cookie
+            result.deviceFingerPrint = configuration.deviceFingerPrint
+            result.name = configuration.name
+            result.priority = configuration.priority
+            result.sTokenV2 = configuration.sTokenV2
+            result.uid = configuration.uid
+            result.uuid = configuration.uuid
+        }
+        return result
+    }
+}
+
+extension PZProfileRefProtocol {
+    public init(from decoder: any Decoder) throws {
+        let decoded = try PZProfileSendable(from: decoder)
+        self.init(
+            game: decoded.game,
+            server: decoded.server,
+            uid: decoded.uid,
+            uuid: decoded.uuid,
+            allowNotification: decoded.allowNotification,
+            cookie: decoded.cookie,
+            deviceFingerPrint: decoded.deviceFingerPrint,
+            name: decoded.name,
+            priority: decoded.priority,
+            serverRawValue: decoded.serverRawValue,
+            sTokenV2: decoded.sTokenV2,
+            deviceID: decoded.deviceID
+        )
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        try asSendable.encode(to: encoder)
+    }
+
+    public var asSendable: PZProfileSendable {
+        .init(
+            game: game,
+            server: server,
+            uid: uid,
+            uuid: uuid,
+            allowNotification: allowNotification,
+            cookie: cookie,
+            deviceFingerPrint: deviceFingerPrint,
+            name: name,
+            priority: priority,
+            serverRawValue: serverRawValue,
+            sTokenV2: sTokenV2,
+            deviceID: deviceID
+        )
+    }
+}
