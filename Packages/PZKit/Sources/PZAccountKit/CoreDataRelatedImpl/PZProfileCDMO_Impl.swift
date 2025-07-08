3e50a7701021ae02cee2c31da10f4d09f8235bd3
ShikiSuen <shikisuen@outlook.com>
2025-07-08 10:56:41 +0800

    PZAccountKit // PZProfileCDMO: Add protocol conformations.

---
+++ Packages/PZKit/Sources/PZAccountKit/CoreDataRelatedImpl/PZProfileCDMO_Impl.swift
@@ -0,0 +1,43 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+import Foundation
+import PZBaseKit
+import PZCoreDataKitShared
+import PZProfileCDMOBackports
+import SwiftData
+
+extension PZProfileCDMO: ProfileMOProtocol {
+    public var game: Pizza.SupportedGame {
+        get {
+            let matchedRaw = try? SDStringEnumCodec.decodeRawValue(from: gameBlob)
+            guard let matchedRaw else { return .genshinImpact }
+            return Pizza.SupportedGame(rawValue: matchedRaw) ?? .genshinImpact
+        }
+        set {
+            do {
+                gameBlob = try SDStringEnumCodec.encodeRawValue(newValue.rawValue, forKey: "game")
+            } catch {
+                // Do not handle. This shouldn't happen.
+                return
+            }
+        }
+    }
+
+    public var server: HoYo.Server {
+        get {
+            let matchedRaw = try? SDStringEnumCodec.decodeRawValue(from: serverBlob)
+            guard let matchedRaw else { return .celestia(game) }
+            return .init(rawValue: matchedRaw)?.withGame(game) ?? .celestia(game)
+        }
+        set {
+            do {
+                serverBlob = try SDStringEnumCodec.encodeRawValue(newValue.rawValue, forKey: "server")
+            } catch {
+                // Do not handle. This shouldn't happen.
+                return
+            }
+        }
+    }
+}
