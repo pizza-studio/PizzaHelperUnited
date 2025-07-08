ace5eed642b6c37b27d9f9224215d48477f6e16b
ShikiSuen <shikisuen@outlook.com>
2025-07-08 12:31:49 +0800

    PZAccountKit // Simplify profile-related protocols.
    - All such types are now considered mutable.

--- Packages/PZKit/Sources/PZAccountKit/PZProfileRelated/Protocols/ProfileProtocols.swift
+++ Packages/PZKit/Sources/PZAccountKit/PZProfileRelated/Protocols/ProfileProtocols.swift
@@ -6,9 +6,97 @@ import Foundation
 import PZBaseKit
 import PZCoreDataKit4LocalAccounts
 
+// MARK: - ProfileBasicProtocol
+
+/// AccountMO 不是统一披萨助手引擎用来主要处理的格式，
+/// 而是专门为了从 CloudKit 读取既有资料而实作的资料交换格式。
+/// 这也是为了方便直接继承旧版原披助手与穹披助手的云端资料。
+/// AccountMO 不曝露给前端使用，不直接用于 SwiftUI。
+public protocol ProfileBasicProtocol: Codable {
+    var allowNotification: Bool { get set }
+    var cookie: String { get set }
+    var deviceFingerPrint: String { get set }
+    var name: String { get set }
+    var priority: Int { get set }
+    var serverRawValue: String { get set }
+    var sTokenV2: String? { get set }
+    var uid: String { get set }
+    var uuid: UUID { get set }
+}
+
+extension Array where Element: ProfileBasicProtocol {
+    mutating public func fixPrioritySettings(respectExistingPriority: Bool = false) {
+        var newResult = self
+        if respectExistingPriority {
+            newResult.sort { $0.priority < $1.priority }
+        }
+        newResult.indices.forEach {
+            var newObj = newResult[$0]
+            newObj.priority = $0
+            newResult[$0] = newObj
+        }
+        self = newResult
+    }
+}
+
+extension ProfileBasicProtocol {
+    public var isValid: Bool {
+        true
+            && isUIDValid
+            && !name.isEmpty
+    }
+
+    public var isOfflineProfile: Bool {
+        cookie.isEmpty
+    }
+
+    public var isInvalid: Bool { !isValid }
+
+    private var isUIDValid: Bool {
+        guard let givenUIDInt = Int(uid) else { return false }
+        /// 绝区零的国服 UID 是八位。
+        #if os(watchOS)
+        return (100_000_00 ... Int(Int32.max)).contains(givenUIDInt)
+        #else
+        return (100_000_00 ... 9_999_999_999).contains(givenUIDInt)
+        #endif
+    }
+}
+
+// MARK: - ProfileProtocol
+
+public protocol ProfileProtocol: ProfileBasicProtocol {
+    var game: Pizza.SupportedGame { get set }
+    var deviceID: String { get set }
+    var server: HoYo.Server { get set }
+}
+
+extension ProfileProtocol {
+    public var uidWithGame: String {
+        "\(game.uidPrefix)-\(uid)"
+    }
+}
+
+extension ProfileProtocol {
+    public mutating func inherit(from target: some ProfileProtocol) {
+        uid = target.uid
+        uuid = target.uuid
+        allowNotification = target.allowNotification
+        cookie = target.cookie
+        deviceFingerPrint = target.deviceFingerPrint
+        name = target.name
+        priority = target.priority
+        serverRawValue = target.serverRawValue
+        sTokenV2 = target.sTokenV2
+        deviceID = target.deviceID
+        game = target.game
+        server = target.server
+    }
+}
+
 // MARK: - PZProfileRefProtocol
 
-public protocol PZProfileRefProtocol: AnyObject, ProfileMOProtocol, Codable {
+public protocol PZProfileRefProtocol: AnyObject, ProfileProtocol, Codable {
     init(
         game: Pizza.SupportedGame,
         server: HoYo.Server,
