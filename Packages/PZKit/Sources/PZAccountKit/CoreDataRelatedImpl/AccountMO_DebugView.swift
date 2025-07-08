20613d8f1900e9fb02aa56473aeffd334076fd8d
ShikiSuen <shikisuen@outlook.com>
2025-07-08 17:55:15 +0800

    PZCoreDataKit // Move AccountMOItemDebugView to PZAccountKit.

--- Packages/PZKit/Sources/PZAccountKit/CoreDataRelatedImpl/AccountMO_DebugView.swift
+++ Packages/PZKit/Sources/PZAccountKit/CoreDataRelatedImpl/AccountMO_DebugView.swift
@@ -3,7 +3,7 @@
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
 #if !os(watchOS)
-
+import PZCoreDataKit4LocalAccounts
 import PZCoreDataKitShared
 import SwiftUI
 @preconcurrency import Sworm
