724c7afd02d895edaab6d5c82d431ebd57062515
ShikiSuen <shikisuen@outlook.com>
2025-07-16 21:49:59 +0800

    PZBaseKit // +KeyboardReadable.

---
+++ Packages/PZKit/Sources/PZBaseKit/OSImpl/SwiftUIImpl/KeyboardReadable.swift
@@ -0,0 +1,30 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
+#if os(iOS) && !targetEnvironment(macCatalyst)
+
+import Combine
+import UIKit
+
+/// Publisher to read keyboard changes.
+public protocol KeyboardReadable {
+    var keyboardPublisher: AnyPublisher<Bool, Never> { get }
+}
+
+extension KeyboardReadable {
+    public var keyboardPublisher: AnyPublisher<Bool, Never> {
+        Publishers.Merge(
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillShowNotification)
+                .map { _ in true },
+
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in false }
+        )
+        .eraseToAnyPublisher()
+    }
+}
+
+#endif
