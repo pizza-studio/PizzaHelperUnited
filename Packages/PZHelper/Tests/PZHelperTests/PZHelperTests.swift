c1eb0e1d47fff2dec8a3e3ab62e45673630cfc11
ShikiSuen <shikisuen@outlook.com>
2025-07-28 13:30:11 +0800

    PZHelper // Use local geetest webpage instead.

--- Packages/PZHelper/Tests/PZHelperTests/PZHelperTests.swift
+++ Packages/PZHelper/Tests/PZHelperTests/PZHelperTests.swift
@@ -1,12 +1,13 @@
+// (c) 2024 and onwards Pizza Studio (AGPL v3.0 License or later).
+// ====================
+// This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
+
 @testable import PZHelper
-import XCTest
+import Testing
 
-final class PZHelperTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+struct PZHelperTests {
+    @Test
+    func testGeeTestURLAccessibility() {
+        #expect(GeetestValidateCoordinator.geetestURL != nil)
     }
 }
