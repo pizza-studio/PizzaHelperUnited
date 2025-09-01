5a023894c7956614fc013c0f97336ea4666b49ac
ShikiSuen <shikisuen@outlook.com>
2025-09-01 16:32:27 +0800

    PZBaseKit // Add dependency: CodableFileMonitor & SFSafeSymbols.

---
+++ Packages/PZKit/Sources/PZBaseKit/ReexportedModules.swift
@@ -0,0 +1,5 @@
+// (c) 2024 and onwards Pizza Studio (MIT License).
+// ====================
+// This code is released under the SPDX-License-Identifier: `MIT License`.
+
+@_exported import CodableFileMonitor
