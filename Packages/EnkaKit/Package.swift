0029b7855c1dd080fa8c3235e6efa1838b14786f
ShikiSuen <shikisuen@outlook.com>
2025-09-08 14:08:55 +0800

    SPM // EnkaDBGenerator -> v1.8.8.

--- Packages/EnkaKit/Package.swift
+++ Packages/EnkaKit/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(path: "../PZKit"),
         .package(path: "../WallpaperKit"),
         .package(
-            url: "https://github.com/pizza-studio/EnkaDBGenerator", .upToNextMajor(from: "1.8.7")
+            url: "https://github.com/pizza-studio/EnkaDBGenerator", .upToNextMajor(from: "1.8.8")
         ),
         .package(
             url: "https://github.com/pizza-studio/ArtifactRatingDB", .upToNextMajor(from: "1.2.0")
