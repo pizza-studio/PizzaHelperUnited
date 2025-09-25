c34775adf37353b8409d00b14f569863c183b2d0
ShikiSuen <shikisuen@outlook.com>
2025-09-25 19:24:17 +0800

    PZHelper & PZBaseKit // Optimize `onAppBecomeActive` for macOS.

--- Packages/PZHelper/Sources/PZHelper/PZH_Frontends/PZH_Features/StartupModifiers/UserDefaultsProfileSynchronizer.swift
+++ Packages/PZHelper/Sources/PZHelper/PZH_Frontends/PZH_Features/StartupModifiers/UserDefaultsProfileSynchronizer.swift
@@ -14,7 +14,7 @@ import UserNotifications
 private struct UserDefaultsProfileSynchronizer: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .onAppBecomeActive {
+            .onAppBecomeActive(debounceOnMac: false) {
                 Task { @MainActor in
                     await ProfileManagerVM.shared
                         .profileActor?
