e697933f95fc708ef9ab0d910527e7a392678637
ShikiSuen <shikisuen@outlook.com>
2025-10-26 19:24:12 +0800

    SPM // EnkaDB -> v1.9.1; ARDB -> 1.2.3.
    Update Package.swift

--- Packages/EnkaKit/Package.swift
+++ Packages/EnkaKit/Package.swift
@@ -18,10 +18,10 @@ let package = Package(
         .package(path: "../PZKit"),
         .package(path: "../WallpaperKit"),
         .package(
-            url: "https://github.com/pizza-studio/EnkaDBGenerator", .upToNextMajor(from: "1.9.0")
+            url: "https://github.com/pizza-studio/EnkaDBGenerator", .upToNextMajor(from: "1.9.1")
         ),
         .package(
-            url: "https://github.com/pizza-studio/ArtifactRatingDB", .upToNextMajor(from: "1.2.2")
+            url: "https://github.com/pizza-studio/ArtifactRatingDB", .upToNextMajor(from: "1.2.3")
         ),
     ],
     targets: [
