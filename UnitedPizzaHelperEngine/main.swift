f7706e9cd5d1fd1593520746656ba5d71f4f2b31
ShikiSuen <shikisuen@outlook.com>
2026-01-01 20:49:20 +0800

    MainApp // Move app constructor to PZHelper package.

--- UnitedPizzaHelperEngine/main.swift
+++ UnitedPizzaHelperEngine/main.swift
@@ -3,12 +3,5 @@
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
 import PZHelper
-import SwiftUI
 
-@main
-@MainActor
-struct UnitedPizzaHelperApp: App {
-    var body: some Scene {
-        PZHelper.makeMainScene()
-    }
-}
+PZHelper.MainApp.main()
