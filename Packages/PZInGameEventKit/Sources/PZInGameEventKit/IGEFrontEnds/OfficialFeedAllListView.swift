148cfaba83154c8b0a636f75e95f243725f8c4ba
ShikiSuen <shikisuen@outlook.com>
2026-01-27 13:41:36 +0800

    PZInGameEventKit // Refactor card view.

--- Packages/PZInGameEventKit/Sources/PZInGameEventKit/IGEFrontEnds/OfficialFeedAllListView.swift
+++ Packages/PZInGameEventKit/Sources/PZInGameEventKit/IGEFrontEnds/OfficialFeedAllListView.swift
@@ -63,18 +63,25 @@ extension OfficialFeed {
                     if contentsToHandle.isEmpty {
                         Spacer(minLength: 50)
                         Text("igev.gameEvents.noCurrentEventInfo", bundle: .currentSPM)
-                            .padding()
+                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity, minHeight: 70, alignment: .topLeading)
+                            .blurMaterialBackground(
+                                enabled: true,
+                                shape: RoundedRectangle(cornerRadius: 20),
+                                interactive: true
+                            )
                     }
                     ForEach(contentsToHandle, id: \.id) { content in
                         NavigationLink(
                             destination: eventDetail(event: content)
                         ) {
-                            cardView(content: content)
+                            SingleEventCardView(content: content)
                         }
                         .buttonStyle(.borderless)
                     }
                 }
                 .id(defaultServer4GI)
+                .padding(.horizontal)
             }
             .scrollContentBackground(.hidden)
             .listContainerBackground()
@@ -93,71 +100,6 @@ extension OfficialFeed {
                     } label: {
                         Image(systemSymbol: .filemenuAndSelection)
                     }
-                }
-            }
-        }
-
-        // MARK: CARD VIEW
-
-        @ViewBuilder
-        func cardView(content: EventModel) -> some View {
-            VStack {
-                ZStack {
-                    HStack {
-                        Spacer()
-                        Text(verbatim: content.title)
-                            .foregroundColor(.gray)
-                            .padding(.horizontal)
-                            .font(.caption)
-                    }
-                    AsyncImage(
-                        url: content.banner.asURL,
-                        transaction: Transaction(animation: .default)
-                    ) { phase in
-                        switch phase {
-                        case let .success(image):
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .scaleEffect(content.game == .starRail ? 1.07 : 1)
-                        default:
-                            WinUI3ProgressRing()
-                        }
-                    }
-                    .scaledToFill()
-                    .cornerRadius(20)
-                    .padding(.horizontal)
-                    VStack {
-                        Spacer()
-                        HStack {
-                            HStack(spacing: 2) {
-                                Image(systemSymbol: .hourglassCircle)
-                                    .font(.caption)
-                                Group {
-                                    let endAtIntel = content.endAtTime
-                                    if let dayLeft = endAtIntel.day, dayLeft > 0 {
-                                        Text(
-                                            "igev.gameEvents.daysLeft:\(dayLeft)",
-                                            bundle: .currentSPM
-                                        )
-                                    } else if let hoursLeft = endAtIntel.hour, hoursLeft > 0 {
-                                        Text(
-                                            "igev.gameEvents.hoursLeft:\(hoursLeft)",
-                                            bundle: .currentSPM
-                                        )
-                                    }
-                                }
-                                .padding(.trailing, 2)
-                                .font(.caption)
-                            }
-                            .padding(2)
-                            .opacityMaterial()
-                            Spacer()
-                        }
-                        .foregroundColor(.primary)
-                        .padding(.horizontal)
-                    }
-                    .padding()
                 }
             }
         }
@@ -194,10 +136,111 @@ extension OfficialFeed {
 }
 
 @available(iOS 17.0, macCatalyst 17.0, *)
-extension View {
-    @ViewBuilder
-    fileprivate func opacityMaterial() -> some View {
-        background(.thinMaterial, in: Capsule())
+extension OfficialFeed.OfficialFeedAllListView {
+    private struct SingleEventCardView: View {
+        // MARK: Lifecycle
+
+        public init(content data: EventModel) {
+            self.data = data
+        }
+
+        // MARK: Public
+
+        public var body: some View {
+            let theCardShape = RoundedRectangle(cornerRadius: 20)
+            AsyncImage(
+                url: data.banner.asURL,
+                transaction: Transaction(animation: .default)
+            ) { phase in
+                switch phase {
+                case let .success(image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaleEffect(data.game == .starRail ? 1.07 : 1)
+                default:
+                    HStack {
+                        Text(verbatim: data.title)
+                            .fontWeight(.medium)
+                            .fontWidth(.condensed)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .multilineTextAlignment(.leading)
+                        WinUI3ProgressRing()
+                    }
+                    .padding()
+                    .padding(.bottom, 35)
+                    .frame(maxWidth: .infinity, minHeight: 70, alignment: .topLeading)
+                    .blurMaterialBackground(
+                        enabled: true,
+                        shape: RoundedRectangle(cornerRadius: 20),
+                        interactive: true
+                    )
+                }
+            }
+            .overlay(alignment: .bottom) {
+                if cardRatio < 1.7 {
+                    Color.black.opacity(0.5).frame(height: canvasSize.height / 2.6)
+                        .overlay(alignment: .topLeading) {
+                            Text(verbatim: data.title)
+                                .foregroundStyle(.white)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .fontDesign(.serif)
+                                .shadow(radius: 4)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .lineLimit(1)
+                                .padding()
+                        }
+                }
+            }
+            .overlay(alignment: .bottomLeading) {
+                HStack {
+                    HStack(spacing: 2) {
+                        Image(systemSymbol: .hourglassCircle)
+                            .font(.caption)
+                        Group {
+                            let endAtIntel = data.endAtTime
+                            if let dayLeft = endAtIntel.day, dayLeft > 0 {
+                                Text(
+                                    "igev.gameEvents.daysLeft:\(dayLeft)",
+                                    bundle: .currentSPM
+                                )
+                            } else if let hoursLeft = endAtIntel.hour, hoursLeft > 0 {
+                                Text(
+                                    "igev.gameEvents.hoursLeft:\(hoursLeft)",
+                                    bundle: .currentSPM
+                                )
+                            }
+                        }
+                        .padding(.trailing, 2)
+                        .font(.caption)
+                    }
+                    .padding(2)
+                    .blurMaterialBackground(
+                        enabled: true,
+                        shape: .capsule,
+                        interactive: true
+                    )
+                }
+                .foregroundColor(.primary)
+                .padding()
+            }
+            .clipShape(theCardShape)
+            .contentShape(theCardShape)
+            .trackCanvasSize { canvasDimension in
+                canvasSize = canvasDimension
+            }
+        }
+
+        // MARK: Private
+
+        @State private var canvasSize: CGSize = .init(width: 300, height: 100)
+
+        private let data: EventModel
+
+        private var cardRatio: Double {
+            Swift.max(canvasSize.width, 1) / Swift.max(canvasSize.height, 1)
+        }
     }
 }
 
