7cad19d78ffe8f0004860fa22addc432f2e9026b
ShikiSuen <shikisuen@outlook.com>
2026-07-11 14:01:29 +0800

    WidgetExtension // Force reload all timelines on booting widgets.

--- WidgetExtension/Modules/PZWidgets.swift
+++ WidgetExtension/Modules/PZWidgets.swift
@@ -7,6 +7,7 @@ import PZAccountKit
 import PZBaseKit
 import PZWidgetsKit
 import WallpaperKit
+import WidgetKit
 
 // MARK: - PZWidgets
 
@@ -20,6 +21,10 @@ extension PZWidgets {
             // 不能再让小工具有权限存取 CoreData / SwiftData 了，已经炸了至少一万多起案例了。
             UserWallpaperFileHandler.migrateUserWallpapersFromUserDefaultsToFiles()
         }
+        // OS 27 起 WidgetKit 可能在 cold boot 時使用 stale 的 entity resolution 快取，
+        // 導致 widget configuration 中的 AppEntity 無法正確解析。
+        // 主動要求 reload 以確保 entity 能被重新 resolve。
+        WidgetCenter.shared.reloadAllTimelines()
     }
 
     public static func getAllProfiles(sortByGame: Bool = false) -> [PZProfileSendable] {
